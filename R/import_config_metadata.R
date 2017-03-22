2f836e950dd99a1c9583562b633ed4e5192f84b5
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-22 18:10:54 -0400

    Formatting fix

--- R/import_config_metadata.R
+++ R/import_config_metadata.R
@@ -19,40 +19,36 @@
 #' \dontrun{
 #' import_config_metadata(bcbio, group = "treatment")
 #' }
-import_config_metadata <-
-    function(project,
-             group = "treatment",
-             laneSplit = FALSE) {
-        metadata <- list.files(project$configDir,
-                               pattern = ".csv",
-                               full.names = TRUE) %>%
-            readr::read_csv(.,
-                            col_types = readr::cols()) %>%
-            basejump::setNamesSnake(.) %>%
-            #` dplyr::rename_(.dots = c("name" = "samplename")) %>%
-            dplyr::mutate_(.dots = set_names(list(group), "group"))
-
-        # Lane splitting
-        # Workflow used by Harvard Biopolymers Facility
-        if (isTRUE(laneSplit)) {
-            lane <- paste0("L", stringr::str_pad(1:4, 3, pad = "0"))
-            metadata <- metadata %>%
-                dplyr::group_by_(.dots = "samplename") %>%
-                tidyr::expand_(~lane) %>%
-                dplyr::left_join(metadata, by = "samplename") %>%
-                dplyr::ungroup() %>%
-                # NSE version
-                # dplyr::mutate(description = paste(samplename, lane, sep = "_")) %>%
-                dplyr::mutate_(.dots = set_names(
-                    list(quote(paste(samplename, lane, sep = "_"))),
-                    "description"
-                ))
-        }
-
-        # Arrange the rows by description
+import_config_metadata <- function(
+    bcbio,
+    group = "treatment",
+    lane_split = FALSE) {
+    metadata <- list.files(bcbio$config_dir,
+                           pattern = ".csv",
+                           full.names = TRUE) %>%
+        readr::read_csv(., col_types = readr::cols()) %>%
+        basejump::setNamesSnake(.) %>%
+        dplyr::mutate_(.dots = set_names(list(group), "group"))
+    
+    # Lane splitting
+    # Workflow used by Harvard Biopolymers Facility
+    if (isTRUE(lane_split)) {
+        lane <- paste0("L", stringr::str_pad(1:4, 3, pad = "0"))
         metadata <- metadata %>%
-            dplyr::arrange_(.dots = "samplename") %>%
-            basejump::setRownames(., "description")
-
-        return(metadata)
+            dplyr::group_by_(.dots = "samplename") %>%
+            tidyr::expand_(~lane) %>%
+            dplyr::left_join(metadata, by = "samplename") %>%
+            dplyr::ungroup() %>%
+            dplyr::mutate_(.dots = set_names(
+                list(quote(paste(samplename, lane, sep = "_"))),
+                "description"
+            ))
     }
+    
+    # Arrange the rows by description
+    metadata <- metadata %>%
+        dplyr::arrange_(.dots = "samplename") %>%
+        basejump::setRownames(., "description")
+    
+    return(metadata)
+}
