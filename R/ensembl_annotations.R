985ab4795087452709d9683a71469b03e2400644
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-25 08:39:09 -0400

    Simplify, don't need to output version

--- R/ensembl_annotations.R
+++ R/ensembl_annotations.R
@@ -5,7 +5,7 @@
 #' @keywords internal
 #'
 #' @import dplyr
-#' @importFrom biomaRt getBM listMarts useMart
+#' @importFrom biomaRt getBM useMart
 #'
 #' @param organism Organism identifier
 #'
@@ -17,14 +17,8 @@
 #' ensembl_annotations("mmusculus")
 #' }
 ensembl_annotations <- function(organism) {
-    ensembl_version <- biomaRt::listMarts(host = "useast.ensembl.org") %>%
-        dplyr::filter(biomart == "ENSEMBL_MART_ENSEMBL") %>%
-        dplyr::select(version) %>% .[[1]]
-    message(ensembl_version)
-
     mart <- biomaRt::useMart("ensembl", dataset = "mmusculus_gene_ensembl")
-    # mart_options <- biomaRt::listAttributes(mart)
-
+    # mart_attributes <- biomaRt::listAttributes(mart)
     annotations <-
         biomaRt::getBM(mart = mart,
                        attributes = c("ensembl_gene_id",
@@ -33,8 +27,6 @@ ensembl_annotations <- function(organism) {
                                       "gene_biotype")) %>%
         dplyr::arrange_(.dots = "ensembl_gene_id") %>%
         set_rownames("ensembl_gene_id")
-
     save(annotations, file = "data/annotations.rda")
-
     return(annotations)
 }
