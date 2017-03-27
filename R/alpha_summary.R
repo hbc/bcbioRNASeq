c17c87b183fa834fd51799f37cd6ddc670030f30
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-27 13:41:35 -0400

    `alpha_summary()` initial commit

---
+++ R/alpha_summary.R
@@ -0,0 +1,23 @@
+#' Print summary statistics of alpha level cutoffs
+#'
+#' @author Michael Steinbaugh
+#'
+#' @import DESeq2
+#'
+#' @param dds DESeq2 data set
+#' @param alpha alpha Numeric vector of alpha levels to check
+#'
+#' @return Printed summary
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' alpha_summary(dds, alpha = 0.05)
+#' }
+alpha_summary <- function(
+    dds,
+    alpha = c(0.1, 0.05, 0.01, 1e-3, 1e-6)) {
+    lapply(seq_along(alpha), function(a) {
+        DESeq2::results(dds, alpha = alpha[a]) %>% summary
+    }) %>% invisible
+}
