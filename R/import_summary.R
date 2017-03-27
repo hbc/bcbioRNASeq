5caa349b450bf3072abf03b09764d1b23c41ad96
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-27 18:27:25 -0400

    Set the `intgroup` in summary from metadata file

--- R/import_summary.R
+++ R/import_summary.R
@@ -7,15 +7,16 @@
 #' @import readr
 #'
 #' @param bcbio bcbio run object
+#' @param metadata Metadata data frame
 #'
 #' @export
 #' @examples
 #' \dontrun{
 #' import_summary(bcbio)
 #' }
-import_summary <- function(bcbio) {
-    df <- file.path(bcbio$project_dir,
-                    "project-summary.csv") %>%
+import_summary <- function(bcbio, metadata) {
+    summary <- file.path(bcbio$project_dir,
+                         "project-summary.csv") %>%
         readr::read_csv(., col_types = readr::cols()) %>%
         basejump::setNamesSnake(.) %>%
         # Remove NA only columns
@@ -26,5 +27,7 @@ import_summary <- function(bcbio) {
                                          "description"))) %>%
         dplyr::arrange_(.dots = "description") %>%
         set_rownames("description")
-    return(df)
+
+    summary$intgroup <- metadata[rownames(summary), "intgroup"]
+    return(summary)
 }
