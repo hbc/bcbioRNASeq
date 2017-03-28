90a96c5f67dd97a3bd59451c8e5d268640e6ec4e
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-28 17:46:53 -0400

    Unchain for better readability

--- R/melt_log10.R
+++ R/melt_log10.R
@@ -6,23 +6,31 @@
 #' @import reshape2
 #' @import tibble
 #'
+#' @param bcbio bcbio list object
 #' @param counts Counts matrix
-#' @param metadata Metadata data frame
 #'
 #' @return log10 melted data frame
 #' @export
-melt_log10 <- function(counts, metadata) {
-    counts %>%
-        as.data.frame %>%
-        tibble::rownames_to_column(.) %>%
-        reshape2::melt(., id = 1) %>%
-        set_names(c("ensembl_gene",
-                    "description",
-                    "counts")) %>%
-        dplyr::filter_(.dots = quote(counts > 0)) %>%
-        merge(metadata) %>%
-        dplyr::mutate_(.dots = set_names(list(
-            quote(log(counts))),
-            "counts"
-        ))
+melt_log10 <- function(bcbio, counts) {
+    metadata <- import_metadata(bcbio)
+
+    counts <- tmm_normalized_counts
+    counts <- as.data.frame(counts)
+    counts <- tibble::rownames_to_column(counts)
+    counts <- reshape2::melt(counts, id = 1)
+    counts <- set_names(counts, c("ensembl_gene_id",
+                                  # Change to `description` instead?
+                                  "samplename",
+                                  "counts"))
+    # Filter zero counts
+    counts <- counts[counts$counts > 0, ]
+    # log10 transform
+    counts$counts <- log(counts$counts)
+    # Convert samplename from factor to character
+    counts$samplename <- as.character(counts$samplename)
+    # Join metadata
+    counts <- dplyr::left_join(counts, metadata, by = "samplename")
+
+    #str(counts)
+    return(counts)
 }
