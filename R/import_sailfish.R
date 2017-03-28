aee88089cb7ea3498ddef243f525336cb077528f
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-28 15:57:08 -0400

    Remove wrapper functions

--- R/import_sailfish.R
+++
@@ -1,8 +0,0 @@
-#' Import sailfish counts
-#'
-#' @keywords internal
-#' @param ... Passthrough to \code{import_counts()}
-#' @export
-import_sailfish <- function(...) {
-    import_counts(..., type = "sailfish")
-}
--- R/import_salmon.R
+++
@@ -1,8 +0,0 @@
-#' Import salmon counts
-#'
-#' @keywords internal
-#' @param ... Passthrough to \code{import_counts()}
-#' @export
-import_salmon <- function(...) {
-    import_counts(..., type = "salmon")
-}
