6557ac9fc5838e0b3ccd72926fe2b773b313fb9b
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-29 16:42:40 -0400

    Improve count file name matching

--- R/import_counts.R
+++ R/import_counts.R
@@ -16,7 +16,6 @@
 #' @param type The type of software used to generate the abundances. Follows the
 #'   conventions of \code{tximport()}.
 #' @param save_tpm Whether to save transcripts per million data frame
-#' @param ... Passthrough to \code{tximport()}
 #'
 #' @return txi \code{tximport} list object
 #' @export
@@ -28,34 +27,36 @@
 import_counts <- function(
     bcbio,
     type = "salmon",
-    save_tpm = TRUE,
-    ...) {
+    save_tpm = TRUE) {
     if (!is.list(bcbio)) {
         stop("bcbio run object is required.")
     }
     if (!type %in% c("salmon", "sailfish")) {
         stop("Unsupported type.")
     }
-    
+
     summary <- import_summary(bcbio)
     tx2gene <- import_file(bcbio, file = "tx2gene.csv", col_names = FALSE)
-    
+
     # Parse the bcbio RNA-Seq run
     # bcbio names the final sample folders by `description`
-    files <- dir(bcbio$final_dir) %>%
-        .[. %in% summary$description] %>%
-        file.path(bcbio$final_dir,
-                  .,
-                  type,
-                  "quant",
-                  "quant.sf") %>%
-        sort
-    if (!length(files)) {
-        stop(paste(type, "files failed to load."))
+    sample_dirs <- dir(bcbio$final_dir) %>%
+        .[. %in% summary$description] %>% sort %>% unique
+    if (!length(sample_dirs)) {
+        stop("No sample directories matched the summary report.")
     }
-    
-    names(files) <- sort(summary$description)
-    
+
+    sample_files <- file.path(bcbio$final_dir, sample_dirs,
+                              type, "quant", "quant.sf")
+    names(sample_files) <- sample_dirs
+    if (!all(file.exists(sample_files))) {
+        stop(paste(type, "count files do not exist."))
+    }
+
+    message(paste("Loading", type, "counts..."))
+    message(paste(sample_dirs, collapse = "\n"))
+    # writeLines(sample_dirs)
+
     # Import the counts
     # https://goo.gl/h6fm15
     # countsFromAbundance = c("no", "scaledTPM", "lengthScaledTPM")
@@ -64,9 +65,8 @@ import_counts <- function(
                               type = type,
                               tx2gene = tx2gene,
                               reader = readr::read_tsv,
-                              countsFromAbundance = "lengthScaledTPM",
-                              ...)
-    
+                              countsFromAbundance = "lengthScaledTPM")
+
     # Transcripts per million
     if (isTRUE(save_tpm)) {
         tpm <- txi$abundance
@@ -79,10 +79,10 @@ import_counts <- function(
             utils::write.csv(tpm, file = "results/tpm.csv")
         }
     }
-    
+
     if (dir.exists("data")) {
         save(txi, file = "data/txi.rda")
     }
-    
+
     return(txi)
 }
