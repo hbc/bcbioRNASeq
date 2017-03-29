7277848b47cabb0d97201dcfddb49d7531921062
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-29 15:02:11 -0400

    Simplify lane splitting using base R methods

--- R/import_metadata.R
+++ R/import_metadata.R
@@ -7,7 +7,6 @@
 #' @import readr
 #' @import stringr
 #' @import tidyr
-#' @importFrom basejump printTable
 #'
 #' @param bcbio bcbio run object
 #' @param lane_split Whether samples were split across flow cell lanes
@@ -35,33 +34,31 @@ import_metadata <- function(
         readr::read_csv(., col_types = readr::cols()) %>%
         basejump::setNamesSnake(.)
 
-    # Lane splitting
-    # Workflow used by Harvard Biopolymers Facility
+    # Lane splitting This assumes the YAML descriptions won't match the
+    # `_L00[1-4]` suffix. Therefore, it renames both the samplename and
+    # description columns to match the bcbio server output. This workflow is
+    # used by Harvard Biopolymers Facility. We can decide to either combine
+    # counts at the server level using `cat` in bash, or we can run DESeq2 later
+    # by pooling the counts with `deseq_lane_pool()`. We may want to deprecate
+    # this method in the future and simply combine counts at the server level
+    # for all lane split runs.
     if (isTRUE(lane_split)) {
         lane <- paste0("L", stringr::str_pad(1:4, 3, pad = "0"))
         metadata <- metadata %>%
             dplyr::group_by_(.dots = "samplename") %>%
             tidyr::expand_(~lane) %>%
             dplyr::left_join(metadata, by = "samplename") %>%
-            dplyr::ungroup() %>%
-            dplyr::mutate_(.dots = set_names(
-                list(quote(paste(samplename, lane, sep = "_"))),
-                "samplename"
-            ))
+            dplyr::ungroup()
+        metadata$samplename <- paste(metadata$samplename, lane, sep = "_")
+        metadata$description <- metadata$samplename
     }
 
-    # Arrange the rows by description
-    metadata <- metadata %>%
-        dplyr::arrange_(.dots = "samplename") %>%
-        set_rownames("samplename")
+    # Ensure that rownames are set
+    rownames(metadata) <- metadata$description
 
     if (isTRUE(save)) {
         save(metadata, file = "data/metadata.rda")
         write.csv(metadata, file = "results/metadata.csv")
-        metadata[, c("samplename",
-                     "description",
-                     bcbio$intgroup)] %>%
-            basejump::printTable(., caption = "Sample metadata")
     }
 
     return(metadata)
