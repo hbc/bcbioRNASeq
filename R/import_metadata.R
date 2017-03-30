5d6a756ce0413a0bf6cb6a2299c64a26c85128ef
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-30 10:29:22 -0400

    Simplify, don't attempt to pool names here

--- R/import_metadata.R
+++ R/import_metadata.R
@@ -11,7 +11,6 @@
 #'
 #' @param bcbio bcbio run object
 #' @param save Save data frame
-#' @param lanes Whether samples were split across flow cell lanes
 #'
 #' @return Metadata data frame
 #' @export
@@ -20,34 +19,31 @@
 #' \dontrun{
 #' import_metadata(bcbio)
 #' }
-import_metadata <- function(
-    bcbio,
-    save = FALSE,
-    lanes = NULL) {
-    # Automatically detect if lanes are split, from bcbio object
-    if (is.null(lanes)) {
-        if (isTRUE(bcbio$lane_split)) {
-            lanes <- "split"
-        } else {
-            lanes <- "standard"
-        }
-    }
-
+import_metadata <- function(bcbio, save = FALSE) {
     metadata <- list.files(bcbio$config_dir,
                            pattern = ".csv",
                            full.names = TRUE) %>%
         readr::read_csv(., col_types = readr::cols()) %>%
-        basejump::setNamesSnake(.)
+        basejump::setNamesSnake(.) %>%
+        dplyr::arrange_(.dots = "description")
 
-    # Lane splitting This assumes the YAML descriptions won't match the
-    # `_L00[1-4]` suffix. Therefore, it renames both the samplename and
-    # description columns to match the bcbio server output. This workflow is
-    # used by Harvard Biopolymers Facility. We can decide to either combine
-    # counts at the server level using `cat` in bash, or we can run DESeq2 later
-    # by pooling the counts with `deseq_lane_pool()`. We may want to deprecate
-    # this method in the future and simply combine counts at the server level
-    # for all lane split runs.
-    if (lanes == "split") {
+    # Check against the sample_dirs
+    description_match <- identical(metadata$description,
+                                   names(bcbio$sample_dirs))
+    samplename_match <- identical(metadata$samplename,
+                                  names(bcbio$sample_dirs))
+
+    if (isTRUE(bcbio$lane_split) &
+        !isTRUE(description_match) &
+        isTRUE(samplename_match)) {
+        # Lane splitting. This assumes the YAML descriptions won't match the
+        # `_L00[1-4]` suffix. Therefore, it renames both the samplename and
+        # description columns to match the bcbio server output. This workflow is
+        # used by Harvard Biopolymers Facility. We can decide to either combine
+        # counts at the server level using `cat` in bash, or we can run DESeq2
+        # later by pooling the counts with `deseq_lane_pool()`. We may want to
+        # deprecate this method in the future and simply combine counts at the
+        # server level for all lane split runs.
         lane <- paste0("L", stringr::str_pad(1:4, 3, pad = "0"))
         metadata <- metadata %>%
             dplyr::group_by_(.dots = "samplename") %>%
@@ -55,8 +51,6 @@ import_metadata <- function(
             dplyr::left_join(metadata, by = "samplename") %>%
             dplyr::ungroup()
         metadata$samplename <- paste(metadata$samplename, lane, sep = "_")
-        metadata$description <- metadata$samplename
-    } else if (lanes == "pooled") {
         metadata$description <- metadata$samplename
     }
 
