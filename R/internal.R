db4eeb39c103e14b945e7e3b1658e2b3fc7b7cd4
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-31 10:10:52 -0400

    Rework internal imports

---
+++ R/internal.R
@@ -0,0 +1,22 @@
+#' set_names
+#' @name set_names
+#' @rdname set_names
+#' @keywords internal
+#' @importFrom purrr set_names
+NULL
+
+
+
+#' @importFrom basejump printTable
+#' @keywords internal
+print_table <- function(...) {
+    basejump::printTable(...)
+}
+
+
+
+#' @importFrom basejump setRownames
+#' @keywords internal
+set_rownames <- function(...) {
+    basejump::setRownames(...)
+}
