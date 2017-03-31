b59cece47bb9fab06541ad498ed99a5aecbeed92
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-31 11:40:41 -0400

    Initial commit

---
+++ R/tpm.R
@@ -0,0 +1,30 @@
+# https://support.bioconductor.org/p/84883/#84929
+
+
+#' Transcripts per million
+#'
+#' @author Michael Steinbaugh
+#'
+#' @import tibble
+#'
+#' @param txi tximport list object
+#'
+#' @return TPM (transcripts per million) data frame
+#' @export
+tpm <- function(txi) {
+    if (!identical(
+        names(txi),
+        c("abundance",
+          "counts",
+          "length",
+          "countsFromAbundance")
+    )) {
+        stop("tximport list is required")
+    }
+
+    tpm <- txi$abundance %>%
+        as.data.frame %>%
+        tibble::rownames_to_column(.)
+
+    return(tpm)
+}
