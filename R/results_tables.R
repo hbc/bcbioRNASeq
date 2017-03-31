3170f682f788a4be381ec5f0c0e2dc7b14027635
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-31 18:21:13 -0400

    Simplify function and rename

--- R/results_tables.R
+++ R/results_tables.R
@@ -1,4 +1,4 @@
-#' Differential gene expression results
+#' Differential gene expression results tables
 #'
 #' @author Michael Steinbaugh
 #'
@@ -9,7 +9,6 @@
 #'
 #' @param bcbio bcbio list object
 #' @param dds \code{DESeqDataSet} object
-#' @param alpha Alpha level cutoff. Used for Wald test BH-adjusted P values.
 #' @param lfc Log fold change ratio (base 2) cutoff. Does not apply to
 #'   statistical hypothesis testing, only gene filtering in the results tables.
 #'   See \code{?DESeq2::results} for additional information about using
@@ -21,18 +20,19 @@
 #'
 #' @examples
 #' \dontrun{
-#' de_results(bcbio, dds, alpha = 0.1, lfc = 0.25)
+#' results_tables(bcbio, res, lfc = 0.25)
 #' }
-de_results <- function(
+results_results <- function(
     bcbio,
-    dds,
-    alpha = 0.1,
+    res,
     lfc = 0) {
     check_bcbio_object(bcbio)
-    if (class(dds)[1] != "DESeqDataSet") {
-        stop("DESeqDataSet required")
+    if (class(res)[1] != "DESeqResults") {
+        stop("DESeqResults required")
     }
-    name <- deparse(substitute(dds))
+    name <- deparse(substitute(res)) %>%
+        gsub("^res$", "", .) %>%
+        gsub("_res$", "", .)
 
     # Running biomaRt without setting `values = results@rownames` is faster
     annotations <- ensembl_annotations(
@@ -42,42 +42,41 @@ de_results <- function(
                        "gene_biotype")
     )
 
-    # Nonzero genes
-    counts <- DESeq2::counts(dds, normalized = TRUE)
-    nonzero_counts <- counts[rowSums(counts) > 0, ]
-    ten_counts <- counts[rowSums(counts) > 10, ]
-
-    # DESeqResults object
-    res <- DESeq2::results(dds, alpha = alpha)
-
-    message(name)
+    if (!is.null(name)) {
+        message(name)
+    }
     message(paste0("alpha = ", res@metadata$alpha))
     message(paste0("lfc = ", lfc, " (only applied to tables)"))
 
     all <- res %>%
         as.data.frame %>%
         tibble::rownames_to_column("ensembl_gene_id") %>%
-        tibble::as_tibble(.) %>%
         basejump::setNamesSnake(.) %>%
         dplyr::left_join(annotations, by = "ensembl_gene_id") %>%
-        dplyr::arrange_(.dots = "padj")
+        dplyr::arrange_(.dots = "ensembl_gene_id") %>%
+        set_rownames("ensembl_gene_id")
 
     deg <- all %>%
-        dplyr::filter_(.dots = ~padj < alpha)
+        dplyr::filter_(.dots = ~padj < alpha) %>%
+        dplyr::arrange_(.dots = "padj") %>%
+        set_rownames("ensembl_gene_id")
 
     deg_lfc <- deg %>%
         dplyr::filter_(.dots = ~log2_fold_change < -lfc |
-                           log2_fold_change > lfc)
+                           log2_fold_change > lfc) %>%
+        set_rownames("ensembl_gene_id")
 
     deg_lfc_up <- deg_lfc %>%
         dplyr::filter_(.dots = ~log2_fold_change > 0) %>%
         # ~-log_fold_change also works.
         # Adding `quote()` here is more readable.
-        dplyr::arrange_(.dots = quote(-log2_fold_change))
+        dplyr::arrange_(.dots = quote(-log2_fold_change)) %>%
+        set_rownames("ensembl_gene_id")
 
     deg_lfc_down <- deg_lfc %>%
         dplyr::filter_(.dots = ~log2_fold_change < 0) %>%
-        dplyr::arrange_(.dots = "log2_fold_change")
+        dplyr::arrange_(.dots = "log2_fold_change") %>%
+        set_rownames("ensembl_gene_id")
 
     writeLines(c(
         paste(name, "differential expression"),
@@ -91,19 +90,19 @@ de_results <- function(
         "significance",
         paste("  alpha:", nrow(deg), "genes"),
         paste("  up:   ", nrow(deg_lfc_up), "genes (lfc applied)"),
-        paste("  down: ", nrow(deg_lfc_down), "genes (lfc applied)")
+        paste("  down: ", nrow(deg_lfc_down), "genes (lfc applied)"),
+        ""
     ))
 
     return(list(
         name = name,
         alpha = alpha,
         lfc = lfc,
-        dds = dds,
-        res = res,
         all = all,
         deg = deg,
         deg_lfc = deg_lfc,
         deg_lfc_up = deg_lfc_up,
-        deg_lfc_down = deg_lfc_down
+        deg_lfc_down = deg_lfc_down,
+        res = res
     ))
 }
