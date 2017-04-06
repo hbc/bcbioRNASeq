e8d89e9e33f62e88bc115f3be400f3fe8580e680
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-06 16:54:03 -0400

    Improve lane split sample handling

--- R/import_metadata.R
+++ R/import_metadata.R
@@ -9,8 +9,10 @@
 #' @importFrom utils write.csv
 #'
 #' @param bcbio bcbio run object
+#' @param grep Apply grep pattern matching to subset by description
 #' @param save Save data frame
 #' @param pool Pool lane split samples
+#' @param print Print table
 #'
 #' @return Metadata data frame
 #' @export
@@ -21,9 +23,11 @@
 #' }
 import_metadata <- function(
     bcbio,
+    grep = NULL,
     save = FALSE,
-    pool = FALSE) {
-    check_bcbio_object(bcbio)
+    pool = FALSE,
+    print = FALSE) {
+    check_bcbio(bcbio)
     metadata <- list.files(bcbio$config_dir,
                            pattern = ".csv",
                            full.names = TRUE) %>%
@@ -32,7 +36,6 @@ import_metadata <- function(
         dplyr::arrange_(.dots = "description")
 
     if (isTRUE(bcbio$lane_split) & !isTRUE(pool)) {
-        lane <- paste0("L", stringr::str_pad(1:4, 3, pad = "0"))
         # Lane splitting. This assumes the YAML descriptions won't match the
         # `_L00[1-4]` suffix. Therefore, it renames both the samplename and
         # description columns to match the bcbio server output. This workflow is
@@ -41,21 +44,31 @@ import_metadata <- function(
         # later by pooling the counts with `deseq_lane_pool()`. We may want to
         # deprecate this method in the future and simply combine counts at the
         # server level for all lane split runs.
+        lane <- paste0("L", stringr::str_pad(1:4, 3, pad = "0"))
+
         metadata <- metadata %>%
             dplyr::group_by_(.dots = "samplename") %>%
             tidyr::expand_(~lane) %>%
             dplyr::left_join(metadata, by = "samplename") %>%
             dplyr::ungroup()
         metadata$samplename <- paste(metadata$samplename, lane, sep = "_")
+
+        # Check against the sample_dirs
+        description_match <- identical(metadata$description,
+                                       names(bcbio$sample_dirs))
+        samplename_match <- identical(metadata$samplename,
+                                      names(bcbio$sample_dirs))
+
+        # Replace description for lanesplit samples
+        if (!isTRUE(description_match) & isTRUE(samplename_match)) {
+            metadata$description <- metadata$samplename
+        }
+    } else if (isTRUE(pool)) {
+        metadata$description <- metadata$samplename
     }
 
-    # Check against the sample_dirs
-    description_match <- identical(metadata$description,
-                                   names(bcbio$sample_dirs))
-    samplename_match <- identical(metadata$samplename,
-                                  names(bcbio$sample_dirs))
-    if (!isTRUE(description_match) | !isTRUE(samplename_match)) {
-        metadata$description <- metadata$samplename
+    if (!is.null(grep)) {
+        metadata <- metadata[grepl(grep, metadata$description), ]
     }
 
     metadata <- metadata %>%
@@ -65,6 +78,11 @@ import_metadata <- function(
     if (isTRUE(save)) {
         save(metadata, file = "data/metadata.rda")
         utils::write.csv(metadata, file = "meta/metadata.csv")
+    }
+
+    if (isTRUE(print)) {
+        metadata[, unique(c("description", bcbio$intgroup))] %>%
+            knit_table(caption = "Sample metadata")
     }
 
     return(metadata)
