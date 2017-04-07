1a4db60120401c018db8ee0173465825b12dbe73
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-07 11:35:29 -0400

    Rework to enable multiple kable knits in a single chunk

---
+++ R/multiple_kables.R
@@ -0,0 +1,19 @@
+#' Knit multiple kables in a single chunk
+#'
+#' @author Michael Steinbaugh
+#'
+#' @importFrom knitr asis_output
+#'
+#' @param list List of tables (e.g. data frame, matrix)
+#'
+#' @return Knit tables, using \code{kable()}
+#' @export
+#'
+#' @examples
+#' multiple_kables(list(iris, mtcars))
+multiple_kables <- function(list) {
+    tables <- lapply(seq_along(list), function(a) {
+        kable(list[a], caption = names(list)[a])
+    })
+    return(knitr::asis_output(tables))
+}
