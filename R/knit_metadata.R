d61f0cd22720aa42a4cc7d8736f4f31168f6dc7e
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-07 17:30:39 -0400

    Remove row names

--- R/knit_metadata.R
+++ R/knit_metadata.R
@@ -2,11 +2,14 @@
 #'
 #' @author Michael Steinbaugh
 #'
+#' @import tibble
+#'
 #' @param bcbio \code{bcbio-nextgen} run object
 #' @param metadata Metadata data frame
 #'
 #' @export
 knit_metadata <- function(bcbio, metadata) {
     metadata[, unique(c("description", bcbio$intgroup))] %>%
+        remove_rownames %>%
         kable(caption = "Sample metadata")
 }
