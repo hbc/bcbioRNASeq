e74987148b5c6a0edcfd8c7db9ba977a927e92c9
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-07 16:23:07 -0400

    Clean up documentation and remove explicit function calls

--- R/import_file.R
+++ R/import_file.R
@@ -1,28 +1,24 @@
-#' Import a bcbio project data file
-#'
-#' @author Michael Steinbaugh
+#' @rdname import
+#' @description Import a project data file
 #'
 #' @import readr
 #' @import stringr
 #' @import tibble
 #'
-#' @param bcbio bcbio run object
 #' @param file File name
-#' @param rownames Column identifier to use for rownames
-#' @param ... Optional pass through to \code{readr} package
+#' @param row_names Column identifier to use for row names
+#' @param ... Optional parameters for \code{readr}
 #'
 #' @return bcbio data
 #' @export
-#'
-#' @examples
-#' \dontrun{
-#' import_file(bcbio, file = "tx2gene.csv", rownames = "id")
-#' }
-import_file <- function(bcbio,
-                        file,
-                        rownames = NULL,
-                        ...) {
+import_file <- function(
+    bcbio,
+    file,
+    row_names = NULL,
+    ...) {
     check_bcbio(bcbio)
+
+    # Check that file exists
     filepath <- file.path(bcbio$project_dir, file)
     if (!file.exists(filepath)) {
         stop("file could not be found")
@@ -30,35 +26,33 @@ import_file <- function(bcbio,
 
     # Detect file extension
     if (grepl("\\.[a-z]+$", file)) {
-        # base method
         # ext <- gsub("^.*\\.([a-z]+)$", "\\1", file)
-        ext <- stringr::str_match(file, "\\.([a-z]+)$")[2]
+        ext <- str_match(file, "\\.([a-z]+)$")[2]
     } else {
         stop("file does not have an extension")
     }
 
     # File import
     if (ext == "csv") {
-        data <- readr::read_csv(filepath, col_types = readr::cols(), ...)
+        data <- read_csv(filepath, col_types = cols(), ...)
     } else if (ext == "tsv") {
-        data <- readr::read_tsv(filepath, col_types = readr::cols(), ...)
+        data <- read_tsv(filepath, col_types = cols(), ...)
     } else if (ext == "counts") {
-        data <- readr::read_tsv(filepath, col_types = readr::cols(), ...) %>%
-            as.matrix
+        data <- read_tsv(filepath, col_types = cols(), ...) %>% as.matrix
     } else {
         stop("unsupported file extension")
     }
 
     # Coerce tibble to data frame. Might want to disable this in a future update
     # as tibble becomes more standardized and the rownames issue is sorted out.
-    if (tibble::is_tibble(data)) {
+    if (is_tibble(data)) {
         data <- as.data.frame(data)
     }
 
-    # Set rownames
-    if (!is.null(rownames)) {
-        rownames(data) <- data[[rownames]]
-        data[[rownames]] <- NULL
+    # Set row names
+    if (!is.null(row_names)) {
+        rownames(data) <- data[[row_names]]
+        data[[row_names]] <- NULL
     }
 
     return(data)
