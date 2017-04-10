523181cef20098dc03bf94106a2178b306da466f
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-10 08:42:46 -0400

    Consolidate integrity checks

---
+++ R/check.R
@@ -0,0 +1,89 @@
+#' Perform object integrity checks
+#'
+#' @rdname check
+#' @author Michael Steinbaugh
+
+
+
+#' @rdname check
+#' @description Check \code{bcbio-nextgen} run
+#' @param run \code{bcbio-nextgen} run
+#' @export
+check_run <- function(run) {
+    if (!is.list(run)) {
+        stop("bcbio run object not found")
+    }
+    if (!length(dir(run$parent_dir))) {
+        stop("could not access parent_dir")
+    }
+    if (!length(dir(run$run_dir))) {
+        stop("could not access run_dir")
+    }
+    if (!length(dir(run$config_dir))) {
+        stop("could not access config_dir")
+    }
+    if (!length(dir(run$final_dir))) {
+        stop("could not access final_dir")
+    }
+    if (!length(run$sample_dirs)) {
+        stop("no sample directories in run")
+    }
+    if (!length(dir(run$project_dir))) {
+        stop("could not access project_dir")
+    }
+}
+
+
+
+#' @rdname check
+#' @description Check \code{DESeqDataSet}
+#' @param dds \code{DESeqDataSet}
+#' @param stop Stop if class doesn't match
+#' @export
+check_dds <- function(dds, stop = TRUE) {
+    if (class(dds)[1] == "DESeqDataSet") {
+        return(TRUE)
+    } else {
+        if (isTRUE(stop)) {
+            stop("DESeqDataSet required")
+        } else {
+            return(FALSE)
+        }
+    }
+}
+
+
+
+#' @rdname check
+#' @description Check \code{DESeqTransform}
+#' @param dt \code{DESeqTransform}
+#' @export
+check_dt <- function(dt, stop = TRUE) {
+    if (class(dt)[1] == "DESeqTransform") {
+        return(TRUE)
+    } else {
+        if (isTRUE(stop)) {
+            stop("DESeqDataSet required")
+        } else {
+            return(FALSE)
+        }
+    }
+}
+
+
+
+#' @rdname check
+#' @description Check \code{DESeqResults}
+#' @param res \code{DESeqResults}
+#' @export
+check_res <- function(res, stop = TRUE) {
+    if (class(res)[1] == "DESeqResults") {
+        return(TRUE)
+    } else {
+        if (isTRUE(stop)) {
+            stop("DESeqDataSet required")
+        } else {
+            return(FALSE)
+        }
+    }
+}
