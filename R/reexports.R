dd6f0af984de4b2e062df707f1adfb07fd1b25c1
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-12 11:18:28 -0400

    Only reexport the pipe function

--- R/reexports.R
+++ R/reexports.R
@@ -1,22 +1,6 @@
-#' @importFrom basejump setNamesSnake
-#' @keywords internal
-set_names_snake <- basejump::setNamesSnake
-
-
-
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
-
-#' @importFrom magrittr set_names
-#' @export
-magrittr::set_names
-
-#' @importFrom magrittr set_rownames
-#' @export
-magrittr::set_rownames
-
-
 
 #' @importFrom utils globalVariables
 #' @keywords internal
