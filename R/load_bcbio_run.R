1c8641f80cb8f79badb1b0f66fe7cfdcb5c69b84
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-12 18:02:57 -0400

    Adjust spacing

--- R/load_bcbio_run.R
+++ R/load_bcbio_run.R
@@ -22,86 +22,86 @@
 #' \dontrun{
 #' load_bcbio_run("illumina_rnaseq")
 #' }
-load_bcbio_run <-
-    function(template,
-             organism,
-             intgroup = NULL,
-             parent_dir = getwd(),
-             config_dir = NULL,
-             final_dir = NULL) {
-        # Interesting groups, defaults to description
-        if (is.null(intgroup)) {
-            intgroup <- "description"
-        }
-
-        # run_dir
-        # Automatic, uses template
-        run_dir <- file.path(parent_dir, template)
-
-        # Preliminary run check
-        if (!length(dir(run_dir))) {
-            stop("run directory failed to load")
-        }
-
-        # config_dir
-        if (is.null(config_dir)) {
-            config_dir <- file.path(run_dir, "config")
-        }
-
-        # final_dir
-        if (is.null(final_dir)) {
-            final_dir <- file.path(run_dir, "final")
-        }
-
-        # project_dir
-        # Naming scheme is `template/final/YYYY-MM-DD_template`
-        project_dir <- file.path(final_dir) %>%
-            dir(full.names = TRUE) %>%
-            .[grepl(paste0("/\\d{4}-\\d{2}-\\d{2}_[^/]+$"), .)]
-
-        # Sample directories
-        sample_dirs <- dir(final_dir, full.names = TRUE)
-        names(sample_dirs) <- basename(sample_dirs)
-        # Dated `project_dir` is nested here, need to exclude
-        sample_dirs <-
-            sample_dirs[!grepl("^\\d{4}-\\d{2}-\\d{2}_", names(sample_dirs))]
-
-        # Data versions
-        data_versions <- file.path(project_dir, "data_versions.csv") %>%
-            readr::read_csv(., col_types = readr::cols())
-
-        # Detect lane split samples
-        if (any(grepl("_L\\d+$", dir(final_dir)))) {
-            lane_split <- TRUE
-        } else {
-            lane_split <- FALSE
-        }
-
-        run <- list(
-            # Input required
-            template = template,
-            organism = organism,  # can automate by parsing YAML
-            # Input recommended
-            intgroup = intgroup,
-            parent_dir = normalizePath(parent_dir),
-            # Automatic for standard runs
-            run_dir = normalizePath(run_dir),
-            config_dir = normalizePath(config_dir),
-            final_dir = normalizePath(final_dir),
-            project_dir = normalizePath(project_dir),
-            sample_dirs = sample_dirs,
-            # Fully automatic
-            lane_split = lane_split,
-            date = Sys.Date(),
-            session_info = sessionInfo(),
-            data_versions = data_versions,
-            wd = getwd()
-        )
-
-        check_run(run)
-
-        create_local_project()
-        save(run, file = "data/run.rda")
-
-        return(run)
+load_bcbio_run <- function(
+    template,
+    organism,
+    intgroup = NULL,
+    parent_dir = getwd(),
+    config_dir = NULL,
+    final_dir = NULL) {
+    # Interesting groups, defaults to description
+    if (is.null(intgroup)) {
+        intgroup <- "description"
     }
+
+    # run_dir
+    # Automatic, uses template
+    run_dir <- file.path(parent_dir, template)
+
+    # Preliminary run check
+    if (!length(dir(run_dir))) {
+        stop("run directory failed to load")
+    }
+
+    # config_dir
+    if (is.null(config_dir)) {
+        config_dir <- file.path(run_dir, "config")
+    }
+
+    # final_dir
+    if (is.null(final_dir)) {
+        final_dir <- file.path(run_dir, "final")
+    }
+
+    # project_dir
+    # Naming scheme is `template/final/YYYY-MM-DD_template`
+    project_dir <- file.path(final_dir) %>%
+        dir(full.names = TRUE) %>%
+        .[grepl(paste0("/\\d{4}-\\d{2}-\\d{2}_[^/]+$"), .)]
+
+    # Sample directories
+    sample_dirs <- dir(final_dir, full.names = TRUE)
+    names(sample_dirs) <- basename(sample_dirs)
+    # Dated `project_dir` is nested here, need to exclude
+    sample_dirs <-
+        sample_dirs[!grepl("^\\d{4}-\\d{2}-\\d{2}_", names(sample_dirs))]
+
+    # Data versions
+    data_versions <- file.path(project_dir, "data_versions.csv") %>%
+        readr::read_csv(., col_types = readr::cols())
+
+    # Detect lane split samples
+    if (any(grepl("_L\\d+$", dir(final_dir)))) {
+        lane_split <- TRUE
+    } else {
+        lane_split <- FALSE
+    }
+
+    run <- list(
+        # Input required
+        template = template,
+        organism = organism,  # can automate by parsing YAML
+        # Input recommended
+        intgroup = intgroup,
+        parent_dir = normalizePath(parent_dir),
+        # Automatic for standard runs
+        run_dir = normalizePath(run_dir),
+        config_dir = normalizePath(config_dir),
+        final_dir = normalizePath(final_dir),
+        project_dir = normalizePath(project_dir),
+        sample_dirs = sample_dirs,
+        # Fully automatic
+        lane_split = lane_split,
+        date = Sys.Date(),
+        session_info = sessionInfo(),
+        data_versions = data_versions,
+        wd = getwd()
+    )
+
+    check_run(run)
+
+    create_local_project()
+    save(run, file = "data/run.rda")
+
+    return(run)
+}
