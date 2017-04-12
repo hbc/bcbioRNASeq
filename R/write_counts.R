4bdf8e6a88b7d99d35741e06058211533dfa83bf
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-12 10:36:40 -0400

    Initial commit

---
+++ R/write_counts.R
@@ -0,0 +1,20 @@
+#' Write gzipped counts to the results directory
+#'
+#' @author Michael Steinbaugh
+#'
+#' @import readr
+#' @import tibble
+#'
+#' @param counts Counts matrix
+#'
+#' @export
+write_counts <- function(counts) {
+    dir.create(file.path("results", "counts"), showWarnings = FALSE)
+    name <- deparse(substitute(counts))
+    counts %>%
+        rownames_to_column %>%
+        write_csv(
+            path = file.path("results", "counts",
+                             paste0(name, ".gz")))
+
+}
