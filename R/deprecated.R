d0edd5a4a800dc385c6d5bea7883d3fd811b28f2
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-13 12:44:03 -0400

    Initial commit

---
+++ R/deprecated.R
@@ -0,0 +1,15 @@
+create_local_project <- function() {
+    .Deprecated("create_new_project")
+}
+
+import_file <- function() {
+    .Deprecated("read_file")
+}
+
+import_metadata <- function() {
+    .Deprecated("read_metadata")
+}
+
+import_summary <- function() {
+    .Deprecated("read_qc_summary")
+}
