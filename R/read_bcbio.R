08c719f1bf67ef953b5b3e29bc253e56a34fc75d
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-26 17:45:03 -0400

    Placeholder function for metadata YAML parsing

--- R/read_bcbio.R
+++ R/read_bcbio.R
@@ -11,7 +11,7 @@
 
 
 #' @rdname read_bcbio
-#' @description Import RNA-Seq counts using \code{tximport}
+#' @description Read RNA-Seq counts using \code{tximport}
 #'
 #' @param type The type of software used to generate the abundances. Follows the
 #'   conventions of \code{tximport}.
@@ -75,7 +75,7 @@ read_bcbio_counts <- function(
 
 
 #' @rdname read_bcbio
-#' @description Import a project data file
+#' @description Read a project data file
 #'
 #' @param file File name
 #' @param row_names Column identifier to use for row names
@@ -132,80 +132,18 @@ read_bcbio_file <- function(
 
 
 #' @rdname read_bcbio
-#' @description Import metadata
-#'
-#' @param grep Apply grep pattern matching to samples
-#' @param pool Pool lane split samples
-#'
+#' @description Read YAML metadata
 #' @return Metadata data frame
 #' @export
-read_bcbio_metadata <- function(
-    run,
-    grep = NULL,
-    save = FALSE,
-    pool = FALSE) {
-    check_run(run)
-    metadata <- list.files(run$config_dir,
-                           pattern = ".csv$",
-                           full.names = TRUE) %>%
-        read_csv(col_types = cols()) %>%
-        set_names_snake
-
-    if (isTRUE(run$lane_split) & !isTRUE(pool)) {
-        # Lane splitting. This assumes the YAML descriptions won't match the
-        # `_L00[1-4]` suffix. Therefore, it renames both the samplename and
-        # description columns to match the bcbio server output. This workflow is
-        # used by Harvard Biopolymers Facility. We can decide to either combine
-        # counts at the server level using `cat` in bash, or we can run DESeq2
-        # later by pooling the counts with `deseq_lane_pool()`. We may want to
-        # deprecate this method in the future and simply combine counts at the
-        # server level for all lane split runs.
-        lane <- paste0("L", str_pad(1:4, 3, pad = "0"))
-
-        metadata <- metadata %>%
-            group_by(.data$samplename) %>%
-            expand_(~lane) %>%
-            left_join(metadata, by = "samplename") %>%
-            ungroup
-        metadata$samplename <- paste(metadata$samplename, lane, sep = "_")
-
-        # Check against the sample directories
-        description_match <- identical(
-            metadata$description,
-            names(run$sample_dirs))
-        samplename_match <- identical(
-            metadata$samplename,
-            names(run$sample_dirs))
-
-        # Replace description for lanesplit samples
-        if (!isTRUE(description_match) & isTRUE(samplename_match)) {
-            metadata$description <- metadata$samplename
-        }
-    } else if (isTRUE(pool)) {
-        metadata$description <- metadata$samplename
-    }
-
-    if (!is.null(grep)) {
-        metadata <- metadata[grepl(grep, metadata$description), ]
-    }
-
-    metadata <- metadata %>%
-        .[order(.$description), ] %>%
-        as.data.frame %>%
-        set_rownames(.$description)
-
-    if (isTRUE(save)) {
-        save(metadata, file = "data/metadata.rda")
-        write_csv(metadata, "meta/metadata.csv")
-    }
-
-    return(metadata)
+read_bcbio_metadata <- function(run) {
+    message("project-summary.yaml")
+    stop("YAML parser still in development")
 }
 
 
 
 #' @rdname read_bcbio
-#' @description Import project quality control summary statistics
+#' @description Read project quality control summary statistics
 #' @return Summary data frame
 #' @export
 read_bcbio_qc_summary <- function(run, save = FALSE) {
