513661bf682927761533685fc93977e263d4dbcf
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-27 22:04:26 -0400

    Update to allow writing of multiple files

--- R/write_counts.R
+++ R/write_counts.R
@@ -2,18 +2,27 @@
 #'
 #' @author Michael Steinbaugh
 #'
-#' @param counts Counts matrix
+#' @param ... Count matrices, passed in as dots
+#' @param dir Output directory
 #'
 #' @export
-write_counts <- function(counts) {
-    dir.create(file.path("results", "counts"),
-               recursive = TRUE, showWarnings = FALSE)
-    name <- deparse(substitute(counts))
-    counts %>%
-        as.data.frame %>%
-        rownames_to_column("ensembl_gene_id") %>%
-        as_tibble %>%
-        write_csv(
-            path = file.path("results", "counts",
-                             paste0(name, ".csv.gz")))
+write_counts <- function(..., dir = file.path("results", "counts")) {
+    names <- as.character(substitute(list(...)))[-1L]
+    dots <- list(...)
+
+    # Create the counts output directory
+    dir.create(dir, recursive = TRUE, showWarnings = FALSE)
+
+    message(paste("Writing", toString(names), "to", dir))
+
+    # Iterate across the dots and write CSVs
+    lapply(seq_along(names), function(a) {
+        name <- names[a]
+        counts <- dots[a]
+        counts %>%
+            as.data.frame %>%
+            rownames_to_column("ensembl_gene_id") %>%
+            as_tibble %>%
+            write_csv(path = file.path(dir, paste0(name, ".csv.gz")))
+    }) %>% invisible
 }
