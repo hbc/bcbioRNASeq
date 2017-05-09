e5622680f0a58a049be7be5d596b649f529008cc
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-09 08:20:30 -0400

    Handle dependency files by downloading from HBC website instead of storing inside RMarkdown template skeleton

---
+++ R/references.R
@@ -0,0 +1,17 @@
+#' Check for BibTex references library
+#'
+#' If file isn't present, download latest version from the HBC website
+#'
+#' @author Michael Steinbaugh
+#'
+#' @param bibtex_file BibTex library file
+#'
+#' @export
+references <- function(bibtex_file = "bcbioRnaseq.bib") {
+    if (!file.exists(bibtex_file)) {
+        download.file(file.path("http://bioinformatics.sph.harvard.edu",
+                                "bcbioRnaseq",
+                                bibtex_file),
+                      bibtex_file)
+    }
+}
