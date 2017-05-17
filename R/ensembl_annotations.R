b07bcbc0fe08c674f588c59629520a5febb306c0
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-16 21:25:17 -0400

    Provide transcript annotations using code from bcbioSinglecell package

--- R/ensembl_annotations.R
+++ R/ensembl_annotations.R
@@ -1,54 +1,45 @@
-#' [Ensembl](http://www.ensembl.org/) annotations
+#' Ensembl annotations
 #'
-#' Download the most recent gene annotations from Ensembl.
+#' Download transcript-to-gene annotations from
+#' [Ensembl](http://www.ensembl.org/). This function also defines broad classes,
+#' which are used in quality control analysis.
 #'
+#' @author Rory Kirchner
 #' @author Michael Steinbaugh
 #'
 #' @param run bcbio-nextgen run.
-#' @param attributes Ensembl attributes.
-#' @param filters biomaRt filters.
-#' @param values Ensembl gene identifier values. Optional but will run faster if
-#'   specified.
 #'
 #' @return Data frame.
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' ensembl_annotations(run, values = "ENSMUSG00000000001")
-#' }
-#'
-#' @seealso
-#' \code{\link[biomaRt]{listAttributes}}, \code{\link[biomaRt]{listFilters}}.
-ensembl_annotations <- function(
-    run,
-    attributes = NULL,
-    filters = "ensembl_gene_id",
-    values = NULL) {
+ensembl_annotations <- function(run) {
+    import_tidy_verbs()
+    # Broad class definitions
+    coding <- c("protein_coding")
+    decaying <- c("non_stop_decay", "nonsense_mediated_decay")
+    noncoding <- c("known_ncrna", "lincRNA", "non_coding")
+    srna <- c("miRNA", "misc_RNA", "ribozyme", "rRNA", "scaRNA", "scRNA",
+              "snoRNA", "snRNA", "sRNA")
     ensembl <- useEnsembl(
         biomart = "ensembl",
         dataset = paste(run$organism, "gene_ensembl", sep = "_"))
-
-    # Set biomaRt input defaults
-    if (is.null(values)) {
-        filters <- ""
-        values <- ""
-    }
-
-    # Defaults to gene name matching
-    if (!is.null(attributes)) {
-        attributes <- c("ensembl_gene_id", attributes) %>% unique
-    } else {
-        attributes <- c("ensembl_gene_id", "external_gene_name")
-    }
-
-    annotations <-
-        getBM(mart = ensembl,
-              attributes = attributes,
-              filters = filters,
-              values = values) %>%
-        arrange(!!!syms(colnames(.))) %>%
-        set_rownames(.$ensembl_gene_id)
-
-    return(annotations)
+    getBM(mart = ensembl,
+          attributes = c("ensembl_gene_id",
+                         "ensembl_transcript_id",
+                         "external_gene_name",
+                         "gene_biotype",
+                         "chromosome_name")) %>%
+        as_tibble %>%
+        group_by(!!sym("ensembl_gene_id")) %>%
+        arrange(!!sym("ensembl_transcript_id"), .by_group = TRUE) %>%
+        mutate(broad_class = case_when(
+            tolower(.data$chromosome_name) == "mt" ~ "mito",
+            # Fix to match Drosophila genome (non-standard)
+            grepl("mito", .data$chromosome_name) ~ "mito",
+            grepl("pseudo", .data$gene_biotype) ~ "pseudo",
+            grepl("TR_", .data$gene_biotype) ~ "TCR",
+            grepl("IG_", .data$gene_biotype) ~ "IG",
+            .data$gene_biotype %in% srna ~ "small",
+            .data$gene_biotype %in% decaying ~ "decaying",
+            .data$gene_biotype %in% noncoding ~ "noncoding",
+            .data$gene_biotype %in% coding ~ "coding",
+            TRUE ~ "other"))
 }
