0841a59fa744d4eadbeedb124418a08094414682
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-19 13:47:33 -0400

    Offload common setup chunk options to file

---
+++ docs/downloads/setup.R
@@ -0,0 +1,25 @@
+# ggplot2 ====
+library(ggplot2)
+theme_set(theme_light(base_size = 14))
+
+# knitr ====
+library(knitr)
+opts_chunk$set(
+    autodep = TRUE,
+    cache = TRUE,
+    cache.lazy = TRUE,
+    error = TRUE,
+    fig.align = "center",
+    fig.height = 8,
+    fig.keep = "all",
+    fig.path = "figures/",
+    fig.retina = 2,
+    fig.width = 8,
+    highlight = TRUE,
+    message = FALSE,
+    prompt = TRUE,
+    tidy = TRUE,
+    warning = FALSE)
+
+# bcbioRnaseq ====
+library(bcbioRnaseq)
