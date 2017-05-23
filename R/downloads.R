55e6106df2e2408b7692c687344ace074a70fbcd
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-23 12:03:40 -0400

    Set up manual file requests from HBC website (e.g. custom scripts)

--- R/downloads.R
+++ R/downloads.R
@@ -5,34 +5,32 @@
 #'
 #' File download utility functions for RMarkdown knit reports.
 #'
-#' @rdname downloads
-#' @keywords internal
 #' @author Michael Steinbaugh
 #'
-#' @param file File name.
+#' @param file File name. If `NULL` (default), download the default dependency
+#'   files for a consult.
+#'
+#' @export
+downloads <- function(file = NULL) {
+    dl <- function(file) {
+        sapply(seq_along(file), function(a) {
+            if (!file.exists(file[a])) {
+                download.file(
+                    file.path("http://bioinformatics.sph.harvard.edu",
+                              "bcbioRnaseq", "downloads", file[a]),
+                    destfile = file[a])
+            }
+        }) %>% invisible
+    }
 
-
-
-#' @rdname downloads
-hbc_download <- function(file) {
-    sapply(seq_along(file), function(a) {
-        if (!file.exists(file[a])) {
-            download.file(
-                file.path("http://bioinformatics.sph.harvard.edu",
-                          "bcbioRnaseq", "downloads", file[a]),
-                destfile = file[a])
-        }
-    }) %>% invisible
-}
-
-
-
-#' @rdname downloads
-downloads <- function() {
-    hbc_download(
-        c("_output.yaml",
-          "bcbioRnaseq.bib",
-          "footer.Rmd",
-          "header.Rmd",
-          "setup.R"))
+    if (!is.null(file)) {
+        dl(file)
+    } else {
+        # HBC project defaults
+        dl(c("_output.yaml",
+             "bcbioRnaseq.bib",
+             "footer.Rmd",
+             "header.Rmd",
+             "setup.R"))
+    }
 }
