98f453f7cb4caeba6ab04dc9ad77a1816d3fdb4f
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-15 06:53:46 -0400

    Draft migration to using S4 as default

--- R/all_classes.R
+++ R/all_classes.R
@@ -14,7 +14,7 @@
 #' @export
 bcbioRnaDataSet <- setClass(
     "bcbioRnaDataSet", contains = "SummarizedExperiment",
-    representation = representation(callers="SimpleList"))
+    representation = representation(callers = "SimpleList"))
 setValidity("bcbioRnaDataSet", function(object) TRUE)
 
 
@@ -27,7 +27,7 @@ setValidity("bcbioRnaDataSet", function(object) TRUE)
         } else if (is(se, "SummarizedExperiment")) {
             # Only to help transition from SummarizedExperiment to new
             # RangedSummarizedExperiment objects, remove once transition is
-            # complete
+            # complete.
             se <- as(se, "SummarizedExperiment")
         } else {
             stop("'se' must be a SummarizedExperiment object")
@@ -38,7 +38,7 @@ setValidity("bcbioRnaDataSet", function(object) TRUE)
     metadata(bcb)[["txi"]] <- NULL
     # [! Fix] copy real data to new pointer so we can remove it from here
     # run$txi <- NULL
-    if (run$analysis == "srnaseq"){
+    if (run$analysis == "srnaseq") {
         bcb <- read_smallrna_counts(bcb)
     }
     bcb
@@ -46,8 +46,34 @@ setValidity("bcbioRnaDataSet", function(object) TRUE)
 
 
 
-#' @rdname load_run
-#' @keywords internal
+#' Load bcbio-nextgen run
+#'
+#' Simply point to the final upload directory output by
+#' [bcbio-nextgen](https://bcbio-nextgen.readthedocs.io/), and this function
+#' will take care of the rest. It automatically imports RNA-seq counts,
+#' metadata, and program versions used.
+#'
+#' When working in RStudio, we recommend connecting to the bcbio-nextgen run
+#' directory as a remote connection over
+#' [sshfs](https://github.com/osxfuse/osxfuse/wiki/SSHFS).
+#'
+#' @author Michael Steinbaugh
+#' @author Lorena Patano
+#'
+#' @param upload_dir Path to final upload directory. This path is set when
+#'   running `bcbio_nextgen -w template`.
+#' @param analysis Analysis type (e.g. `rnaseq` or `srnaseq`).
+#' @param intgroup Character vector of interesting groups. First entry is used
+#'   for plot colors during quality control (QC) analysis. Entire vector is used
+#'   for PCA and heatmap QC functions.
+#' @param metadata_file Custom metadata file to import. Otherwise defaults to
+#'   sample metadata saved in the YAML file.
+#' @param organism Organism name, following Ensembl/Biomart conventions. Must be
+#'   lowercase and one word (e.g. hsapiens). This will be detected automatically
+#'   for common reference genomes and normally does not need to be set.
+#' @param ... Additional optional parameters passed to [SummarizedExperiment].
+#'
+#' @return [bcbioRnaDataSet] containing counts and metadata.
 #' @export
 load_run <- function(
     upload_dir = "final",
@@ -56,15 +82,73 @@ load_run <- function(
     metadata_file = NULL,
     organism = NULL,
     ...) {
-    run <- load_run(
+    run <- load_run_as_list(
         upload_dir,
-        analysis,
+        analysis = analysis,
         intgroup = intgroup,
-        metadata_file = metadata_file,
         organism = organism)
+
+    # colData
+    message("Processing sample metadata (colData)...")
+    if (!is.null(metadata_file)) {
+        colData <- read_metadata(metadata_file, lanes = run$lanes)
+        run$metadata_file <- metadata_file
+    } else {
+        colData <- read_bcbio_metadata(run)
+    }
+    colData <- DataFrame(colData)
+
+    # Subset the sample_dirs by the metadata data frame
+    run$sample_dirs <- run$sample_dirs[colData$description]
+    message(paste(length(run$sample_dirs), "samples matched by metadata"))
+
+    if (analysis == "rnaseq") {
+        # Metrics ----
+        message("Reading metrics...")
+        run$metrics <- read_bcbio_metrics(run)
+
+        # tx2gene ----
+        # Use the tx2gene file output by `bcbio-nextgen`. Alternatively,
+        # we can handle this directly in R using the Ensembl annotations
+        # obtained with biomaRt instead, if the file doesn't exist. This
+        # is currently the case with the fast RNA-seq pipeline, for example.
+        message("Processing tx2gene annotations...")
+        run$tx2gene <- read_bcbio_file(
+            run, file = "tx2gene.csv", col_names = FALSE)
+        if (is.null(run$tx2gene)) {
+            run$tx2gene <- tx2gene(run)
+        }
+
+        # Ensembl annotations ----
+        message("Downloading gene annotations from Ensembl (rowData)...")
+        run$ensembl_version <- listMarts() %>% .[1, 2]
+        ensembl <- ensembl(run) %>% gene_level_annotations
+    }
+
+    # Read counts ----
+    # Lightweight-aligned counts with tximport
+    if (analysis %in% c("rnaseq", "srnaseq")) {
+        # [fix] Method for loading aligned accounts (e.g. STAR)? If we add
+        # support for this, we don't want to slot into txi.
+        run$txi <- read_bcbio_counts(run)
+    } else {
+        stop("Unsupported analysis method")
+    }
+
+    # rowData ----
+    # Here we need to subset the Ensembl annotations to match the genes in the
+    # count matrix data
+    run$txi$counts %>% nrow
+    ensembl %>% nrow
+    # This fails...
+    all(rownames(run$txi$counts) %in% rownames(ensembl))
+    # How to expand to match??
+
     se <- SummarizedExperiment(
         assays = SimpleList(counts = run$txi$counts),
-        colData = DataFrame(run$metadata),
-        metadata = run)
+        colData = colData,
+        # rowData = rowData,
+        metadata = run,
+        ...)
     .bcbioRnaDataSet(se, run)
 }
