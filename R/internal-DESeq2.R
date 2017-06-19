a60369cadc5d1d8a8a63b44af5671e7c0170f80c
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-19 08:10:58 -0400

    Consolidate internal DESeq2 operations

---
+++ R/internal-DESeq2.R
@@ -0,0 +1,38 @@
+# Class constructor ====
+# [TODO] This needs a complete rework...
+# No longer pulling data from run list object
+#' @rdname internal
+#' Get contrast name from [DESeqResults]
+#'
+#' @rdname res_contrast_name
+#' @keywords internal
+#' @author Michael Steinbaugh
+#'
+#' @param res [DESeqResults].
+#'
+#' @return Contrast name string.
+.res_contrast_name <- function(res) {
+    mcols(res)[2, 2] %>%
+        str_replace("^.*:\\s", "")
+}
+
+
+
+#' [DESeq] [colData]
+#'
+#' Selects `groups_of_interest`-defined interesting groups automatically.
+#'
+#' @rdname select_groups_of_interest
+#' @keywords internal
+#'
+#' @author Michael Steinbaugh
+#'
+#' @param dds [DESeqDataSet] or [DESeqTransform].
+#' @param groups_of_interest Groups of interest.
+#'
+#' @return [colData()] data frame.
+.select_groups_of_interest <- function(dds, groups_of_interest) {
+    colData(dds) %>%
+        as.data.frame %>%
+        tidy_select(!!quo(groups_of_interest))
+}
