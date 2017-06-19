1e324ff1ca0aeccae8136c3de4773520224b49c2
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-19 08:10:17 -0400

    Break out generics

---
+++ R/all_generics.R
@@ -0,0 +1,14 @@
+#' @rdname bcbio
+#' @export
+setGeneric("bcbio", function(object, ...) standardGeneric("bcbio"))
+
+
+
+#' @rdname bcbio
+#' @export
+setGeneric("bcbio<-", function(object, ..., value) standardGeneric("bcbio<-"))
+
+
+
+#' @rdname bcbcols
+setGeneric("bcbcols", function(object) standardGeneric("bcbcols"))
