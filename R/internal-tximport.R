de6bb64d7d27b7164aaad9a2485454fa2441823f
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-19 07:04:15 -0400

    Update tximport

---
+++ R/internal-tximport.R
@@ -0,0 +1,65 @@
+#' Import RNA-seq counts
+#'
+#' Import RNA-seq counts using [tximport()]. Currently supports
+#' [salmon](https://combine-lab.github.io/salmon/) (**recommended**) and
+#' [sailfish](http://www.cs.cmu.edu/~ckingsf/software/sailfish/).
+#'
+#' @rdname tximport
+#' @keywords internal
+#'
+#' @author Michael Steinbaugh
+#' @author Rory Kirchner
+#'
+#' @param sample_dirs Sample directories to import.
+#' @param tx2gene Transcript to gene annotations.
+#'
+#' @return Counts saved in [tximport] list object.
+#'
+#' @seealso
+#' - [tximport::tximport()].
+.tximport <- function(sample_dirs, tx2gene) {
+    # Check for count output format, by using the first sample directory
+    subdirs <- list.dirs(sample_dirs[1], full.names = FALSE, recursive = FALSE)
+    if ("salmon" %in% subdirs) {
+        type <- "salmon"
+    } else if ("sailfish" %in% subdirs) {
+        type <- "sailfish"
+    } else {
+        stop("Unsupported counts output format")
+    }
+
+    # Locate `quant.sf` file for salmon or sailfish output
+    if (type %in% c("salmon", "sailfish")) {
+        sample_files <- list.files(
+            file.path(sample_dirs, type),
+            pattern = "quant.sf",
+            full.names = TRUE,
+            recursive = TRUE)
+    }
+
+    # Assign descriptions to sample files
+    names(sample_files) <- names(sample_dirs)
+
+    # Begin loading of selected counts
+    message(paste("Reading", type, "counts"))
+    names(sample_files) %>% toString %>% message
+
+    # Coerce tx2gene to data frame
+    tx2gene <- as.data.frame(tx2gene)
+
+    # Import the counts
+    # https://goo.gl/h6fm15
+    # countsFromAbundance = c("no", "scaledTPM", "lengthScaledTPM")
+    if (type %in% c("salmon", "sailfish")) {
+        # Use `lengthScaledTPM` for salmon and sailfish
+        countsFromAbundance <- "lengthScaledTPM"
+    } else {
+        countsFromAbundance <- "no"
+    }
+
+    tximport(files = sample_files,
+             type = type,
+             tx2gene = tx2gene,
+             importer = read_tsv,
+             countsFromAbundance = countsFromAbundance)
+}
