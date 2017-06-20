13f3347f52fe6a6c61359cc531940a12eb9e7268
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-20 14:28:59 -0400

    Updated QC plots to use S4 method

--- R/plot-qc.R
+++ R/plot-qc.R
@@ -1,8 +1,3 @@
-# [fix] Fast RNA-seq pipeline isn't outputting metrics. Does MultiQC 1.0 add
-# support for lightweight/pseudoaligned counts?
-
-
-
 #' Gene-level quality control plots
 #'
 #' @rdname qc_plots
@@ -18,34 +13,35 @@
 #'   [DESeqDataSet] using [counts()] with `normalized = FALSE`.
 #' @param pass_limit Threshold to plot pass color marker.
 #' @param warn_limit Threshold to plot warning color marker.
-#' @param groups_of_interest Category to use to group samples (color and shape). Default: `NULL`
+#' @param groups_of_interest (*Optional*). Category to use to group samples
+#'   (color and shape). If unset, this is automatically determined by
+#'   the metadata set inside the [bcbioRnaDataSet].
 #'
 #' @return [ggplot].
 #' @export
 plot_total_reads <- function(
     bcb,
-    pass_limit = 20,
-    warn_limit = 10,
+    pass_limit = 20L,
+    warn_limit = 10L,
     groups_of_interest = NULL) {
-    run <- metadata(bcb)
-    if (is.null(run$metrics)) {
-        return(NULL)
+    metrics <- .metrics(bcb)
+    if (is.null(metrics)) return(NULL)
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
     }
-    if(!is.null(groups_of_interest)){
-        groups_of_interest=as.name(groups_of_interest)
-    }
-    run$metrics %>%
-        mutate(total_reads = as.numeric(.data$total_reads)) %>%
-        ggplot(aes_(x = ~description,
-                    y = ~total_reads / 1e6,
-                    fill = groups_of_interest)) +
+    groups_of_interest <- as.name(groups_of_interest)
+    ggplot(
+        metrics,
+        aes_(x = ~description,
+             y = ~total_reads / 1e6,
+             fill = groups_of_interest)) +
         ggtitle("total reads") +
         geom_bar(stat = "identity") +
         geom_hline(color = warn_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = warn_limit) +
         geom_hline(color = pass_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = pass_limit) +
         labs(x = "sample",
              y = "total reads (million)",
@@ -59,28 +55,26 @@ plot_total_reads <- function(
 #' @export
 plot_mapped_reads <- function(
     bcb,
-    pass_limit = 20,
-    warn_limit = 10,
+    pass_limit = 20L,
+    warn_limit = 10L,
     groups_of_interest = NULL) {
-    run <- metadata(bcb)
-    if (is.null(run$metrics)) {
-        return(NULL)
+    metrics <- .metrics(bcb)
+    if (is.null(metrics)) return(NULL)
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
     }
-    if(!is.null(groups_of_interest)){
-        groups_of_interest=as.name(groups_of_interest)
-    }
-    run$metrics %>%
-        mutate(mapped_reads = as.numeric(.data$mapped_reads)) %>%
-        ggplot(aes_(x = ~description,
-                    y = ~mapped_reads / 1e6,
-                    fill = groups_of_interest)) +
+    ggplot(
+        metrics,
+        aes_(x = ~description,
+             y = ~mapped_reads / 1e6,
+             fill = groups_of_interest)) +
         ggtitle("mapped reads") +
         geom_bar(stat = "identity") +
         geom_hline(color = warn_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = warn_limit) +
         geom_hline(color = pass_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = pass_limit) +
         labs(x = "sample",
              y = "mapped reads (million)",
@@ -97,31 +91,28 @@ plot_mapping_rate <- function(
     pass_limit = 90,
     warn_limit = 70,
     groups_of_interest = NULL) {
-    run <- metadata(bcb)
-    if (is.null(run$metrics)) {
-        return(NULL)
+    metrics <- .metrics(bcb)
+    if (is.null(metrics)) return(NULL)
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
     }
-    if(!is.null(groups_of_interest)){
-        groups_of_interest=as.name(groups_of_interest)
-    }
-    run$metrics %>%
-        mutate(mapped_reads = as.numeric(.data$mapped_reads),
-               total_reads = as.numeric(.data$total_reads)) %>%
-        ggplot(aes_(x = ~description,
-                    y = ~mapped_reads / total_reads * 100,
-                    fill = groups_of_interest)) +
+    ggplot(
+        metrics,
+        aes_(x = ~description,
+             y = ~mapped_reads / total_reads * 100L,
+             fill = groups_of_interest)) +
         ggtitle("mapping rate") +
         geom_bar(stat = "identity") +
         geom_hline(color = warn_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = warn_limit) +
         geom_hline(color = pass_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = pass_limit) +
         labs(x = "sample",
              y = "mapping rate (%)",
              fill = "") +
-        ylim(0, 100) +
+        ylim(0L, 100L) +
         coord_flip()
 }
 
@@ -131,24 +122,23 @@ plot_mapping_rate <- function(
 #' @export
 plot_genes_detected <- function(
     bcb,
-    raw_counts,
     pass_limit = 20000,
     groups_of_interest = NULL) {
-    run <- metadata(bcb)
-    if (is.null(run$metrics)) {
-        return(NULL)
+    metrics <- .metrics(bcb)
+    counts <- assay(bcb)
+    if (is.null(metrics)) return(NULL)
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
     }
-    if(!is.null(groups_of_interest)){
-        groups_of_interest=as.name(groups_of_interest)
-    }
-    run$metrics %>%
-        ggplot(aes_(x = ~description,
-                    y = colSums(raw_counts > 0),
-                    fill = groups_of_interest)) +
+    groups_of_interest <- as.name(groups_of_interest)
+    ggplot(metrics,
+           aes_(x = ~description,
+                y = colSums(counts > 0L),
+                fill = groups_of_interest)) +
         ggtitle("genes detected") +
         geom_bar(stat = "identity") +
         geom_hline(color = pass_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = pass_limit) +
         labs(x = "sample",
              y = "gene count",
@@ -162,23 +152,21 @@ plot_genes_detected <- function(
 #' @export
 plot_gene_detection_saturation <- function(
     bcb,
-    raw_counts,
     groups_of_interest = NULL) {
-    run <- metadata(bcb)
-    if (is.null(run$metrics)) {
-        return(NULL)
+    metrics <- .metrics(bcb)
+    counts <- assay(bcb)
+    if (is.null(metrics)) return(NULL)
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
     }
-    if(!is.null(groups_of_interest)){
-        groups_of_interest=as.name(groups_of_interest)
-    }
-    run$metrics %>%
-        mutate(mapped_reads = as.numeric(.data$mapped_reads)) %>%
-        ggplot(aes_(x = ~mapped_reads / 1e6,
-                    y = ~colSums(raw_counts > 0),
-                    color = groups_of_interest,
-                    shape = groups_of_interest)) +
+    groups_of_interest <- as.name(groups_of_interest)
+    ggplot(
+        metrics,
+        aes_(x = ~mapped_reads / 1e6,
+             y = colSums(counts > 0L),
+             color = groups_of_interest)) +
         ggtitle("gene detection saturation") +
-        geom_point(size = 3) +
+        geom_point(size = 3L) +
         geom_smooth(method = "lm", se = FALSE) +
         labs(x = "mapped reads (million)",
              y = "gene count",
@@ -192,29 +180,27 @@ plot_gene_detection_saturation <- function(
 #' @export
 plot_exonic_mapping_rate <- function(
     bcb,
-    pass_limit = 60,
+    pass_limit = 60L,
     groups_of_interest = NULL) {
-    run <- metadata(bcb)
-    if (is.null(run$metrics)) {
-        return(NULL)
+    metrics <- .metrics(bcb)
+    if (is.null(metrics)) return(NULL)
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
     }
-    if(!is.null(groups_of_interest)){
-        groups_of_interest=as.name(groups_of_interest)
-    }
-    run$metrics %>%
-        mutate(exonic_rate = as.numeric(.data$exonic_rate)) %>%
-        ggplot(aes_(x = ~description,
-                    y = ~exonic_rate * 100,
-                    fill = groups_of_interest)) +
+    groups_of_interest <- as.name(groups_of_interest)
+    ggplot(metrics,
+           aes_(x = ~description,
+                y = ~exonic_rate * 100L,
+                fill = groups_of_interest)) +
         ggtitle("exonic mapping rate") +
         geom_bar(stat = "identity") +
         geom_hline(color = pass_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = pass_limit) +
         labs(x = "sample",
              y = "exonic mapping rate (%)",
              fill = "") +
-        ylim(0, 100) +
+        ylim(0L, 100L) +
         coord_flip()
 }
 
@@ -224,29 +210,28 @@ plot_exonic_mapping_rate <- function(
 #' @export
 plot_intronic_mapping_rate <- function(
     bcb,
-    warn_limit = 20,
+    warn_limit = 20L,
     groups_of_interest = NULL) {
-    run <- metadata(bcb)
-    if (is.null(run$metrics)) {
-        return(NULL)
+    metrics <- .metrics(bcb)
+    if (is.null(metrics)) return(NULL)
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
     }
-    if(!is.null(groups_of_interest)){
-        groups_of_interest=as.name(groups_of_interest)
-    }
-    run$metrics %>%
-        mutate(intronic_rate = as.numeric(.data$intronic_rate)) %>%
-        ggplot(aes_(x = ~description,
-                    y = ~intronic_rate * 100,
-                    fill = groups_of_interest)) +
+    groups_of_interest <- as.name(groups_of_interest)
+    ggplot(
+        metrics,
+        aes_(x = ~description,
+             y = ~intronic_rate * 100L,
+             fill = groups_of_interest)) +
         ggtitle("intronic mapping rate") +
         geom_bar(stat = "identity") +
         geom_hline(color = warn_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = warn_limit) +
         labs(x = "sample",
              y = "intronic mapping rate (%)",
              fill = "") +
-        ylim(0, 100) +
+        ylim(0L, 100L) +
         coord_flip()
 }
 
@@ -256,24 +241,23 @@ plot_intronic_mapping_rate <- function(
 #' @export
 plot_rrna_mapping_rate <- function(
     bcb,
-    warn_limit = 10,
+    warn_limit = 10L,
     groups_of_interest = NULL) {
-    run <- metadata(bcb)
-    if (is.null(run$metrics)) {
-        return(NULL)
+    metrics <- .metrics(bcb)
+    if (is.null(metrics)) return(NULL)
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
     }
-    if(!is.null(groups_of_interest)){
-        groups_of_interest=as.name(groups_of_interest)
-    }
-    run$metrics %>%
-        mutate(r_rna_rate = as.numeric(.data$r_rna_rate) * 100) %>%
-        ggplot(aes_(x = ~description,
-                    y = ~r_rna_rate,
-                    fill = groups_of_interest)) +
+    groups_of_interest <- as.name(groups_of_interest)
+    ggplot(
+        metrics,
+        aes_(x = ~description,
+             y = ~r_rna_rate * 100L,
+             fill = groups_of_interest)) +
         ggtitle("rRNA mapping rate") +
         geom_bar(stat = "identity") +
         geom_hline(color = warn_color,
-                   size = 2,
+                   size = 2L,
                    yintercept = warn_limit) +
         labs(x = "sample",
              y = "rRNA mapping rate (%)",
@@ -287,29 +271,30 @@ plot_rrna_mapping_rate <- function(
 #' @export
 plot_counts_per_gene <- function(
     bcb,
-    normalized_counts,
+    normalized_counts = NULL,
     groups_of_interest = NULL) {
-    run <- metadata(bcb)
-    name <- deparse(substitute(normalized_counts))
-    melted <- melt_log10(run, normalized_counts)
-
-    if(!is.null(groups_of_interest)){
-        melted$color=melted[[groups_of_interest]]
-    }else{
-        melted$color="ALL"
+    if (is.null(normalized_counts)) {
+        # [TODO] Use tpm accessor
+        normalized_counts <- assays(bcb)[["abundance"]]
+        name <- "tpm"
+    } else {
+        name <- deparse(substitute(normalized_counts))
     }
-
-    data.frame(x = melted$description,
-               y = melted$counts,
-               color = melted$color) %>%
-        ggplot(aes_(x = ~x,
-                    y = ~y,
-                    color = ~color)) +
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
+    }
+    melted <- melt_log10(bcb, normalized_counts)
+    ggplot(
+        melted,
+        aes_(x = ~description,
+             y = ~counts,
+             color = as.name(groups_of_interest))) +
         ggtitle(paste("counts per gene", name, sep = label_sep)) +
         geom_boxplot(outlier.shape = NA) +
         # optional way to make log10 subscript:
         # expression(log[10]~counts~per~gene)
         labs(x = "sample",
+             # [TODO] Fix axis label?
              y = "log10 counts per gene",
              color = groups_of_interest) +
         coord_flip()
@@ -320,14 +305,25 @@ plot_counts_per_gene <- function(
 #' @export
 plot_count_density <- function(
     bcb,
-    normalized_counts) {
-    run <- metadata(bcb)
-    name <- deparse(substitute(normalized_counts))
-    melt_log10(run, normalized_counts) %>%
-        ggplot(aes_(x = ~counts,
-                    group = ~description)) +
+    normalized_counts = NULL) {
+    if (is.null(normalized_counts)) {
+        # [TODO] Use tpm accessor
+        normalized_counts <- assays(bcb)[["abundance"]]
+        name <- "tpm"
+    } else {
+        name <- deparse(substitute(normalized_counts))
+    }
+    if (is.null(groups_of_interest)) {
+        groups_of_interest <- metadata(bcb)[["groups_of_interest"]]
+    }
+    melted <- melt_log10(bcb, normalized_counts)
+    ggplot(
+        melted,
+        aes_(x = ~counts,
+             group = ~description)) +
         ggtitle(paste("count density", name, sep = label_sep)) +
         geom_density() +
+        # [TODO] Fix axis label?
         labs(x = "log10 counts per gene",
              y = "density")
 }
@@ -336,10 +332,9 @@ plot_count_density <- function(
 
 #' @rdname qc_plots
 #' @export
-plot_gender_markers <- function(bcb, normalized_counts) {
-    run <- metadata(bcb)
-    name <- deparse(substitute(normalized_counts))
-    organism <- run$organism
+plot_gender_markers <- function(bcb, normalized_counts = NULL) {
+    # Organism-specific dimorphic markers ----
+    organism <- metadata(bcb)[["organism"]]
 
     # Load the relevant internal gender markers data
     if (organism == "mmusculus") {
@@ -351,12 +346,24 @@ plot_gender_markers <- function(bcb, normalized_counts) {
 
     # Prepare the source gender markers data frame
     gender_markers <- gender_markers %>%
-        filter(.data$include == TRUE) %>%
+        filter(.data[["include"]] == TRUE) %>%
         arrange(!!!syms(c("chromosome", "gene_symbol")))
 
     # Ensembl identifiers
     identifier <- gender_markers$ensembl_gene %>% sort %>% unique
 
+
+    # Normalized counts ----
+    if (is.null(normalized_counts)) {
+        # [TODO] Use tpm accessor
+        normalized_counts <- assays(bcb)[["abundance"]]
+        name <- "transcripts per million (tpm)"
+    } else {
+        name <- deparse(substitute(normalized_counts))
+    }
+
+
+    # ggplot ----
     normalized_counts[identifier, ] %>%
         as.data.frame %>%
         rownames_to_column %>%
@@ -371,14 +378,12 @@ plot_gender_markers <- function(bcb, normalized_counts) {
             aes_(x = ~gene_symbol,
                  y = ~counts,
                  color = ~description,
-                 shape = ~chromosome)
-        ) +
+                 shape = ~chromosome)) +
         ggtitle("gender markers") +
         geom_jitter(size = 4) +
         expand_limits(y = 0) +
         labs(x = "gene",
-             y = name) +
-        theme(legend.position = "none")
+             y = name)
 }
 
 
@@ -389,34 +394,39 @@ plot_gender_markers <- function(bcb, normalized_counts) {
 #'
 #' @param bcb [bcbioRnaDataSet].
 #' @param dt [DESeqTransform]. [rlog()]-transformed counts are recommended.
-#' @param use character vector. List of columns to use in degCovariates
+#' @param use Character vector. List of columns to use in degCovariates.
 #' @param ... Passthrough parmeters to [DEGreport::degCovariates()].
 #'
 #' @export
-plot_pca_covariates <- function(bcb, dt, use=NULL, ...) {
-    run <- metadata(bcb)
-    # [fix] Not working for some consults?
-    if (is.null(run$metrics)) {
+plot_pca_covariates <- function(bcb, dt, use = NULL, ...) {
+    # [TODO] Switch to S4 `metrics()` accessor
+    metrics <- .metrics(bcb)
+    if (is.null(metrics)) {
         return(NULL)
     }
-    if (is.null(use)){
-        use <- colnames(run$metrics)
+
+    if (is.null(use)) {
+        use <- colnames(metrics)
     } else {
-        use <- intersect(use, colnames(run$metrics))
+        use <- intersect(use, colnames(metrics))
     }
-    if (length(use) == 0)
+
+    if (length(use) == 0L) {
         stop("Not columns matched between use and metadata")
+    }
 
     keep_metrics <- lapply(use, function(a) {
-        if (length(unique(run$metrics[, a])) > 1) { a }
+        if (length(unique(metrics[, a])) > 1L) a
     }) %>% unlist %>% .[!is.null(.)]
-    metrics <- run$metrics %>%
+
+    metrics <- metrics %>%
         as.data.frame %>%
         set_rownames(.$description) %>%
         .[, setdiff(keep_metrics, c("description", "file_name"))]
+
     dt %>%
         assay %>%
         degCovariates(metadata = metrics, ...) %>%
-        .$plot +
-        theme(axis.text.x = element_text(angle = 60, hjust = 1))
+        .[["plot"]] +
+        theme(axis.text.x = element_text(angle = 60L, hjust = 1L))
 }
