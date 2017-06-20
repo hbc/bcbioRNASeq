a1d82f042e4092a2d05158f49777579156768aac
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-20 14:56:22 -0400

    Restructure generics and methods

--- R/all_generics.R
+++ R/all_generics.R
@@ -1,14 +1,67 @@
+# bcbio ====
+# [TODO] Too vague
+#' Accessors for the count matrix of a [bcbioRnaDataSet] object
+#'
+#' This method will be used to access all different count matrix from
+#' the object. Gene expression, transcript expression, miRNA expression...
+#'
 #' @rdname bcbio
-#' @export
+#' @name bcbio
+#' @docType methods
+#'
+#' @aliases bcbio bcbio,bcbioRnaDataSet-method
+#'   bcbio<-,bcbioRnaDataSet,matrix-method
+#'
+#' @param object [bcbioRnaDataSet] object.
+#' @param value An integer matrix or other object.
+#' @param type type of count data to retrieve
+#' @param ... Matrix count data.
+#'
+#' @return Matrix/Object containing count data.
 setGeneric("bcbio", function(object, ...) standardGeneric("bcbio"))
 
-
-
 #' @rdname bcbio
+#' @name "bcbio<-"
 #' @export
 setGeneric("bcbio<-", function(object, ..., value) standardGeneric("bcbio<-"))
 
 
 
-#' @rdname bcbcols
-setGeneric("bcbcols", function(object) standardGeneric("bcbcols"))
+# raw_counts ====
+#' Raw counts
+#'
+#' @rdname raw_counts
+#' @name raw_counts
+#' @export
+setGeneric("raw_counts", function(object) standardGeneric("raw_counts"))
+
+
+
+# sample_dirs ====
+#' Sample directories of [bcbioRnaDataSet] object.
+#'
+#' This method will be used to access folders where sample information is kept.
+#'
+#' @rdname sample_dirs
+#' @docType methods
+#' @name bcbcols
+#' @aliases bcbcols bcbcols,bcbioRnaDataSet
+#'
+#' @param object [bcbioRnaDataSet] object.
+#'
+#' @return Folders where samples are kept.
+#' @export
+#' @rdname sample_dirs
+#' @name sample_dirs
+#' @export
+setGeneric("sample_dirs", function(object) standardGeneric("sample_dirs"))
+
+
+
+# tpm ====
+#' Transcripts per million (TPM)
+#'
+#' @rdname tpm
+#' @name tpm
+#' @export
+setGeneric("tpm", function(object) standardGeneric("tpm"))
--- R/all_methods.R
+++ R/all_methods.R
@@ -1,76 +1,52 @@
-# Count matrix accessors =======================================================
-#' Accessors for the count matrix of a [bcbioRnaDataSet] object
-#'
-#' This method will be used to access all different count matrix from
-#' the object. Gene expression, transcript expression, miRNA expression...
-#'
-#' @rdname bcbio
-#' @docType methods
-#' @name bcbio
-#' @aliases bcbio bcbio,bcbioRnaDataSet-method
-#'   bcbio<-,bcbioRnaDataSet,matrix-method
-#'
-#' @param object [bcbioRnaDataSet] object.
-#' @param value An integer matrix or other object.
-#' @param type type of count data to retrieve
-#' @param ... Matrix count data.
-#'
-#' @return Matrix/Object containing count data.
-NULL
-
-
-
+# bcbio ====
 #' @rdname bcbio
 #' @export
-bcbio.bcbioRnaDataSet <- function(object, type = "counts") {  # nolint
-    if (type == "counts")
-        return(assays(object)[["counts"]])
-    if (type %in% names(slot(object, "callers")))
-        return(slot(object, "callers")[[type]])
-    message(type, " not found.")
-}
+setMethod("bcbio", "bcbioRnaDataSet", function(object, type = "counts") {
+    if (!type %in% c("counts", "abundance", "length", "alt_counts")) {
+        stop("Unsupported type")
+    }
+    assays(object)[[type]]
+})
 
-#' @rdname bcbio
-#' @export
-setMethod("bcbio", signature(object = "bcbioRnaDataSet"), bcbio.bcbioRnaDataSet)
-
+# [TODO] Modified to just work on assays (simpler). Otherwise we can define
+# our own slots, which may not be necessary.
 #' @rdname bcbio
 #' @exportMethod "bcbio<-"
 setReplaceMethod(
-    "bcbio", signature(object = "bcbioRnaDataSet", value = "ANY"),
+    "bcbio",
+    signature(object = "bcbioRnaDataSet", value = "matrix"),  # ANY
     function(object, type = "counts", value) {
-        if (type == "counts") {
-            assays(object)[["counts"]] <- value
-            validObject(object)
-        } else {
-            slot(object, "callers")[[type]] <- value
+        if (!type %in% c("counts", "abundance", "length", "alt_counts")) {
+            stop("Unsupported type")
         }
+        assays(object)[[type]] <- value
+        validObject(object)
         object
     })
 
 
 
-
-
-
-# Sample accessors =============================================================
-bcbio.samples <- function(object){  # nolint
-    metadata(object)[["sample_dirs"]]
-}
-
-
-
-#' Accessors samples dir of a [bcbioRnaDataSet] object.
-#'
-#' This method will be used to access folders where sample information is kept.
-#'
-#' @rdname bcbcols
-#' @docType methods
-#' @name bcbcols
-#' @aliases bcbcols bcbcols,bcbioRnaDataSet
-#'
-#' @param object [bcbioRnaDataSet] object.
-#'
-#' @return Folders where samples are kept.
+# raw_counts ====
+#' @rdname raw_counts
 #' @export
-setMethod("bcbcols", signature(object = "bcbioRnaDataSet"), bcbio.samples)
+setMethod("raw_counts", "bcbioRnaDataSet", function(object) {
+    assays(object)[["counts"]]
+})
+
+
+
+# sample_dirs ====
+#' @rdname sample_dirs
+#' @export
+setMethod("sample_dirs", "bcbioRnaDataSet", function(object) {
+    metadata(object)[["sample_dirs"]]
+})
+
+
+
+# tpm ====
+#' @rdname tpm
+#' @export
+setMethod("tpm", "bcbioRnaDataSet", function(object) {
+    assays(object)[["abundance"]]
+})
