c41f6c4fa0c292f99885a1260bbbc17784717014
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-27 07:35:16 -0400

    Increase spacing to improve readability

--- R/all_generics.R
+++ R/all_generics.R
@@ -4,21 +4,31 @@ setGeneric("aggregate_replicates", function(object, ...) {
     standardGeneric("aggregate_replicates")
 })
 
+
+
 #' @rdname bcbio
 #' @export
 setGeneric("bcbio", function(object, ...) standardGeneric("bcbio"))
+
+
 
 #' @rdname bcbio
 #' @export
 setGeneric("bcbio<-", function(object, ..., value) standardGeneric("bcbio<-"))
 
+
+
 #' @rdname gene2symbol
 #' @export
 setGeneric("gene2symbol", function(object) standardGeneric("gene2symbol"))
 
+
+
 #' @rdname melt_log10
 #' @export
 setGeneric("melt_log10", function(object, ...) standardGeneric("melt_log10"))
+
+
 
 #' @rdname metadata_table
 #' @export
@@ -26,17 +36,25 @@ setGeneric("metadata_table", function(object, ...) {
     standardGeneric("metadata_table")
 })
 
+
+
 #' @rdname metrics
 #' @export
 setGeneric("metrics", function(object) standardGeneric("metrics"))
+
+
 
 #' @rdname sample_dirs
 #' @export
 setGeneric("sample_dirs", function(object) standardGeneric("sample_dirs"))
 
+
+
 #' @rdname tmm
 #' @export
 setGeneric("tmm", function(object) standardGeneric("tmm"))
+
+
 
 #' @rdname tpm
 #' @export
