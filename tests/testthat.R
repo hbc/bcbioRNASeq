329c6dcae0fc21b74639f5178a98d2e8b4b7effb
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-09 09:51:13 -0400

    Set up test that with example data

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,12 @@
 library(testthat)
 library(bcbioRnaseq)
 
+data(bcb)
+dds <- DESeqDataSetFromTximport(
+    txi = txi(bcb),
+    colData = colData(bcb),
+    design = formula(~group)) %>%
+    DESeq
+res <- results(dds)
+
 test_check("bcbioRnaseq")
--- tests/testthat/test_lintr.R
+++ tests/testthat/test_lintr.R
@@ -1,6 +1,6 @@
 if (requireNamespace("lintr", quietly = TRUE)) {
     context("lints")
-    test_that("Package Style", {
+    test_that("Package style", {
         lintr::expect_lint_free()
     })
 }
