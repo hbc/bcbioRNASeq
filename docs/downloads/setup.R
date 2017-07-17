befde2fe6f3fdc43a21a81ef3f9cf1998eb7be2b
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-17 15:43:17 -0400

    Simplify setup.R file

--- docs/downloads/setup.R
+++ docs/downloads/setup.R
@@ -1,18 +1,13 @@
-# bcbioRnaseq ====
-library(bcbioRnaseq)
-output_dir <- getwd()  # where to save all output
-data_dir <- file.path(output_dir, "data")
-count_dir <- file.path(output_dir, "results", "counts")
-res_dir <- file.path(output_dir, "results", "de")
-
-# knitr ====
 library(knitr)
+library(ggplot2)
+library(bcbioRnaseq)
+
 opts_chunk$set(
     autodep = TRUE,
     bootstrap.show.code = FALSE,
     cache = TRUE,
     cache.lazy = TRUE,
-    dev = c("png", "pdf", "svg"),
+    dev = c("png", "pdf"),
     error = TRUE,
     fig.height = 8,
     fig.retina = 2,
@@ -23,6 +18,20 @@ opts_chunk$set(
     tidy = TRUE,
     warning = FALSE)
 
-# ggplot2 ====
-library(ggplot2)
 theme_set(theme_light(base_size = 14))
+
+# Directory paths
+output_dir <- getwd()
+data_dir <- file.path(output_dir, "data")
+counts_dir <- file.path(output_dir, "results", "counts")
+results_dir <- file.path(output_dir, "results", "de")
+
+# bcbioRnaDataSet
+if (file.exists(file.path(data_dir, "bcb.rda"))) {
+    load(file.path(data_dir, "bcb.rda"))
+} else {
+    bcb <- load_run(
+        upload_dir = file.path("data", "final"),
+        interesting_groups = c("genotype", "treatment"))
+    save_data(bcb, dir = data_dir)
+}
