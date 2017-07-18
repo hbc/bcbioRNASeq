d67423160457df9cf7b8a9a314fa3d734c39ed5e
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-18 12:47:02 -0400

    Finish adding plot generics

--- R/all_generics.R
+++ R/all_generics.R
@@ -58,8 +58,104 @@ setGeneric("metrics", function(object) {
 
 #' @rdname all_generics
 #' @export
+setGeneric("plot_correlation_heatmap", function(object, ...) {
+    standardGeneric("plot_correlation_heatmap")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_deg_heatmap", function(object, counts, ...) {
+    standardGeneric("plot_deg_heatmap")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_dispersion", function(object) {
+    standardGeneric("plot_dispersion")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_gender_markers", function(object, ...) {
+    standardGeneric("plot_gender_markers")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_gene", function(object, keys, ...) {
+    standardGeneric("plot_gene")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_gene_heatmap", function(object, counts, ...) {
+    standardGeneric("plot_gene_heatmap")
+})
+
+
+
+#' @rdname all_generics
+#' @export
 setGeneric("plot_ma", function(object, ...) {
     standardGeneric("plot_ma")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_mean_sd", function(object, ...) {
+    standardGeneric("plot_mean_sd")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_mirna_counts", function(object, ...) {
+    standardGeneric("plot_mirna_counts")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_pattern", function(object, ...) {
+    standardGeneric("plot_pattern")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_pca", function(object, ...) {
+    standardGeneric("plot_pca")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_pca_covariates", function(object, ...) {
+    standardGeneric("plot_pca_covariates")
+})
+
+
+
+#' @rdname all_generics
+#' @export
+setGeneric("plot_size_distribution", function(object, ...) {
+    standardGeneric("plot_size_distribution")
 })
 
 
