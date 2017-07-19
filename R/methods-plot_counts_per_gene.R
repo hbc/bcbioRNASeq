0226853bfb3904418968cec84d0e16bfdbc0a7f5
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-19 15:40:59 -0400

    S4 method update

---
+++ R/methods-plot_counts_per_gene.R
@@ -0,0 +1,38 @@
+#' @rdname plot_metrics
+#' @family Quality Control Metrics Plots
+#' @author Michael Steinbaugh, Rory Kirchner, Victor Barrera
+#' @inherit plot_metrics
+#'
+#' @examples
+#' data(bcb)
+#' plot_counts_per_gene(bcb)
+.plot_counts_per_gene <- function(
+    melted,
+    interesting_group,
+    flip = TRUE) {
+    p <- ggplot(melted,
+                aes_(x = ~sample_name,
+                     y = ~counts,
+                     color = as.name(interesting_group))) +
+        geom_boxplot(outlier.shape = NA) +
+        labs(title = "counts per gene",
+             x = "sample",
+             y = "log10 counts per gene",
+             color = as.name(interesting_group))
+    if (isTRUE(flip)) {
+        p <- p + coord_flip()
+    }
+    p
+}
+
+
+
+#' @rdname plot_counts_per_gene
+#' @export
+setMethod("plot_counts_per_gene", "bcbioRNADataSet", function(
+    object, normalized = "tmm", ...) {
+    .plot_counts_per_gene(
+        melted = melt_log10(object, normalized = normalized),
+        interesting_group = .interesting_group(object),
+        ...)
+})
