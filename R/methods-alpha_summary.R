f3562b3c1852486bbc85718826fe105bd1d9adde
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-20 12:53:08 -0400

    Migrate to S4

---
+++ R/methods-alpha_summary.R
@@ -0,0 +1,48 @@
+#' Print Summary Statistics of Alpha Level Cutoffs
+#'
+#' @rdname alpha_summary
+#' @author Michael Steinbaugh, Lorena Patano
+#'
+#' @param alpha Numeric vector of desired alpha cutoffs.
+#' @param contrast Character vector to use with [results] function.
+#'
+#' @return [kable].
+#' @export
+#'
+#' @examples
+#' data(bcb)
+#' dds <- DESeqDataSetFromTximport(
+#'     txi = txi(bcb),
+#'     colData = colData(bcb),
+#'     design = formula(~group)) %>%
+#'     DESeq
+#' alpha_summary(dds)
+setMethod("alpha_summary", "DESeqDataSet", function(
+    object,
+    alpha = c(0.1, 0.05, 0.01, 1e-3, 1e-6),
+    contrast = NULL) {
+    if (is.null(contrast))
+        contrast <- strsplit(resultsNames(object)[[2L]], "_") %>%
+            unlist %>%
+            .[c(1L, 2L, 4L)]
+    lapply(seq_along(alpha), function(a) {
+        .info <- capture.output(
+            results(object, contrast = contrast, alpha = alpha[a]) %>%
+                summary)[4L:8L]
+        .parse <- sapply(
+            .info, function(i) {
+                gsub(".*:", "", i)
+            })[1L:4L]
+        .parse <- c(.parse, .info[[5L]])
+        data.frame(alpha = as.vector(.parse))
+    }) %>%
+        bind_cols %>%
+        set_colnames(alpha) %>%
+        set_rownames(c("LFC > 0 (up)",
+                       "LFC < 0 (down)",
+                       "outliers",
+                       "low counts",
+                       "cutoff")) %>%
+        kable %>%
+        show
+})
