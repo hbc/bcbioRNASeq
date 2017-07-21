476c58d702e7df512e89c951ea5701769954196e
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-21 08:36:29 -0400

    Draft update -- broken code

--- R/methods-plot_pca_covariates.R
+++ R/methods-plot_pca_covariates.R
@@ -1,3 +1,10 @@
+# FIXME Rename the `use` argument, it's too vague
+# FIXME Function is returning this error:
+# Error in colnames(COVAR_data)[sapply(COVAR_data, is.factor)] :
+# invalid subscript type 'list'
+
+
+
 #' Find Correlation Between Principal Components (PCs) and Covariates
 #'
 #' [DEGreport::degCovariates()] wrapper supporting a [bcbioRNADataSet].
@@ -5,12 +12,17 @@
 #' @rdname plot_pca_covariates
 #' @author Lorena Pantano, Michael Steinbaugh
 #'
-#' @param transform String specifying [rlog] (**recommended**) or [vst]
-#'   [DESeqTransform] slotted inside the [bcbioRNADataSet].
+#' @param transform String specifying [DESeqTransform] slotted inside the
+#'   [bcbioRNADataSet]:
+#'   - `rlog` (**recommended**).
+#'   - `vst`: variance stabilizing transformation.
 #' @param use *Optional*. Character vector of columns to use.
-#' @param ... Passthrough arguments to [DEGreport::degCovariates()].
+#' @param ... Additional arguments, passed to [degCovariates()].
 #'
-#' @seealso [DEGreport::degCovariates()].
+#' @seealso
+#' - [DEGreport::degCovariates()].
+#' - [DESeq2::rlog()].
+#' - [DESeq2::varianceStabilizingTransformation()].
 #'
 #' @return [ggplot].
 #' @export
@@ -19,8 +31,15 @@ setMethod("plot_pca_covariates", "bcbioRNADataSet", function(
     metrics <- metrics(object)
     if (is.null(metrics)) return(NULL)
 
+    # Check for valid `transform` argument
+    transform_args <- c("rlog", "vst")
+    if (!transform %in% transform_args) {
+        stop(paste("Valid transforms:", toString(transform_args)))
+    }
+
     # Get the columns of interest
     if (is.null(use)) {
+        # FIXME We want to use `interesting_groups` by default right?
         use <- colnames(metrics)
     } else {
         use <- intersect(use, colnames(metrics))
@@ -49,6 +68,7 @@ setMethod("plot_pca_covariates", "bcbioRNADataSet", function(
         # Assay needed here to get the matrix from [DESeqTransform]
         assay %>%
         degCovariates(metadata = metrics, ...)
+    # FIXME Need to clean up the plotting
     res %>%
         .[["plot"]] +
         theme(axis.text.x = element_text(angle = 60L, hjust = 1L))
