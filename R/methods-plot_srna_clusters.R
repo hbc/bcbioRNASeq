d4f66d9c964b4aa215b9efbf10cc257ee85bdcfc
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-24 12:45:49 -0400

    S4 method support update for bcbRNADataSet, including code fixes for DESeq and pheatmap updates

--- R/methods-plot_srna_clusters.R
+++ R/methods-plot_srna_clusters.R
@@ -13,15 +13,20 @@
 #' data(bcb)
 #' plot_srna_clusters(bcb)
 setMethod("plot_srna_clusters", "bcbioRNADataSet", function(object) {
-    counts <- counts(object)
+    counts <- assay(object) %>%
+        # Require at least 3 counts
+        .[rowSums(. > 0L) > 3L, ] %>%
+        # DESeq requires integers
+        round
     dds <- DESeqDataSetFromMatrix(
-        counts[rowSums(counts > 0L) > 3L, ],
+        countData = counts,
         colData = colData(object),
         design = ~1L)
-    vst <- rlog(dds, betaPriorVar = FALSE)
+    vst_counts <- rlog(dds) %>% assay
     annotation_col <- colData(object) %>%
+        as.data.frame %>%
         .[, metadata(object)[["interesting_groups"]], drop = FALSE]
-    pheatmap(assay(vst),
+    pheatmap(vst_counts,
              annotation_col = annotation_col,
              clustering_distance_cols = "correlation",
              clustering_method = "ward.D",
