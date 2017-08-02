17fd030fe431b4b19672c6f59350a3d37640ec42
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-02 10:51:28 -0400

    Resave datasets into camelCase

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,6 +1,6 @@
 library(basejump)
-gender_markers_mmusculus <-
-    file.path("data-raw", "gender_markers_mmusculus.csv") %>%
+genderMarkersMmusculus <-
+    file.path("data-raw", "genderMarkersMmusculus.csv") %>%
     read_csv
-save_data(gender_markers_mmusculus)
-load_all()
+saveData(genderMarkersMmusculus)
+devtools::load_all()
