53d34d4469b49614d0d5101cbdb3278ba4eb979e
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-21 13:00:09 -0400

    Improve contrast name appearance for difference of differences

--- R/internal-DESeq2.R
+++ R/internal-DESeq2.R
@@ -9,5 +9,8 @@
 #' @return Contrast name string.
 .resContrastName <- function(res) {
     mcols(res)[2L, 2L] %>%
-        str_replace("^.*:\\s", "")
+        str_replace("^.*:\\s", "") %>%
+        str_replace_all("_", " ") %>%
+        # Improve appearance for difference of differences
+        str_replace_all("\\+", " \\+\n    ")
 }
