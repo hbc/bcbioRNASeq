163f3cc109b4f1ced077641d25da4778597bd6c2
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-21 09:24:36 -0400

    Add automatic title for DESeqResults

--- R/methods-plotMA.R
+++ R/methods-plotMA.R
@@ -57,9 +57,11 @@ NULL
         labs(x = "mean expression across all samples",
              y = "log2 fold change")
     if (isTRUE(title)) {
-        p <- p + ggtitle("mean average")
+        p <- p +
+            ggtitle("mean average")
     } else if (is.character(title)) {
-        p <- p + ggtitle(title)
+        p <- p +
+            ggtitle(paste("mean average:", title))
     }
     if (!is.null(labelPoints)) {
         labels <- results %>%
@@ -89,7 +91,10 @@ NULL
 # Methods ====
 #' @rdname plotMA
 #' @export
-setMethod("plotMA", "DESeqResults", .plotMA)
+setMethod("plotMA", "DESeqResults", function(object) {
+    title <- .resContrastName(object)
+    .plotMA(object, title = title)
+})
 
 
 
