34b4c58bbdb3f3d2fc0d3201ae4128e01fa69ea8
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-25 16:57:09 -0400

    Minor tweaks to fix working example

--- R/methods-alphaSummary.R
+++ R/methods-alphaSummary.R
@@ -13,12 +13,19 @@
 #' alphaSummary(dds)
 NULL
 
+
+
+# Constructors ====
 .guessResults <- function(object, what, alpha){
-    if (length(what) == 1L)
+    if (length(what) == 1L) {
         res <- results(object, name = what, alpha = alpha)
-    else res <- results(object, contrast = what, alpha = alpha)
+    } else {
+        res <- results(object, contrast = what, alpha = alpha)
+    }
     res
 }
+
+
 
 # Methods ====
 #' @rdname alphaSummary
@@ -28,20 +35,26 @@ setMethod("alphaSummary", "DESeqDataSet", function(
     alpha = c(0.1, 0.05, 0.01, 1e-3, 1e-6),
     contrast = NULL,
     caption = NULL) {
-    if (is.null(contrast))
+    if (is.null(contrast)) {
         contrast <- resultsNames(object)[[2L]]
-    if (is.null(caption))
+    }
+    if (is.null(caption)) {
         caption <- contrast
+    }
     lapply(seq_along(alpha), function(a) {
-        .info <- capture.output(
-            .guessResults(object, contrast, alpha[a]) %>%
-                summary)[4L:8L]
-        .parse <- sapply(
-            .info, function(i) {
-                gsub(".*:", "", i)
-            })[1L:4L]
-        .parse <- c(.parse, .info[[5L]])
-        data.frame(alpha = as.vector(.parse))
+        info <- capture.output(
+            summary(.guessResults(object, contrast, alpha[a]))
+        ) %>%
+            # Get the lines of interest from summary
+            .[4L:8L]
+        parse <- info[1L:5L] %>%
+            # Extract the values after the colon in summary
+            sapply(function(a) {
+                gsub("^.+\\:\\s(.+)\\s$", "\\1", a)
+            }) %>%
+            # Coerce to character here to remove names
+            as.character
+        data.frame(alpha = parse)
     }) %>%
         bind_cols %>%
         set_colnames(alpha) %>%
@@ -50,6 +63,6 @@ setMethod("alphaSummary", "DESeqDataSet", function(
                        "outliers",
                        "low counts",
                        "cutoff")) %>%
-        kable(., caption = paste(caption)) %>%
+        kable(caption = paste(caption)) %>%
         show
 })
