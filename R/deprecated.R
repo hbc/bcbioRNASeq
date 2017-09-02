eca592a42478700f9d7ec7d4caadebccde933c2a
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-02 08:34:23 -0400

    Add `plotGeneDetectionSaturation()`

--- R/deprecated.R
+++ R/deprecated.R
@@ -9,9 +9,17 @@ NULL
 
 
 
+# 0.0.25 ====
 #' @rdname deprecated
 #' @export
 download <- function(...) {
     .Deprecated("prepareRNASeqTemplate")
     prepareRNASeqTemplate(...)
+}
+
+#' @rdname deprecated
+#' @export
+plotGeneDetectionSaturation <- function(...) {
+    .Deprecated("plotGeneSaturation")
+    plotGeneSaturation(...)
 }
