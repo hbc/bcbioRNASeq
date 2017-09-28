95a51d652ba6727e7468a6dc8eaa9cec5980dfec
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-28 10:06:40 -0400

    Use `sampleMetadata()` instead of `metadataTable()`

---
+++ R/methods-sampleMetadata.R
@@ -0,0 +1,45 @@
+#' Sample Metadata
+#'
+#' Returns the sample metadata.
+#'
+#' @rdname sampleMetadata
+#' @name sampleMetadata
+#'
+#' @inheritParams AllGenerics
+#'
+#' @return [data.frame].
+#'
+#' @examples
+#' data(bcb, dds, rld)
+#'
+#' # bcbioRNASeq
+#' sampleMetadata(bcb)
+#'
+#' # DESeqDataSet
+#' sampleMetadata(dds)
+#'
+#' # DESeqTransform
+#' sampleMetadata(dds)
+NULL
+
+
+
+# Constructors ====
+.sampleMetadata <- function(object, ...) {
+    object %>%
+        colData() %>%
+        as.data.frame()
+}
+
+
+
+# Methods ====
+#' @rdname sampleMetadata
+#' @export
+setMethod("sampleMetadata", "bcbioRNASeqANY", .sampleMetadata)
+
+
+
+#' @rdname sampleMetadata
+#' @export
+setMethod("sampleMetadata", "DESeqANY", .sampleMetadata)
