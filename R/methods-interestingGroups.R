0d3958b8ab1373e47fb53036dab2f1823b7b2658
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-10 10:31:49 -0400

    Use a constructor function

--- R/methods-interestingGroups.R
+++ R/methods-interestingGroups.R
@@ -6,10 +6,21 @@
 #' @inheritParams AllGenerics
 #'
 #' @return Character vector.
+NULL
+
+
+
+# Constructors ====
+.interestingGroups <- function(object) {
+    metadata(object)[["interestingGroups"]]
+}
+
+
+
+# Methods ====
+#' @rdname interestingGroups
 #' @export
 setMethod(
     "interestingGroups",
     signature = "bcbioRNASeqANY",
-    definition = function(object) {
-        metadata(object)[["interestingGroups"]]
-    })
+    definition = .interestingGroups)
