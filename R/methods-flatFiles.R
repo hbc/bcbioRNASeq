9d5087b2c96fee48ae8e6d8e0ffd3f031a33be9d
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-11 10:55:48 -0400

    Add support for `flatFiles()` per Rory's suggestion

---
+++ R/methods-flatFiles.R
@@ -0,0 +1,29 @@
+#' Get Flat Files from S4 Object
+#'
+#' Prepare an unstructured list of information saved in the S4 object,
+#' for improved archival data storage.
+#'
+#' @rdname flatFiles
+#' @name flatFiles
+#' @author Michael Steinbaugh, Rory Kirchner
+#'
+#' @return [list].
+NULL
+
+
+
+# Methods ====
+#' @rdname flatFiles
+#' @export
+setMethod(
+    "flatFiles",
+    signature("bcbioRNASeqANY"),
+    function(object) {
+        list(
+            assays = assays(object),
+            rowData = rowData(object),
+            colData = colData(object),
+            metadata = metadata(object),
+            bcbio = bcbio(object)
+        )
+    })
