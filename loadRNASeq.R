41a46a46292856da13f1628017d84bf88e53f22a
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-19 14:32:51 -0400

    Add script used to save bcb

---
+++ loadRNASeq.R
@@ -0,0 +1,16 @@
+# GSE65267 example dataset
+# Run this script on O2
+library(bcbioRNASeq)
+bcb <- loadRNASeq(
+    uploadDir = file.path(
+        "/n",
+        "scratch2",
+        "hsph_bioinformatic_core",
+        "lp113",
+        "workflow",
+        "samples-merged",
+        "final"),
+    interestingGroups = "group",
+    ensemblVersion = "current"
+)
+save(bcb, file = "bcb.rda", compress = "xz")
