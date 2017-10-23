a38a20df9340278da4d0ed4b608c11b0adef7895
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-22 20:55:13 -0400

    Add support for custom fill

--- R/methods-plotGenderMarkers.R
+++ R/methods-plotGenderMarkers.R
@@ -6,6 +6,7 @@
 #' @author Michael Steinbaugh
 #'
 #' @inheritParams AllGenerics
+#'
 #' @param organism *Optional*. Organism name. Should be detected automatically,
 #'   unless a spike-in FASTA sequence is provided containing a gene identifier
 #'   that is first alphabetically in the count matrix rownames.
@@ -14,7 +15,6 @@
 #' @return [ggplot].
 #'
 #' @examples
-#' data(bcb)
 #' plotGenderMarkers(bcb)
 NULL
 
@@ -28,17 +28,17 @@ NULL
 .plotGenderMarkers <- function(
     object,
     organism,
-    ylab = "counts") {
-    counts <- object
-
+    ylab = "counts",
+    color = viridis::scale_color_viridis(discrete = TRUE)) {
     # Load the relevant internal gender markers data
     envir <- loadNamespace("bcbioRNASeq")
-    if (organism == "Mus musculus") {
+    if (organism == "Homo sapiens") {
+        markers <- get("genderMarkersHsapiens", envir = envir)
+    } else if (organism == "Mus musculus") {
         markers <- get("genderMarkersMmusculus", envir = envir)
-    } else if (organism == "Homo sapiens") {
-        stop("Human marker support coming in future update", call. = FALSE)
     } else {
-        stop("Unsupported organism", call. = FALSE)
+        warning("Unsupported organism", call. = FALSE)
+        return(NULL)
     }
 
     # Ensembl identifiers
@@ -48,12 +48,12 @@ NULL
         sort() %>%
         unique()
 
-    if (!all(ensgene %in% rownames(counts))) {
+    if (!all(ensgene %in% rownames(object))) {
         warning("Missing gender markers in count matrix", call. = FALSE)
         return(NULL)
     }
 
-    counts %>%
+    counts <- object %>%
         .[ensgene, , drop = FALSE] %>%
         # This will coerce rownames to a column named `rowname`. We will rename
         # this to `ensgene` after melting the counts.
@@ -62,55 +62,74 @@ NULL
         # `setNames()`. If you don't set `id`, function will output a message.
         melt(id = 1) %>%
         setNames(c("ensgene", "sampleName", "counts")) %>%
-        left_join(markers, by = "ensgene") %>%
-        ggplot(
-            mapping = aes_string(
-                x = "symbol",
-                y = "counts",
-                color = "sampleName",
-                shape = "chromosome")
-        ) +
+        left_join(markers, by = "ensgene")
+    p <- ggplot(
+        counts,
+        mapping = aes_string(
+            x = "symbol",
+            y = "counts",
+            color = "sampleName",
+            shape = "chromosome")
+    ) +
         geom_jitter(size = 4) +
         expand_limits(y = 0) +
         labs(title = "gender markers",
              x = "gene",
-             y = ylab) +
-        scale_color_viridis(discrete = TRUE)
+             y = ylab)
+    if (!is.null(color)) {
+        p <- p + color
+    }
+    p
 }
 
 
 
 # Methods ====
 #' @rdname plotGenderMarkers
+#' @importFrom S4Vectors metadata
+#' @importFrom viridis scale_color_viridis
 #' @export
 setMethod(
     "plotGenderMarkers",
-    signature("bcbioRNASeqANY"),
-    function(object) {
+    signature("bcbioRNASeq"),
+    function(
+        object,
+        color = viridis::scale_color_viridis(discrete = TRUE)) {
         counts <- tpm(object)
         organism <- metadata(object)[["organism"]]
         ylab <- "transcripts per million (tpm)"
-        .plotGenderMarkers(counts, organism = organism, ylab = ylab)
+        .plotGenderMarkers(
+            counts,
+            organism = organism,
+            ylab = ylab,
+            color = color)
     })
 
 
 
 #' @rdname plotGenderMarkers
+#' @importFrom basejump detectOrganism
+#' @importFrom viridis scale_color_viridis
 #' @export
 setMethod(
     "plotGenderMarkers",
     signature("DESeqDataSet"),
     function(
         object,
-        organism = NULL) {
+        organism,
+        color = viridis::scale_color_viridis(discrete = TRUE)) {
         counts <- counts(object, normalized = TRUE)
-        if (is.null(organism)) {
+        if (is.missing(organism)) {
             organism <- rownames(counts) %>%
                 .[[1]] %>%
                 detectOrganism()
         }
         ylab <- "normalized counts"
-        .plotGenderMarkers(counts, organism = organism, ylab = ylab)
+        .plotGenderMarkers(
+            counts,
+            organism = organism,
+            ylab = ylab,
+            color = color)
     })
 
 
