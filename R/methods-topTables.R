3f8e5a9fc11e5ae4ec77aa158bc62ffe36612c39
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-22 22:35:30 -0400

    Improve spacing and documentation

--- R/methods-topTables.R
+++ R/methods-topTables.R
@@ -11,7 +11,6 @@
 #' @return Top table kables, for knit report.
 #'
 #' @examples
-#' data(res)
 #' resTbl <- resultsTables(res, write = FALSE)
 #' topTables(resTbl)
 NULL
@@ -20,7 +19,7 @@ NULL
 
 # Constructors ====
 #' @importFrom basejump fixNA
-#' @importFrom dplyr mutate rename
+#' @importFrom dplyr filter mutate rename
 #' @importFrom S4Vectors head
 #' @importFrom tibble remove_rownames
 .subsetTop <- function(df, n, coding) {
@@ -30,23 +29,19 @@ NULL
     }
     df <- df %>%
         head(n = n) %>%
-        dplyr::rename(lfc = .data[["log2FoldChange"]]) %>%
+        rename(lfc = .data[["log2FoldChange"]]) %>%
         mutate(
             baseMean = round(.data[["baseMean"]]),
             lfc = format(.data[["lfc"]], digits = 3),
-            padj = format(.data[["padj"]],
-                          digits = 3,
-                          scientific = TRUE),
+            padj = format(.data[["padj"]], digits = 3, scientific = TRUE),
             # Remove symbol information in description, if present
-            description = gsub(x = .data[["description"]],
-                               pattern = " \\[.+\\]$",
-                               replacement = "")) %>%
-        .[, c("ensgene",
-              "baseMean",
-              "lfc",
-              "padj",
-              "symbol",
-              "description")] %>%
+            description = gsub(
+                x = .data[["description"]],
+                pattern = " \\[.+\\]$",
+                replacement = "")
+        ) %>%
+        .[, c("ensgene", "baseMean", "lfc",
+              "padj", "symbol", "description")] %>%
         remove_rownames() %>%
         fixNA()
 }
@@ -66,14 +61,13 @@ setMethod(
         coding = FALSE) {
         up <- .subsetTop(object[["degLFCUp"]], n = n, coding = coding)
         down <- .subsetTop(object[["degLFCDown"]], n = n, coding = coding)
-
-        # Captions
         contrastName <- object[["contrast"]]
-
         show(kable(
             up,
-            caption = paste(contrastName, "(upregulated)")))
+            caption = paste(contrastName, "(upregulated)")
+        ))
         show(kable(
             down,
-            caption = paste(contrastName, "(downregulated)")))
+            caption = paste(contrastName, "(downregulated)")
+        ))
     })
