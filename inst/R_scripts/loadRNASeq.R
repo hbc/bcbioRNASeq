15e4f2b91d1839476273109e20eaa08939add736
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-24 11:59:47 -0400

    Save flatFiles at the loading step, not in the QC template

--- inst/R_scripts/loadRNASeq.R
+++ inst/R_scripts/loadRNASeq.R
@@ -2,4 +2,6 @@ library(bcbioRNASeq)
 bcb <- loadRNASeq(
     file.path("bcbio_rnaseq_run", "final"),
     interestingGroups = c("genotype", "treatment"))
-saveData(bcb)
+# Back up all data inside bcbioRNASeq object
+flatFiles <- flatFiles(bcb)
+saveData(bcb, flatFiles)
