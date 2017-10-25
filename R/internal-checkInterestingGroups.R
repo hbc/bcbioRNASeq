dab3a829af848a3095c6932df85bf38d09918b36
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-25 09:27:41 -0400

    Add `warnOnNULL` parameter

--- R/internal-checkInterestingGroups.R
+++ R/internal-checkInterestingGroups.R
@@ -9,18 +9,28 @@
 #'
 #' @param object Object supporting [colnames()], typically a [data.frame].
 #' @param interestingGroups Interesting groups character vector.
+#' @param warnOnNULL Warn the user on `NULL` argument.
 #'
 #' @return Valid character of defined interesting groups. Stop on failure.
-.checkInterestingGroups <- function(object, interestingGroups) {
+.checkInterestingGroups <- function(
+    object,
+    interestingGroups,
+    warnOnNULL = FALSE) {
     # Check that interesting groups are present in DESeqTransform colData
     if(!all(interestingGroups %in% colnames(object))) {
         stop(paste(
-            "Missing interesting groups:",
+            "Interesting group(s) not defined in metadata:",
             toString(setdiff(interestingGroups, colnames(object)))
         ), call. = FALSE)
     }
     # Default to `sampleName` if `NULL`
     if (is.null(interestingGroups)) {
+        if (isTRUE(warnOnNULL)) {
+            warning(paste(
+                "'interestingGroups' is 'NULL'.",
+                "Defaulting to 'sampleName'."
+            ), call. = FALSE)
+        }
         interestingGroups <- "sampleName"
     }
     interestingGroups
