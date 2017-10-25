1be577d781ea1c2799308b6621a1e1226a2280e0
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-25 11:23:35 -0400

    Fix `value` documentation for `interestingGroup` assignment

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -7,6 +7,7 @@
 #' @param object Object.
 #' @param x Object.
 #' @param i An integer or numeric scalar.
+#' @param value Value to assign.
 #' @param withDimnames A `logical`, indicating whether dimnames should be
 #'   applied to extracted assay elements.
 #' @param ... *Additional arguments (for the S4 generic definition).*
