7620648481675604c59c12c501b8310c2bceb452
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-27 05:57:11 -0400

    Rename test

---
+++ tests/testthat/test_plotMA.R
@@ -0,0 +1,18 @@
+context("plotMA")
+
+test_that("plotMA", {
+    p <- plotMA(res)
+
+    # Check geom classes
+    geomtype <- sapply(p[["layers"]], function(x) class(x[["geom"]])[[1L]])
+    expect_identical(
+        geomtype, c("GeomPoint", "GeomLogticks"))
+
+    # Check plot labels
+    expect_identical(
+        p[["labels"]][["y"]],
+        "log2 fold change")
+    expect_identical(
+        p[["labels"]][["x"]],
+        "mean expression across all samples")
+})
