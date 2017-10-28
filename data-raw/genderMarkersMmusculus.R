a53457640b52a885b5a405ad651a78775bff9ca6
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-28 18:18:17 -0400

    Add support for Homo sapiens

--- data-raw/genderMarkersMmusculus.R
+++
@@ -1,6 +0,0 @@
-library(devtools)
-load_all()
-genderMarkersMmusculus <-
-    file.path("data-raw", "genderMarkersMmusculus.csv") %>%
-    read_csv
-use_data(genderMarkersMmusculus, compress = "xz", overwrite = TRUE)
