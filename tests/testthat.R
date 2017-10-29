5c583025f93de6206750b91a22a05666d108e14f
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-29 07:56:53 -0400

    Use GSE65267 data for all unit tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,17 @@ set.seed(42L)
 
 library(testthat)
 library(bcbioRNASeq)
-data(bcb, dds, res)
+
+# Use F1000 workflow example data (GSE65267) for unit tests
+# https://github.com/hbc/bcbioRNASeq/tree/f1000v1
+file.path(
+    "https://github.com",
+    "hbc",
+    "bcbioRNASeq",
+    "raw",
+    "f1000v1",
+    "data",
+    "bcb.rda") %>%
+    loadRemoteData(quiet = TRUE)
 
 test_check("bcbioRNASeq")
