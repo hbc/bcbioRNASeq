82be09a2b551d5e720fb9d2311cf36aaceed1f93
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-29 13:11:42 -0400

    Initial commit

---
+++ tests/testthat/test_deprecated.R
@@ -0,0 +1,30 @@
+context("Deprecated Functions")
+
+test_that("download", {
+    expect_warning(
+        download(),
+        "Use 'prepareRNASeqTemplate' instead."
+    )
+})
+
+test_that("loadRNASeqRun", {
+    uploadDir <- system.file("extdata/bcbio", package = "bcbioRNASeq")
+    expect_warning(
+        loadRNASeqRun(uploadDir),
+        "Use 'loadRNASeq' instead."
+    )
+})
+
+test_that("plotGeneDetectionSaturation", {
+    expect_warning(
+        plotGeneDetectionSaturation(bcb),
+        "Use 'plotGeneSaturation' instead."
+    )
+})
+
+test_that("plotDispersion", {
+    expect_warning(
+        plotDispersion(bcb),
+        "Use 'plotDispEsts' instead."
+    )
+})
