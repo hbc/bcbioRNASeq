2b99bcbc31da266019920eab05d8bcd5ab66dd6b
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-15 15:35:30 -0500

    Add message

--- R/loadRNASeq.R
+++ R/loadRNASeq.R
@@ -174,6 +174,7 @@ loadRNASeq <- function(
     # Note that sample metrics used for QC plots are not currently generated
     # when using fast RNA-seq workflow. This depends upon MultiQC and aligned
     # counts generated with STAR.
+    message("Reading sample metrics")
     metrics <- sampleYAMLMetrics(yaml)
 
     # bcbio-nextgen run information ====
