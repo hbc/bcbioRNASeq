c179b81e0fd3a29a4ca7d52015c47fc4d81effdb
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-01 13:18:05 -0500

    Improve reexport definitions

--- R/reexports.R
+++ R/reexports.R
@@ -1,17 +1,23 @@
 #' @importFrom ggplot2 ggplot
-#' @importFrom viridis inferno scale_color_viridis scale_fill_viridis viridis
-
 #' @export
 ggplot2::ggplot
 
+#' @importFrom tibble glimpse
+#' @export
+tibble::glimpse
+
+#' @importFrom viridis inferno
 #' @export
 viridis::inferno
 
+#' @importFrom viridis scale_color_viridis
 #' @export
 viridis::scale_color_viridis
 
+#' @importFrom viridis scale_fill_viridis
 #' @export
 viridis::scale_fill_viridis
 
+#' @importFrom viridis viridis
 #' @export
 viridis::viridis
