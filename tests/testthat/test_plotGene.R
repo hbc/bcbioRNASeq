c5b89d78fd7d1b427c47719a079e83b5db4e4937
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-08 11:51:31 -0500

    Update unit test

--- tests/testthat/test_plotGene.R
+++ tests/testthat/test_plotGene.R
@@ -1,26 +1,27 @@
 context("plotGene")
 
-bcb <- examples[["bcb"]]
-symbol <- rowData(bcb)[["symbol"]][1:3]
+load(system.file(
+    file.path("inst", "extdata", "bcb.rda"),
+    package = "bcbioRNASeq"))
+
 ensgene <- rowData(bcb)[["ensgene"]][1:3]
-
-# Check for Rgs20, Rab23, Ncoa2 inside the ggplot objects
-
-test_that("Gene symbol", {
-    p <- plotGene(bcb, gene = symbol, format = "symbol")
-    expect_is(p, "ggplot")
-})
+symbol <- rowData(bcb)[["symbol"]][1:3]
 
 test_that("Ensembl gene identifier", {
-    p <- plotGene(bcb, gene = ensgene, format = "ensgene")
+    p <- plotGene(bcb, genes = ensgene, format = "ensgene")
     expect_is(p, "ggplot")
 })
 
-test_that("plotlist return", {
+test_that("Gene symbol", {
+    p <- plotGene(bcb, genes = symbol, format = "symbol")
+    expect_is(p, "ggplot")
+})
+
+test_that("plotlist", {
     list <- plotGene(
         bcb,
-        gene = symbol,
-        format = "symbol",
+        genes = ensgene,
+        format = "ensgene",
         returnList = TRUE)
     expect_is(list, "list")
     expect_true(
