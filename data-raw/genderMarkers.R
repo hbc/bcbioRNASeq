70af6402bb1920529db91f1d222f6fa4f36f2c99
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-14 15:29:57 -0500

    Resave in camel case

--- data-raw/genderMarkers.R
+++ data-raw/genderMarkers.R
@@ -1,11 +1,9 @@
-library(devtools)
-library(magrittr)
-library(readxl)
-load_all()
+devtools::load_all()
+library(tidyverse)
 file <- file.path("data-raw", "genderMarkers.xlsx")
 sheets <- excel_sheets(file)
 genderMarkers <- lapply(seq_along(sheets), function(a) {
     read_excel(file, sheet = sheets[[a]])
-}) %>%
-    set_names(sheets)
-use_data(genderMarkers, compress = "xz", overwrite = TRUE)
+})
+names(genderMarkers) <- camel(sheets)
+devtools::use_data(genderMarkers, compress = "xz", overwrite = TRUE)
