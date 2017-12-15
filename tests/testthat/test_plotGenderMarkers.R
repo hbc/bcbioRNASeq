e428104cc9dbe1e663b1478f32030ebd74ddb702
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-15 11:21:12 -0500

    Update unit test to match example bcb, instead of using F1000 dataset

--- tests/testthat/test_plotGenderMarkers.R
+++ tests/testthat/test_plotGenderMarkers.R
@@ -1,15 +1,15 @@
 context("plotGenderMarkers")
 
-# Use F1000 workflow example data (GSE65267) for unit tests
-# https://github.com/hbc/bcbioRNASeq/tree/f1000v1
-loadRemoteData(
-    file.path("http://bcbiornaseq.seq.cloud",
-              "f1000v1",
-              "data",
-              "bcb.rda"),
-    quiet = TRUE)
+load(system.file(
+    file.path("extdata", "bcb.rda"),
+    package = "bcbioRNASeq"))
 
 test_that("plotGenderMarkers", {
-    p <- plotGenderMarkers(bcb)
-    expect_is(p, "ggplot")
+    # Current working example doesn't contain the dimorphic genes
+    expect_warning(
+        plotGenderMarkers(bcb),
+        "Missing gender markers in count matrix"
+    )
+    p <- suppressWarnings(plotGenderMarkers(bcb))
+    expect_is(p, "NULL")
 })
