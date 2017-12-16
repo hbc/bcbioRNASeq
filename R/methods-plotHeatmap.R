8abdd68b7010fa68a1d7cb73bf36d1c4e333c9c5
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-16 09:43:01 -0500

    Offload basic `plotHeatmap()` method support to basejump, so it can be used with bcbioSingleCell

--- R/methods-plotHeatmap.R
+++ R/methods-plotHeatmap.R
@@ -1,11 +1,9 @@
 #' Plot Heatmap
 #'
-#' These functions facilitate heatmap plotting of a specified set of genes. By
-#' default, row- and column-wise hierarchical clustering is performed using the
-#' Ward method, but this behavior can be overrided by setting `cluster_rows` or
-#' `cluster_cols` to `FALSE`. When column clustering is disabled, the columns
-#' are sorted by the interesting groups (`interestingGroups`) specified in the
-#' [bcbioRNASeq] and then the sample names.
+#' @details
+#' When column clustering is disabled, the columns are sorted by the interesting
+#' groups (`interestingGroups`) specified in the [bcbioRNASeq] and then the
+#' sample names.
 #'
 #' @rdname plotHeatmap
 #' @name plotHeatmap
@@ -14,36 +12,11 @@
 #'
 #' @importFrom basejump plotHeatmap
 #'
-#' @inheritParams basejump::gene2symbol
 #' @inheritParams AllGenerics
+#' @inheritParams basejump::plotHeatmap
 #' @inheritParams counts
 #'
-#' @param genes *Optional*. Character vector of gene identifiers to plot. These
-#'   must be the stable identifiers (e.g. ENSG00000000003) used on Ensembl and
-#'   not the gene symbols.
-#' @param gene2symbol Apply gene identifier to symbol mappings. If set `TRUE`,
-#'   the function will attempt to automatically map gene identifiers to symbols
-#'   from Ensembl using [basejump::annotable()]. If set `FALSE`/`NULL`, then
-#'   gene2symbol mapping will be disabled. This is useful when working with a
-#'   poorly annotated genome. Alternatively, a gene2symbol [data.frame] can be
-#'   passed in, and must contain the columns `ensgene` and `symbol`. then the
-#'   Ensembl gene identifiers will be labeled in place of gene symbols.
-#' @param annotationCol *Optional*. [data.frame] that specifies the annotations
-#'   shown on the right side of the heatmap. Each row of this [data.frame]
-#'   defines the features of the heatmap columns.
-#' @param scale Character indicating if the values should be centered and scaled
-#'   in either the row direction or the column direction, or none. Corresponding
-#'   values are "row", "column" and "none".
-#' @param color Colors to use for plot. Defaults to the [viridis::viridis()]
-#'   palette.
-#' @param legendColor Colors to use for legend labels. Defaults to the
-#'   [viridis::viridis()] palette.
-#' @param title *Optional*. Plot title.
-#' @param ... Passthrough arguments to [pheatmap::pheatmap()].
-#'
-#' @seealso [pheatmap::pheatmap()].
-#'
-#' @return Graphical output only.
+#' @return [pheatmap::pheatmap()] ggplot return.
 #'
 #' @examples
 #' load(system.file(
@@ -78,124 +51,6 @@ NULL
 
 
 
-# Constructors =================================================================
-#' @importFrom basejump checkGene2symbol gene2symbol
-#' @importFrom dplyr mutate_all
-#' @importFrom pheatmap pheatmap
-#' @importFrom stats setNames
-#' @importFrom tibble column_to_rownames rownames_to_column
-#' @importFrom viridis viridis
-.plotHeatmap <- function(
-    object,
-    genes = NULL,
-    gene2symbol = TRUE,
-    annotationCol = NULL,
-    scale = "row",
-    color = viridis::viridis(256),
-    legendColor = viridis::viridis,
-    title = NULL,
-    quiet = FALSE,
-    ...) {
-    if (!is.matrix(object)) return(NULL)
-    counts <- object
-
-    # Check for missing genes
-    if (!is.null(genes)) {
-        if (!all(genes %in% rownames(counts))) {
-            stop(paste(
-                "Genes missing from counts matrix:",
-                toString(setdiff(genes, rownames(counts)))),
-                call. = FALSE)
-        }
-        counts <- counts %>%
-            .[rownames(.) %in% genes, , drop = FALSE]
-    } else {
-        # Remove zero counts
-        counts <- counts %>%
-            .[rowSums(.) > 0, , drop = FALSE]
-    }
-
-    if (nrow(counts) < 2) {
-        stop("Need at least 2 genes to plot heatmap", call. = FALSE)
-    }
-
-    # Convert Ensembl gene identifiers to symbol names, if necessary
-    if (nrow(counts) <= 100) {
-        showRownames <- TRUE
-    } else {
-        showRownames <- FALSE
-    }
-    if (isTRUE(showRownames)) {
-        if (isTRUE(gene2symbol)) {
-            counts <- gene2symbol(counts, quiet = quiet)
-        } else if (is.data.frame(gene2symbol)) {
-            checkGene2symbol(gene2symbol)
-            # Remap the rownames to use the gene symbols
-            remap <- gene2symbol %>%
-                .[rownames(counts), "symbol"] %>%
-                make.names(unique = TRUE)
-            rownames(counts) <- remap
-        }
-    }
-
-    # Prepare the annotation columns
-    if (!is.null(annotationCol)) {
-        # Check for colnames mismatch
-        if (!identical(rownames(annotationCol), colnames(counts))) {
-            stop(paste(
-                "Name mismatch between counts matrix colnames",
-                "and 'annotationCol' rownames."
-            ), call. = FALSE)
-        }
-        annotationCol <- annotationCol %>%
-            as.data.frame() %>%
-            rownames_to_column() %>%
-            mutate_all(factor) %>%
-            column_to_rownames()
-    }
-
-    # Define colors for each annotation column, if desired
-    if (is.data.frame(annotationCol) & is.function(legendColor)) {
-        annotationColors <- lapply(
-            seq_along(colnames(annotationCol)), function(a) {
-                col <- annotationCol[[a]] %>%
-                    levels()
-                colors <- annotationCol[[a]] %>%
-                    levels() %>%
-                    length() %>%
-                    legendColor
-                names(colors) <- col
-                colors
-            }) %>%
-            setNames(colnames(annotationCol))
-    } else {
-        annotationColors <- NULL
-    }
-
-    # pheatmap will error if `NULL` title is passed as `main`
-    if (is.null(title)) {
-        title <- ""
-    }
-
-    # If `color = NULL`, use the pheatmap default
-    if (!is.character(color)) {
-        color <- colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(100)
-    }
-
-    pheatmap(
-        mat = counts,
-        annotation_col = annotationCol,
-        annotation_colors = annotationColors,
-        border_color = NA,
-        color = color,
-        main = title,
-        scale = scale,
-        show_rownames = showRownames,
-        ...)
-}
-
-
-
 # Methods ======================================================================
 #' @rdname plotHeatmap
 #' @importFrom viridis viridis
@@ -218,7 +73,7 @@ setMethod(
         interestingGroups <- interestingGroups(object)
         annotationCol <- colData(object) %>%
             .[, interestingGroups, drop = FALSE]
-        .plotHeatmap(
+        plotHeatmap(
             object = counts,
             genes = genes,
             gene2symbol = gene2symbol,
@@ -252,7 +107,7 @@ setMethod(
         quiet = FALSE,
         ...) {
         counts <- counts(object, normalized = normalized)
-        .plotHeatmap(
+        plotHeatmap(
             object = counts,
             genes = genes,
             gene2symbol = gene2symbol,
@@ -285,7 +140,7 @@ setMethod(
         quiet = FALSE,
         ...) {
         counts <- assay(object)
-        .plotHeatmap(
+        plotHeatmap(
             object = counts,
             genes = genes,
             gene2symbol = gene2symbol,
@@ -297,13 +152,3 @@ setMethod(
             quiet = quiet,
             ...)
     })
-
-
-
-#' @rdname plotHeatmap
-#' @importFrom viridis viridis
-#' @export
-setMethod(
-    "plotHeatmap",
-    signature("matrix"),
-    .plotHeatmap)
