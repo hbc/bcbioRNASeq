bed2409f1677f68b2f43b1e755682580864610f6
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-11 15:54:05 -0500

    Update documentation

--- R/methods-plotHeatmap.R
+++ R/methods-plotHeatmap.R
@@ -14,11 +14,18 @@
 #'
 #' @inheritParams AllGenerics
 #' @inheritParams counts
-#'
 #' @inheritParams gene2symbol
 #' @inheritParams plotGene
 #'
 #' @param samples *Optional*. Samples (colnames) to plot.
+#' @param annotationCol *Optional*. [data.frame] that defines annotation
+#'   mappings for the columns.
+#' @param scale Character indicating if the values should be centered and scaled
+#'   in either the row direction or the column direction, or none. Corresponding
+#'   values are "row", "column" and "none".
+#' @param legendColor Colors to use for legend labels. Defaults to the
+#'   [viridis::viridis()].
+#' @param title Include plot title.
 #'
 #' @examples
 #' load(system.file(
