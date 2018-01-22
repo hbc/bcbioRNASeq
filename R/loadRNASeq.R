a36271f852cb86b9dd61526f350613021f71d2a4
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-22 17:20:01 -0500

    Merge with subset-fix

--- R/loadRNASeq.R
+++ R/loadRNASeq.R
@@ -69,6 +69,7 @@
 #' uploadDir <- system.file(
 #'     file.path("extdata", "bcbio"),
 #'     package = "bcbioRNASeq")
+#'
 #' bcb <- loadRNASeq(uploadDir, interestingGroups = "group")
 #' print(bcb)
 #'
