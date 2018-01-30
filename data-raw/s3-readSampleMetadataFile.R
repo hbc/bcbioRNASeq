a6995b7877b6919e817c0ce612ca83879066a855
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-30 11:54:58 -0500

    Ensure `fileName` column is present in example sample metadata

--- data-raw/s3-readSampleMetadataFile.R
+++ data-raw/s3-readSampleMetadataFile.R
@@ -3,5 +3,8 @@ uploadDir <- system.file(
     file.path("extdata", "bcbio"),
     package = "bcbioRNASeq")
 bcb <- loadRNASeq(uploadDir)
-meta <- sampleMetadata(bcb)
+meta <- sampleMetadata(bcb) %>%
+    # Add required `fileName` column
+    mutate(fileName = paste0(sampleID, ".fastq.gz")) %>%
+    select(fileName, everything())
 write_csv(meta, path = "~/Desktop/sample_metadata.csv")
