08b0ecd7ea3680deebb4298018875ecadbd672eb
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-06 17:18:15 -0500

    Offload `copyToDropbox()` functionality to bcbioBase package

--- R/methods-resultsTables.R
+++ R/methods-resultsTables.R
@@ -113,47 +113,8 @@ NULL
 
 
 
-#' @importFrom rdrop2 drop_auth drop_delete drop_exists drop_share drop_upload
-.copyToDropbox <- function(files, dir) {
-    if (!(is.character(files) || is.list(files))) {
-        abort("`files` must be a character vector or list")
-    }
-    if (!is_string(dir)) {
-        abort("`dir` must be a string")
-    }
-
-    # Check that local files exist
-    if (!all(vapply(files, file.exists, logical(1L)))) {
-        missing <- !vapply(files, file.exists, logical(1L))
-        abort(paste(
-            "Missing local files:",
-            toString(basename(files[missing]))
-        ))
-    }
-
-    # Ensure user is authenticated with Dropbox
-    drop_auth()
-
-    # Loop across the files in list
-    invisible(lapply(files, function(file) {
-        dropboxFile <- file.path(dir, basename(file))
-        # Delete file if it exists already. Otherwise `drop_share()`
-        # is currently erroring out when we try to re-share an existing
-        # file.
-        if (drop_exists(dropboxFile)) {
-            drop_delete(dropboxFile)
-        }
-        drop_upload(file = file, path = dir)
-        drop_share(
-            path = dropboxFile,
-            requested_visibility = "public")
-    }))
-}
-
-
-
-#' @importFrom bcbioBase annotable camel checkAnnotable mdHeader mdList
-#'   sanitizeAnnotable snake
+#' @importFrom bcbioBase annotable camel checkAnnotable copyToDropbox mdHeader
+#'   mdList sanitizeAnnotable snake
 #' @importFrom dplyr arrange desc left_join
 #' @importFrom readr write_csv
 #' @importFrom rlang !! sym
@@ -315,7 +276,7 @@ NULL
 
         # Copy to Dropbox (optional) ===========================================
         if (is.character(dropboxDir)) {
-            dropboxFiles <- .copyToDropbox(localFiles, dir = dropboxDir)
+            dropboxFiles <- copyToDropbox(localFiles, dir = dropboxDir)
             resTbl[["dropboxFiles"]] <- dropboxFiles
         }
 
