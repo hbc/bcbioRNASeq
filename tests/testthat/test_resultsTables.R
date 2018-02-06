b9a8f61ab4ea130e05eafc8d24fe32130afab0f1
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-06 11:21:21 -0500

    Add unit testing for `summary = TRUE` and `write = TRUE` options

--- tests/testthat/test_resultsTables.R
+++ tests/testthat/test_resultsTables.R
@@ -1,19 +1,21 @@
 context("resultsTables")
 
 load(system.file(
+    file.path("extdata", "bcb.rda"),
+    package = "bcbioRNASeq"))
+load(system.file(
     file.path("extdata", "res.rda"),
     package = "bcbioRNASeq"))
 
 test_that("resultsTables", {
-    resTbl <- resultsTables(
+    resTbl <- suppressMessages(
+        resultsTables(
         res,
         lfc = 0.25,
         summary = FALSE,
         write = FALSE)
-    expect_identical(
-        class(resTbl),
-        "list"
     )
+    expect_identical(class(resTbl), "list")
     tibble <- c("tbl_df", "tbl", "data.frame")
     expect_identical(
         lapply(resTbl, class),
@@ -26,4 +28,80 @@ test_that("resultsTables", {
              "degLFCUp" = tibble,
              "degLFCDown" = tibble)
     )
+})
+
+test_that("Summary and write support", {
+    annotable <- annotable(bcb)
+    # This is also capturing the contents of the list return. Not sure how to
+    # fix here for knitr asis_output
+    output <- capture.output(
+        resultsTables(
+        res,
+        lfc = 0.25,
+        annotable = annotable,
+        summary = TRUE,
+        headerLevel = 2L,
+        write = TRUE,
+        dir = "resultsTables"),
+        type = "output"
+    ) %>%
+        # Just evaluate the R Markdown output
+        head(24L)
+    expect_identical(
+        output,
+        c(
+            "",
+            "",
+            "## Summary statistics",
+            "",
+            "",
+            "- 505 genes in counts matrix",
+            "- Base mean > 0: 304 genes (non-zero)",
+            "- Base mean > 1: 213 genes",
+            "- Alpha cutoff: 0.1",
+            "- LFC cutoff: 0.25 (applied in tables only)",
+            "- DEG pass alpha: 4 genes",
+            "- DEG LFC up: 2 genes",
+            "- DEG LFC down: 2 genes",
+            "",
+            "",
+            "",
+            "## Results tables",
+            "",
+            "",
+            paste0(
+                "- [`group_ko_vs_ctrl_all.csv.gz`]",
+                "(resultsTables/group_ko_vs_ctrl_all.csv.gz): ",
+                "All genes, sorted by Ensembl identifier."
+            ),
+            paste0(
+                "- [`group_ko_vs_ctrl_deg.csv.gz`]",
+                "(resultsTables/group_ko_vs_ctrl_deg.csv.gz): ",
+                "Genes that pass the alpha (FDR) cutoff."
+            ),
+            paste0(
+                "- [`group_ko_vs_ctrl_deg_lfc_up.csv.gz`]",
+                "(resultsTables/group_ko_vs_ctrl_deg_lfc_up.csv.gz): ",
+                "Upregulated DEG; positive log2 fold change."
+            ),
+            paste0(
+                "- [`group_ko_vs_ctrl_deg_lfc_down.csv.gz`]",
+                "(resultsTables/group_ko_vs_ctrl_deg_lfc_down.csv.gz): ",
+                "Downregulated DEG; negative log2 fold change."
+            ),
+            ""
+        )
+    )
+    # Check for the output files
+    files <- list.files("resultsTables")
+    expect_identical(
+        files,
+        c(
+            "group_ko_vs_ctrl_all.csv.gz",
+            "group_ko_vs_ctrl_deg_lfc_down.csv.gz",
+            "group_ko_vs_ctrl_deg_lfc_up.csv.gz",
+            "group_ko_vs_ctrl_deg.csv.gz"
+        )
+    )
+    unlink("resultsTables", recursive = TRUE)
 })
