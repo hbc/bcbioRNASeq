8e4842822e750c0e08fd6a7e0717812de4ce02cb
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-14 16:45:31 -0500

    Add assertive checks

--- R/assert.R
+++ R/assert.R
@@ -4,15 +4,20 @@
 #' @name assert
 #'
 #' @importFrom assertive assert_are_identical
+#' @importFrom assertive assert_has_dimnames
+#' @importFrom assertive assert_is_a_bool
 #' @importFrom assertive assert_is_a_string
 #' @importFrom assertive assert_is_any_of
+#' @importFrom assertive assert_is_character
 #' @importFrom assertive assert_is_data.frame
 #' @importFrom assertive assert_is_matrix
 #' @importFrom assertive assert_is_numeric
 #' @importFrom assertive assert_is_scalar
 #' @importFrom assertive assert_is_subset
+#' @importFrom assertive is_a_string
 #'
 #' @importFrom basejump assert_formal_gene2symbol
+#' @importFrom basejump assert_is_a_string_or_null
 #' @importFrom basejump assert_is_gene2symbol
 #' @importFrom basejump assert_is_implicit_integer
 #'
