9c013b6c4c86b6a1c9a6e879fb160192eea7e4c7
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-15 13:02:44 -0500

    Move legacy class to separate file

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -117,25 +117,3 @@ setValidity("bcbioRNASeq", function(object) {
 
     TRUE
 })
-
-
-
-# Legacy =======================================================================
-#' bcbioRNADataSet Legacy Object Class
-#'
-#' This class has been superceded by [bcbioRNASeq] and will be formally
-#' deprecated in a future release.
-#'
-#' @author Lorena Pantano, Michael Steinbaugh
-#' @keywords internal
-#'
-#' @slot callers [SimpleList] containing additional bcbio run data with
-#'   dimensions that don't match the count matrix.
-#'
-#' @export
-bcbioRNADataSet <- setClass(
-    "bcbioRNADataSet",
-    contains = "SummarizedExperiment",
-    slots = c(callers = "SimpleList")
-)
-setValidity("bcbioRNADataSet", function(object) TRUE)
