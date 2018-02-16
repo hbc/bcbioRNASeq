d938c0e052161a0510af827ad023592464ac908a
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-16 17:18:42 -0500

    Improve unit test

--- tests/testthat/test_colData.R
+++ tests/testthat/test_colData.R
@@ -5,9 +5,9 @@ load(system.file(
     package = "bcbioRNASeq"))
 
 test_that("bcbioRNASeq", {
-    fix <- colData(bcb)
-    fix[["batch"]] <- c(1L, 2L, 1L, 2L)
-    colData(bcb) <- fix
+    data <- colData(bcb)
+    data[["batch"]] <- c(1L, 2L, 1L, 2L)
+    colData(bcb) <- data
     value <- colData(bcb)
     expect_identical(dim(value), c(4L, 5L))
     expect_true(
