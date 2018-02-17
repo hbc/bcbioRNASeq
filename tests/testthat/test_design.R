1fe928e3f1bccff56f515975a171f5845293d3bf
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-17 08:05:13 -0500

    Add code coverage

---
+++ tests/testthat/test_design.R
@@ -0,0 +1,16 @@
+context("design")
+
+load(system.file(
+    file.path("extdata", "bcb.rda"),
+    package = "bcbioRNASeq"))
+
+test_that("bcbioRNASeq", {
+    expect_equal(design(bcb), formula(~1))
+    expect_error(
+        design(bcb) <- formula(~XXX),
+        paste(
+            "invalid class \"DESeqDataSet\" object:",
+            "all variables in design formula must be columns in colData"
+        )
+    )
+})
