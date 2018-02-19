4a903c40e103e0ca9a12d2f96bbde80f537d30cb
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-19 11:11:45 -0500

    Update internal assert checks

--- R/internal-ggplot2.R
+++ R/internal-ggplot2.R
@@ -31,8 +31,8 @@ labelSep <- ": "
 # Line functions ===============================================================
 #' @importFrom ggplot2 geom_hline
 qcPassLine <- function(intercept) {
-    assert_is_numeric(intercept)
-    assert_is_scalar(intercept)
+    assert_is_a_number(intercept)
+    assert_all_are_non_negative(intercept)
     geom_hline(
         alpha = qcLineAlpha,
         color = qcPassColor,
@@ -43,8 +43,8 @@ qcPassLine <- function(intercept) {
 
 #' @importFrom ggplot2 geom_hline
 qcWarnLine <- function(intercept) {
-    assert_is_numeric(intercept)
-    assert_is_scalar(intercept)
+    assert_is_a_number(intercept)
+    assert_all_are_non_negative(intercept)
     geom_hline(
         alpha = qcLineAlpha,
         color = qcWarnColor,
