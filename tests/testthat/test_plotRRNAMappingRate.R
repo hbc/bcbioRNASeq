2ed0ef3a52c931bbdd4d388e672785d61d55cf84
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-19 15:19:54 -0500

    Add check for legacy `rRnaRate` column

--- tests/testthat/test_plotRRNAMappingRate.R
+++ tests/testthat/test_plotRRNAMappingRate.R
@@ -2,7 +2,23 @@ context("plotRRNAMappingRate")
 
 load(system.file("extdata/bcb.rda", package = "bcbioRNASeq"))
 
-test_that("plotRRNAMappingRate", {
+test_that("bcbioRNASeq", {
     p <- plotRRNAMappingRate(bcb)
     expect_is(p, "ggplot")
+})
+
+test_that("Data frame", {
+    data <- metrics(bcb)
+    p <- plotRRNAMappingRate(bcb)
+    expect_is(p, "ggplot")
+})
+
+test_that("Legacy rRnaRate column", {
+    data <- metrics(bcb)
+    data[["rRnaRate"]] <- data[["rrnaRate"]]
+    data[["rrnaRate"]] <- NULL
+    expect_warning(
+        plotRRNAMappingRate(data),
+        "`rrnaRate` is missing from `metrics\\(\\)`"
+    )
 })
