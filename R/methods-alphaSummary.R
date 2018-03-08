debabfecc0127b8c1ad795b19314d3a6d79747b8
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-08 10:02:16 -0500

    Simplify constructors and add `validObject()` checks

--- R/methods-alphaSummary.R
+++ R/methods-alphaSummary.R
@@ -1,4 +1,4 @@
-#' Print Summary Statistics of Alpha Level Cutoffs
+#' Alpha Level Cutoff Summary Statistics
 #'
 #' @note `bcbioRNASeq` class does currently support contrast definitions, since
 #'   the object contains an internal `DESeqDataSet` with an empty design
@@ -10,7 +10,6 @@
 #'
 #' @inheritParams general
 #' @inheritParams DESeq2::results
-#'
 #' @param alpha Numeric vector of desired alpha cutoffs.
 #' @param caption *Optional.* Character vector to add as caption to the table.
 #' @param ... *Optional.* Passthrough arguments to [DESeq2::results()]. Use
@@ -24,10 +23,10 @@
 #' load(system.file("extdata/bcb.rda", package = "bcbioRNASeq"))
 #' load(system.file("extdata/dds.rda", package = "bcbioRNASeq"))
 #'
-#' # bcbioRNASeq
+#' # bcbioRNASeq ====
 #' alphaSummary(bcb)
 #'
-#' # DESeqDataSet
+#' # DESeqDataSet ====
 #' resultsNames(dds)
 #' alphaSummary(dds)
 #' alphaSummary(dds, contrast = c("day", "7", "0"))
@@ -36,71 +35,71 @@ NULL
 
 
 
-# Constructors =================================================================
+# Methods ======================================================================
+#' @rdname alphaSummary
 #' @importFrom DESeq2 results
 #' @importFrom dplyr bind_cols
 #' @importFrom knitr kable
 #' @importFrom magrittr set_colnames set_rownames
 #' @importFrom utils capture.output
-.alphaSummary <- function(
-    object,
-    alpha = c(0.1, 0.05, 0.01, 1e-3, 1e-6),
-    caption = NULL,
-    ...
-) {
-    assert_is_all_of(object, "DESeqDataSet")
-    assert_is_numeric(alpha)
-    assertIsAStringOrNULL(caption)
-    dots <- list(...)
-
-    # Generate an automatic caption
-    if (is.null(caption)) {
-        if (!is.null(dots[["contrast"]])) {
-            caption <- paste(dots[["contrast"]], collapse = " ")
-        } else if (!is.null(dots[["name"]])) {
-            caption <- dots[["name"]]
-        }
-    }
-
-    # TODO Switch to mapply call
-    dflist <- lapply(seq_along(alpha), function(a) {
-        output <- capture.output(summary(
-            results(object, ..., alpha = alpha[a])
-        ))
-        # Subset the lines of interest from summary
-        output <- output[4L:8L]
-        # Extract the values after the colon in summary
-        values <- vapply(
-            X = output,
-            FUN = function(a) {
-                gsub("^.+\\:\\s(.+)\\s$", "\\1", a)
-            },
-            FUN.VALUE = "character")
-        data.frame(alpha = values)
-    })
-
-    dflist %>%
-        bind_cols() %>%
-        set_colnames(alpha) %>%
-        set_rownames(c(
-            "LFC > 0 (up)",
-            "LFC < 0 (down)",
-            "outliers",
-            "low counts",
-            "cutoff"
-        )) %>%
-        kable(caption = caption)
-}
-
-
-
-# Methods ======================================================================
-#' @rdname alphaSummary
 #' @export
 setMethod(
     "alphaSummary",
     signature("DESeqDataSet"),
-    .alphaSummary
+    function(
+        object,
+        alpha = c(0.1, 0.05, 0.01, 1e-3, 1e-6),
+        caption = NULL,
+        ...
+    ) {
+        validObject(object)
+        assert_is_numeric(alpha)
+        assertIsAStringOrNULL(caption)
+        dots <- list(...)
+
+        # Warn on empty design formula
+        if (design(object) == formula(~1)) {  # nolint
+            warn("Empty design formula detected")
+        }
+
+        # Generate an automatic caption
+        if (is.null(caption)) {
+            if (!is.null(dots[["contrast"]])) {
+                caption <- paste(dots[["contrast"]], collapse = " ")
+            } else if (!is.null(dots[["name"]])) {
+                caption <- dots[["name"]]
+            }
+        }
+
+        dflist <- lapply(alpha, function(x) {
+            output <- capture.output(summary(
+                results(object, ..., alpha = x)
+            ))
+            # Subset the lines of interest from summary
+            output <- output[4L:8L]
+            # Extract the values after the colon in summary
+            values <- vapply(
+                X = output,
+                FUN = function(x) {
+                    gsub("^.+\\:\\s(.+)\\s$", "\\1", x)
+                },
+                FUN.VALUE = "character"
+            )
+            data.frame(alpha = values)
+        })
+
+        dflist %>%
+            bind_cols() %>%
+            set_colnames(alpha) %>%
+            set_rownames(c(
+                "LFC > 0 (up)",
+                "LFC < 0 (down)",
+                "outliers",
+                "low counts",
+                "cutoff"
+            )) %>%
+            kable(caption = caption)
+    }
 )
 
 
@@ -118,11 +117,9 @@ setMethod(
         caption = NULL,
         ...
     ) {
-        dds <- bcbio(object, "DESeqDataSet")
-        # Warn if empty design formula detected
-        if (design(dds) == formula(~1)) {  # nolint
-            warn("Internal DESeqDataSet has an empty design formula")
-        }
+        validObject(bcb)
+        dds <- assays(object)[["dds"]]
+        assert_is_all_of(dds, "DESeqDataSet")
         alphaSummary(
             object = dds,
             alpha = alpha,
