0188b528f4537099a49bb5a184006599f08c0690
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-08 19:16:08 -0500

    Add custom `show()` method

---
+++ R/methods-show.R
@@ -0,0 +1,33 @@
+#' Show an Object
+#'
+#' @name show
+#' @author Michael Steinbuagh
+#'
+#' @inherit methods::show
+#'
+#' @examples
+#' load(system.file("extdata/bcb.rda", package = "bcbioRNASeq"))
+#' show(bcb)
+NULL
+
+
+
+# Methods ======================================================================
+#' @rdname show
+#' @export
+setMethod(
+    "show",
+    signature("bcbioRNASeq"),
+    function(object) {
+        validObject(object)
+        cat(
+            paste(class(object), metadata(object)[["version"]]),
+            paste("samples:", ncol(object)),
+            paste0(metadata(object)[["level"]], ": ", nrow(object)),
+            paste("organism:", metadata(object)[["organism"]]),
+            paste("bcbio run date:", metadata(object)[["runDate"]]),
+            paste("load date:", metadata(object)[["date"]]),
+            sep = "\n"
+        )
+    }
+)
