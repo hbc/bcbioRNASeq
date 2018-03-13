0a3f2b932f675cee58f71c9d916e675fe8521ce8
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-13 17:26:10 -0400

    Document example datasets

--- R/datasets.R
+++ R/datasets.R
@@ -1,5 +1,25 @@
+#' Minimal `bcbioRNASeq` Example
+#'
+#' GSE65267
+#'
+#' @author Michael Steinbaugh, Lorena Pantano
+#' @keywords internal
+"bcb_small"
+
+
+
 #' Gender Marker Genes
 #'
 #' @author Michael Steinbaugh
 #' @keywords internal
 "genderMarkers"
+
+
+
+#' Minimal `DESeqResults` Example
+#'
+#' GSE65267
+#'
+#' @author Michael Steinbaugh, Lorena Pantano
+#' @keywords internal
+"res_small"
