f838a5d93984933126edcb04ea9aadb1fa3a0e59
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-13 18:14:06 -0400

    Make sure genderMarkers get updated before bcb_small

--- data-raw/genderMarkers.R
+++
@@ -1,9 +0,0 @@
-devtools::load_all()
-library(tidyverse)
-file <- "data-raw/genderMarkers.xlsx"
-sheets <- excel_sheets(file)
-genderMarkers <- lapply(seq_along(sheets), function(a) {
-    read_excel(file, sheet = sheets[[a]])
-})
-names(genderMarkers) <- camel(sheets)
-devtools::use_data(genderMarkers, compress = "xz", overwrite = TRUE)
