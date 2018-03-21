0be7708ad1a74a1ddd189332673891925a66a771
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-21 08:46:16 -0400

    Add `headerLevel` and `return` to shared documentation

--- R/internal-general.R
+++ R/internal-general.R
@@ -8,11 +8,14 @@
 #'   `NULL`, the default ggplot2 color palette will be used. If manual color
 #'   definitions are desired, we recommend using [scale_fill_manual()].
 #' @param flip Flip x and y axes.
+#' @param headerLevel R Markdown header level.
 #' @param i An integer or numeric scalar.
 #' @param interestingGroups Character vector denoting groups of interest that
 #'   define the samples. If left unset, defaults to `sampleName`.
 #' @param object Object.
 #' @param passLimit Threshold to plot pass color marker.
+#' @param return Object class to return. Uses [match.arg()] internally and picks
+#'   the first item in the vector by default.
 #' @param title Title.
 #' @param value Value to assign.
 #' @param warnLimit Threshold to plot warning color marker.
