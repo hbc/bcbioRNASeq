da56fdc75da28def599b89c057c429c2246eb638
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-21 15:39:21 -0400

    Load alphabetically

--- tests/testthat/helper_01_cache.R
+++ tests/testthat/helper_01_cache.R
@@ -1,7 +1,7 @@
 cacheURL <- "http://bcbiornaseq.seq.cloud"
 files <- c(
-    "bcb_small.rda",
     "bcb_invalid.rda",
+    "bcb_small.rda",
     "dds_small.rda",
     "res_small.rda",
     "rld_small.rda",
