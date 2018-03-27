88249b012e767300e678a13636a0c2094b013d72
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-27 10:30:22 -0400

    Simplify test by modifying bcb_small. Will improve in a future update

--- tests/testthat/test_updateObject.R
+++ tests/testthat/test_updateObject.R
@@ -1,20 +1,21 @@
 context("updateObject")
 
-test_that("v0.1.4", {
-    expect_error(validObject(bcb_invalid))
-    expect_identical(
-        metadata(bcb_invalid)[["version"]],
-        package_version("0.1.4")
+test_that("updateObject", {
+    invalid <- bcb_small
+    metadata(invalid)[["version"]] <- package_version("0.1.7")
+    expect_error(
+        validObject(invalid),
+        "metadata\\(object\\)\\[\\[\"version\"\\]\\] >= 0.2 is not TRUE"
     )
-    organism <- metadata(bcb_invalid)[["organism"]]
+    organism <- metadata(invalid)[["organism"]]
     rowRanges <- genes(organism)
-    bcb <- suppressWarnings(updateObject(bcb_invalid, rowRanges = rowRanges))
+    valid <- suppressWarnings(updateObject(invalid, rowRanges = rowRanges))
     expect_identical(
         metadata(bcb)[["version"]],
         packageVersion
     )
     expect_identical(
-        metadata(bcb)[["previousVersion"]],
-        package_version("0.1.4")
+        metadata(valid)[["previousVersion"]],
+        package_version("0.1.7")
     )
 })
