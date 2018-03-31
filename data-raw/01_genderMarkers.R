5db821869df743880e1ff28643d18986c8f1268f
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-31 15:37:57 -0400

    Remove `include` column

--- data-raw/01_genderMarkers.R
+++ data-raw/01_genderMarkers.R
@@ -5,7 +5,9 @@ load_all()
 file <- "data-raw/genderMarkers.xlsx"
 sheets <- excel_sheets(file)
 genderMarkers <- lapply(seq_along(sheets), function(a) {
-    read_excel(file, sheet = sheets[[a]])
+    read_excel(file, sheet = sheets[[a]]) %>%
+        filter(include == TRUE) %>%
+        select(-include)
 })
 names(genderMarkers) <- camel(sheets)
 use_data(genderMarkers, compress = "xz", overwrite = TRUE)
