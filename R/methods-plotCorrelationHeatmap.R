250c8f9198f581a04210d0a3d6a3ff32410b1c9e
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-03 23:24:52 -0400

    Simplify bcbioRNASeq method by using RangedSummarizedExperiment

--- R/methods-plotCorrelationHeatmap.R
+++ R/methods-plotCorrelationHeatmap.R
@@ -1,10 +1,7 @@
 #' Plot Correlation Heatmap
 #'
-#' This function calculates a correlation matrix based on gene expression per
-#' sample. By default, this function processes all gene counts per sample to
-#' calculate the corrlation matrix. This behavior can be overrided with the
-#' input of `gene` identifier vector. In this case, only the expression of the
-#' desired genes will be used to calculate the correlation matrix.
+#' This function calculates a correlation matrix based on feature expression per
+#' sample.
 #'
 #' @name plotCorrelationHeatmap
 #' @family Quality Control Functions
@@ -50,72 +47,17 @@ setMethod(
     function(
         object,
         normalized = c("rlog", "vst", "tmm", "tpm"),
-        method = c("pearson", "spearman"),
-        genes = NULL,
-        samples = NULL,
-        interestingGroups,
-        color = viridis,
-        legendColor = viridis,
-        title = TRUE,
         ...
     ) {
-        # Passthrough: method, genes, samples, color, legendColor
         validObject(object)
         normalized <- match.arg(normalized)
-        method <- match.arg(method)
-        if (missing(interestingGroups)) {
-            interestingGroups <- bcbioBase::interestingGroups(object)
-        }
-        assertFormalInterestingGroups(colData(object), interestingGroups)
-
-        # Title
-        if (isTRUE(title)) {
-            title <- paste(
-                normalized,
-                paste(method, "correlation"),
-                sep = " : "
-            )
-        }
-
-        # Subset the counts matrix
         counts <- counts(object, normalized = normalized)
-        if (is.character(genes)) {
-            counts <- counts[genes, , drop = FALSE]
-        }
-        if (is.character(samples)) {
-            counts <- counts[, samples, drop = FALSE]
-            if (is.data.frame(annotationCol)) {
-                annotationCol <- annotationCol[samples, , drop = FALSE]
-            }
-        }
 
-        # Rename the matrix columns to `sampleName`, if necessary
-        colData <- colData(object)
-        if (!identical(
-            x = colnames(object),
-            y = as.character(colData[["sampleName"]])
-        )) {
-            rownames(colData) <- colData[["sampleName"]] %>%
-                as.character() %>%
-                makeNames(unique = TRUE)
-            colnames(counts) <- rownames(colData)
-        }
+        # Coerce to RangedSummarizedExperiment
+        rse <- as(object, "RangedSummarizedExperiment")
+        assays(rse) <- list("assay" = counts)
+        validObject(rse)
 
-        # Don't set annotation columns if we're only grouping by sample name
-        if (identical(interestingGroups, "sampleName")) {
-            annotationCol <- NULL
-        } else {
-            annotationCol <- colData[, interestingGroups, drop = FALSE]
-        }
-
-        plotCorrelationHeatmap(
-            object = counts,
-            method = method,
-            annotationCol = as.data.frame(annotationCol),
-            color = color,
-            legendColor = legendColor,
-            title = title,
-            ...
-        )
+        plotCorrelationHeatmap(rse, ...)
     }
 )
