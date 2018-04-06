b87acd372617d49ad24923ad503a2d094abc84d2
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-06 17:12:50 -0400

    Improve return to separate by chromosome

--- R/methods-plotGenderMarkers.R
+++ R/methods-plotGenderMarkers.R
@@ -26,75 +26,53 @@ NULL
 
 
 
-# Constructors =================================================================
-.plotGenderMarkers.matrix <- function(  # nolint
-    object,
-    colData,
-    interestingGroups = "sampleName",
-    organism,
-    countsAxisLabel = "counts",
-    medianLine = TRUE,
-    color = scale_color_viridis(discrete = TRUE),
-    title = TRUE
-) {
-    assert_is_matrix(object)
-    assertFormalAnnotationCol(object, colData)
-    assertFormalInterestingGroups(colData, interestingGroups)
-    if (missing(organism)) {
-        organism <- detectOrganism(object)
-    }
-    assert_is_a_string(organism)
-    assert_is_a_string(countsAxisLabel)
-    assert_is_a_bool(medianLine)
-    assertIsColorScaleDiscreteOrNULL(color)
-
-    # Title
-    if (isTRUE(title)) {
-        title <- "gender markers"
-    } else if (!is_a_string(title)) {
-        title <- NULL
-    }
-
-    colData <- uniteInterestingGroups(colData, interestingGroups)
-
-    # Load the relevant internal gender markers data
-    markers <- bcbioRNASeq::genderMarkers
-    assert_is_subset(camel(organism), names(markers))
-    markers <- markers[[camel(organism)]]
-
-    gene2symbol <- markers %>%
-        mutate(
-            geneName = paste(
-                !!!syms(c("chromosome", "geneName")),
-                sep = " : "
-            )
-        ) %>%
-        .[, c("geneID", "geneName")] %>%
-        as.data.frame() %>%
-        set_rownames(.[["geneID"]])
-    assertIsGene2symbol(gene2symbol)
-
-    genes <- gene2symbol[["geneID"]] %>%
-        .[. %in% rownames(object)]
-    assert_is_non_empty(genes)
-
-    .plotGene.matrix(
-        object = object,
-        genes = genes,
-        gene2symbol = gene2symbol,
-        colData = colData,
-        interestingGroups = interestingGroups,
-        countsAxisLabel = countsAxisLabel,
-        medianLine = medianLine,
-        color = color,
-        return = "wide"
-    ) +
-        ggtitle(title)
-}
-
-
-
 # Methods ======================================================================
+#' @rdname plotGenderMarkers
+#' @export
+setMethod(
+    "plotGenderMarkers",
+    signature("SummarizedExperiment"),
+    function(object, ...) {
+        validObject(object)
+        return <- "wide"
+
+        # Load the relevant internal gender markers data
+        organism <- metadata(object)[["organism"]]
+        if (!is_a_string(organism)) {
+            organism <- detectOrganism(counts)
+        }
+        markers <- bcbioRNASeq::genderMarkers
+        assert_is_subset(camel(organism), names(markers))
+        markers <- markers[[camel(organism)]]
+
+        xGenes <- markers %>%
+            .[.[["chromosome"]] == "X", "geneID", drop = TRUE]
+        yGenes <- markers %>%
+            .[.[["chromosome"]] == "Y", "geneID", drop = TRUE]
+
+        xPlot <- plotGene(
+            object = rse,
+            genes = xGenes,
+            return = return,
+            ...
+        ) +
+            ggtitle("X chromosome")
+
+        yPlot <- plotGene(
+            object = rse,
+            genes = yGenes,
+            return = return,
+            ...
+        ) +
+            ggtitle("Y chromosome")
+
+        plotlist <- list(xPlot, yPlot)
+        plot_grid(plotlist = plotlist)
+    }
+)
+
+
+
 #' @rdname plotGenderMarkers
 #' @export
 setMethod(
@@ -102,29 +80,25 @@ setMethod(
     signature("bcbioRNASeq"),
     function(
         object,
-        interestingGroups,
         normalized = c("rlog", "vst", "tpm"),
-        color = scale_color_viridis(discrete = TRUE),
-        title = TRUE
+        ...
     ) {
         validObject(object)
         normalized <- match.arg(normalized)
-        if (missing(interestingGroups)) {
-            interestingGroups <- bcbioBase::interestingGroups(object)
-        }
         counts <- counts(object, normalized = normalized)
         # Ensure counts are log2 scale
         if (!normalized %in% c("rlog", "vst")) {
             counts <- log2(counts + 1L)
         }
-        .plotGenderMarkers.matrix(
-            object = counts,
-            interestingGroups = interestingGroups,
-            organism = metadata(object)[["organism"]],
-            colData = colData(object),
-            countsAxisLabel = paste(normalized, "counts (log2)"),
-            color = color,
-            title = title
+        countsAxisLabel <- paste(normalized, "counts (log2)")
+
+        rse <- as(object, "RangedSummarizedExperiment")
+        assay(rse) <- counts
+
+        plotGenderMarkers(
+            object = rse,
+            countsAxisLabel = countsAxisLabel,
+            ...
         )
     }
 )
@@ -136,26 +110,18 @@ setMethod(
 setMethod(
     "plotGenderMarkers",
     signature("DESeqDataSet"),
-    function(
-        object,
-        interestingGroups = "sampleName",
-        organism,
-        color = scale_color_viridis(discrete = TRUE),
-        title = TRUE
-    ) {
+    function(object, ...) {
         validObject(object)
         counts <- log2(counts(object, normalized = TRUE) + 1L)
-        colData <- colData(object)
-        # Drop the numeric sizeFactor column
-        colData[["sizeFactor"]] <- NULL
-        .plotGenderMarkers.matrix(
-            object = counts,
-            interestingGroups = interestingGroups,
-            organism = organism,
-            colData = colData,
-            countsAxisLabel = "normalized counts (log2)",
-            color = color,
-            title = title
+        countsAxisLabel <- "normalized counts (log2)"
+
+        rse <- as(object, "RangedSummarizedExperiment")
+        assay(rse) <- counts
+
+        plotGenderMarkers(
+            object = rse,
+            countsAxisLabel = countsAxisLabel,
+            ...
         )
     }
 )
@@ -167,33 +133,19 @@ setMethod(
 setMethod(
     "plotGenderMarkers",
     signature("DESeqTransform"),
-    function(
-        object,
-        interestingGroups = "sampleName",
-        organism,
-        color = scale_color_viridis(discrete = TRUE),
-        title = TRUE
-    ) {
-        # Passthrough: interestingGroups, color, title
+    function(object, ...) {
         validObject(object)
-        counts <- assay(object)
-
-        colData <- colData(object)
-        # Drop the numeric sizeFactor column
-        colData[["sizeFactor"]] <- NULL
         if ("rlogIntercept" %in% colnames(mcols(object))) {
             normalized <- "rlog"
         } else {
             normalized <- "vst"
         }
-        .plotGenderMarkers.matrix(
-            object = counts,
-            interestingGroups = interestingGroups,
-            organism = organism,
-            colData = colData,
-            countsAxisLabel = paste(normalized, "counts (log2)"),
-            color = color,
-            title = title
+        countsAxisLabel <- paste(normalized, "counts (log2)")
+        rse <- as(object, "RangedSummarizedExperiment")
+        plotGenderMarkers(
+            object = rse,
+            countsAxisLabel = countsAxisLabel,
+            ...
         )
     }
 )
