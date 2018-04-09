fe6f93d99e661043ed7cb1bd7e15c4ca74bc6aaa
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-09 13:15:29 -0400

    Explictly define `metadata<-` method, to avoid unwanted object coercion. Need to file a bug report for this?

---
+++ R/methods-metadata.R
@@ -0,0 +1,22 @@
+#' Metadata
+#'
+#' @name metadata
+#' @author Michael Steinbaugh
+#' @keywords internal
+#'
+#' @examples
+#' # bcbioSingleCell ====
+#' metadata(bcb_small)[["stash"]] <- "test"
+#' metadata(bcb_small)[["stash"]]
+NULL
+
+
+
+# Methods ======================================================================
+#' @rdname metadata
+#' @importFrom S4Vectors metadata<-
+setMethod(
+    "metadata<-",
+    signature("bcbioRNASeq"),
+    getMethod("metadata<-", "Annotated")
+)
