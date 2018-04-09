c5dca2e1bf5f8adc7cad02bcc5c6fe4d9e422cd8
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-09 00:35:55 -0400

    Improve unit test code coverage

--- tests/testthat/test_gene_expression_functions.R
+++ tests/testthat/test_gene_expression_functions.R
@@ -24,12 +24,30 @@ test_that("plotGenderMarkers : DESeqTransform", {
 
 
 # plotGene =====================================================================
-test_that("bcbioRNASeq", {
-    p <- plotGene(bcb_small, genes = genes)
+test_that("plotGene : bcbioRNASeq", {
+    # facet
+    p <- plotGene(bcb_small, genes = genes, return = "facet")
     expect_is(p, "ggplot")
-})
 
-test_that("List return", {
+    # wide
+    p <- plotGene(bcb_small, genes = genes, return = "wide")
+    expect_is(p, "ggplot")
+
+    # grid
+    p <- plotGene(bcb_small, genes = genes, return = "grid")
+    expect_is(p, "ggplot")
+
+    # markdown
+    gene <- gene2symbol[1L, "geneName", drop = TRUE]
+    output <- capture.output(
+        plotGene(bcb_small, genes = genes, return = "markdown")
+    )
+    expect_identical(
+        output[[3L]],
+        paste("##", gene)
+    )
+
+    # list
     x <- plotGene(bcb_small, genes = genes, return = "list")
     expect_is(x, "list")
     expect_true(
@@ -39,23 +57,12 @@ test_that("List return", {
     )
 })
 
-test_that("Markdown return", {
-    gene <- gene2symbol[1L, "geneName", drop = TRUE]
-    output <- capture.output(
-        plotGene(bcb_small, genes = genes, return = "markdown")
-    )
-    expect_identical(
-        output[[3L]],
-        paste("##", gene)
-    )
-})
-
-test_that("DESeqDataSet", {
+test_that("plotGene : DESeqDataSet", {
     p <- plotGene(dds_small, genes = genes)
     expect_is(p, "ggplot")
 })
 
-test_that("DESeqTransform", {
+test_that("plotGene : DESeqTransform", {
     p <- plotGene(rld_small, genes = genes)
     expect_is(p, "ggplot")
 })
