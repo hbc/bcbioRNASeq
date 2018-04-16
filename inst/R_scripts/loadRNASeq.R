ff2bec1c4e034ebbe98c376f2877d6fe76ca4de3
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-16 17:19:59 -0400

    Remove example script [ci skip]

--- inst/R_scripts/loadRNASeq.R
+++
@@ -1,7 +0,0 @@
-library(bcbioRNASeq)
-bcb <- loadRNASeq(
-    uploadDir = "bcbio_rnaseq_run/final",
-    interestingGroups = c("genotype", "treatment"),
-    organism = "Homo sapiens"
-)
-saveData(bcb, dir = file.path("data", Sys.Date()))
