64ac84e9f4f56a3aa9836161264175897d02704e
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-19 14:06:21 -0400

    Move general arguments up

--- R/internal-general.R
+++ R/internal-general.R
@@ -3,6 +3,12 @@
 #' @name general
 #' @keywords internal
 #'
+#' @param object Object.
+#' @param i An integer or numeric scalar.
+#' @param value Value to assign.
+#' @param x Object.
+#' @param ... Additional arguments.
+#'
 #' @param alpha Adjusted P value ("alpha") cutoff.
 #' @param colData `data.frame` describing the columns of the object. Must
 #'   contain `rownames` identical to the `colnames` of the object.
@@ -21,7 +27,6 @@
 #'   rownames of the object. It is best practice to use the stable gene
 #'   identifiers from Ensembl (e.g. "ENSG00000000003") and not the gene symbols.
 #' @param headerLevel R Markdown header level.
-#' @param i An integer or numeric scalar.
 #' @param interestingGroups Character vector denoting groups of interest that
 #'   define the samples. If left unset, defaults to `sampleName`.
 #' @param lfc Log fold change ratio (base 2) cutoff.
@@ -31,20 +36,21 @@
 #'   - "`tmm`": edgeR trimmed mean of M-values. Calculated on the fly.
 #'   - "`rlog`": DESeq2 **log2** regularized log transformation.
 #'   - "`vst`": DESeq2 **log2** variance stabilizing transformation.
-#' @param object Object.
+#' @param ntop Number of top genes to label.
 #' @param passLimit Threshold to plot pass color marker.
+#' @param pointColor Default point color for the plot.
 #' @param results `DESeqResults` object.
 #' @param return Object class to return. Uses [match.arg()] internally and picks
 #'   the first item in the vector by default.
 #' @param samples Character vector of samples to include.
+#' @param sigPointColor `character` vector containing color names for labeling
+#'   upregulated and downregulated genes. Also supports a character string for
+#'   labeling DEGs with the same color, regardless of direction.
 #' @param subtitle Subtitle of plot.
 #' @param title Title of plot.
-#' @param value Value to assign.
 #' @param warnLimit Threshold to plot warning color marker.
 #' @param withDimnames A `logical`, indicating whether dimnames should be
 #'   applied to extracted assay elements.
-#' @param x Object.
-#' @param ... Additional arguments.
 #'
 #' @return No value.
 NULL
