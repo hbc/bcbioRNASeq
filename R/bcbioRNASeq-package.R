e97b2aa3e7a234af44999560bd473f09930bc40d
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-20 08:26:01 -0400

    Remove viridis dependency

--- R/bcbioRNASeq-package.R
+++ R/bcbioRNASeq-package.R
@@ -35,8 +35,9 @@
 #'   coord_flip element_blank element_text expand_limits facet_wrap geom_bar
 #'   geom_boxplot geom_density geom_hline geom_jitter geom_point geom_polygon
 #'   geom_ribbon geom_smooth geom_vline ggplot ggtitle guides labs
-#'   position_jitterdodge scale_color_manual scale_fill_manual
-#'   scale_x_continuous scale_y_continuous stat_summary theme xlab ylim
+#'   position_jitterdodge scale_color_hue scale_color_manual scale_fill_hue
+#'   scale_fill_manual scale_x_continuous scale_y_continuous stat_summary theme
+#'   xlab ylim
 #' @importFrom ggrepel geom_label_repel geom_text_repel
 #' @importFrom grid arrow unit
 #' @importFrom knitr kable
@@ -53,7 +54,6 @@
 #'   rownames_to_column tibble
 #' @importFrom tximport tximport
 #' @importFrom utils capture.output globalVariables packageVersion
-#' @importFrom viridis inferno scale_color_viridis scale_fill_viridis viridis
 #' @importFrom vsn meanSdPlot
 #'
 #' @importFrom assertive assert_all_are_dirs
