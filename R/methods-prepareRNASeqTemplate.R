81456a2d6f08844be09ea3fbaae63eec01b5bbce
Michael Steinbaugh <mike@steinbaugh.com>
2018-05-24 17:36:38 -0400

    Add clean up step

--- R/methods-prepareRNASeqTemplate.R
+++ R/methods-prepareRNASeqTemplate.R
@@ -11,6 +11,15 @@
 #' @examples
 #' x <- prepareRNASeqTemplate()
 #' x
+#'
+#' # Clean up
+#' unlink(c(
+#'     "_footer.Rmd",
+#'     "_header.Rmd",
+#'     "_output.yaml",
+#'     "_setup.R",
+#'     "bibliography.bib"
+#' ))
 prepareRNASeqTemplate <- function(overwrite = FALSE) {
     prepareTemplate(
         file = c(
