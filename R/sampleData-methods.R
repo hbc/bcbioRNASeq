2965df09aa087d244cd613c9ed3a9280dce431d9
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-11 06:54:45 -0400

    Migrate sampleData method for bcbioRNASeq object from bcbioBase (formerly SummarizedExperiment)

---
+++ R/sampleData-methods.R
@@ -0,0 +1,77 @@
+#' Sample Data
+#'
+#' Return the sample metadata. Columns are always sanitized to factor.
+#'
+#' @note This is a complement to the standard [colData()] function, but improves
+#'   support for accessing sample metadata for datasets where multiple items in
+#'   the columns map to a single sample (e.g. cells for a single-cell RNA-seq
+#'   experiment).
+#'
+#' @name sampleData
+#' @family Data Functions
+#' @author Michael Steinbaugh
+#'
+#' @importFrom basejump sampleData
+#'
+#' @inheritParams general
+#' @param clean `logical`. Only return `factor` columns not defined in
+#'   [bcbioBase::metadataBlacklist].
+#'
+#' @return Data describing the samples.
+#'
+#' @seealso [bcbioBase::metadataBlacklist].
+#'
+#' @examples
+#' # SummarizedExperiment ====
+#' sampleData(bcb_small, clean = TRUE) %>% glimpse()
+#' sampleData(bcb_small, clean = FALSE) %>% glimpse()
+#'
+#' # Assignment support
+#' x <- bcb_small
+#' sampleData(x)[["test"]] <- seq_len(ncol(x))
+#' # `test` column should be now defined
+#' "test" %in% colnames(sampleData(x))
+NULL
+
+
+
+# Methods ======================================================================
+#' @rdname sampleData
+#' @export
+setMethod(
+    "sampleData",
+    signature("bcbioRNASeq"),
+    function(
+        object,
+        clean = TRUE,
+        interestingGroups,
+        return = c("DataFrame", "data.frame", "kable")
+    ) {
+        data <- colData(object)
+        assert_is_a_bool(clean)
+        return <- match.arg(return)
+
+        # Only return factor columns, if desired
+        if (isTRUE(clean)) {
+            data <- data[, vapply(data, is.factor, logical(1L)), drop = FALSE]
+            # Drop remaining blacklisted columns
+            setdiff <- setdiff(colnames(data), bcbioBase::metadataBlacklist)
+            data <- data[, setdiff, drop = FALSE]
+        } else {
+            # Include `interestingGroups` column, if not NULL
+            if (missing(interestingGroups)) {
+                interestingGroups <- bcbioBase::interestingGroups(object)
+            }
+            if (length(interestingGroups)) {
+                data <- uniteInterestingGroups(data, interestingGroups)
+            }
+        }
+
+        # Return
+        if (return == "kable") {
+            kable(as.data.frame(data), row.names = FALSE)
+        } else {
+            as(data, return)
+        }
+    }
+)
