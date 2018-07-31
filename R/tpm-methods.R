2411be64c96f5df2b321c753c314af57e5e3b0c9
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-31 18:28:56 -0400

    Improve `tpm()` working example and simplify return

--- R/tpm-methods.R
+++ R/tpm-methods.R
@@ -9,7 +9,7 @@
 #' @return `matrix`.
 #'
 #' @examples
-#' tpm(bcb_small) %>% glimpse()
+#' tpm(bcb_small) %>% summary()
 NULL
 
 
@@ -22,8 +22,6 @@ setMethod(
     signature("bcbioRNASeq"),
     function(object) {
         validObject(object)
-        data <- assays(object)[["tpm"]]
-        assert_is_matrix(data)
-        data
+        assays(object)[["tpm"]]
     }
 )
