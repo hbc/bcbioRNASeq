4e5c02fb7f24843850c31b34ab5bc6937a85dd1a
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-02 08:36:15 -0400

    Define `tximportCallers` and `featureCountsCallers` as globals for easier handling in `updateObject()` call

--- R/globals.R
+++ R/globals.R
@@ -1,7 +1,12 @@
 globalVariables(".")
-legacyMetricsCols <- c("name", "x53Bias")
+
 packageVersion <- packageVersion("bcbioRNASeq")
+legacyMetricsCols <- c("name", "x53Bias")
+
 # v0.2.6: tpm and length are now optional, since we're supporting featureCounts
 requiredAssays <- "counts"
+
 # v0.2.6: added STAR and HISAT2 support
-validCallers <- c("salmon", "kallisto", "sailfish", "star", "hisat2")
+tximportCallers <- c("salmon", "kallisto", "sailfish")
+featureCountsCallers <- c("star", "hisat2")
+validCallers <- c(tximportCallers, featureCountsCallers)
