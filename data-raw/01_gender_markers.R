25d3529b91b914d10e570c21318924a0417be343
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-01 21:46:39 -0400

    Reorganize internal data

--- data-raw/01_gender_markers.R
+++ data-raw/01_gender_markers.R
@@ -2,12 +2,12 @@ library(devtools)
 library(readxl)
 library(tidyverse)
 load_all()
-file <- "data-raw/genderMarkers.xlsx"
+file <- "data-raw/gender_markers.xlsx"
 sheets <- excel_sheets(file)
-genderMarkers <- lapply(seq_along(sheets), function(a) {
+gender_markers <- lapply(seq_along(sheets), function(a) {
     read_excel(file, sheet = sheets[[a]]) %>%
         filter(include == TRUE) %>%
         select(-include)
 })
-names(genderMarkers) <- camel(sheets)
-use_data(genderMarkers, compress = "xz", overwrite = TRUE)
+names(gender_markers) <- camel(sheets)
+use_data(gender_markers, compress = "xz", overwrite = TRUE)
