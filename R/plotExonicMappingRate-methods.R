54929bad1bb33ef32657a3237ff43e3956a8463f
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-02 11:42:33 -0400

    Fix implicit integers

--- R/plotExonicMappingRate-methods.R
+++ R/plotExonicMappingRate-methods.R
@@ -64,7 +64,7 @@ setMethod(
 
         if (is_positive(limit)) {
             # Convert to percentage
-            if (limit > 1) {
+            if (limit > 1L) {
                 warning("`limit`: Use ratio (0-1) instead of percentage")
             } else {
                 limit <- limit * 100L
--- R/plotMappedReads-methods.R
+++ R/plotMappedReads-methods.R
@@ -27,7 +27,7 @@ setMethod(
     function(
         object,
         interestingGroups,
-        limit = 10e6,
+        limit = 10e6L,
         fill = getOption("bcbio.discrete.fill", NULL),
         flip = getOption("bcbio.flip", TRUE),
         title = "mapped reads"
@@ -66,10 +66,10 @@ setMethod(
 
         if (is_positive(limit)) {
             # Convert limit to per million
-            if (limit < 1e6) {
+            if (limit < 1e6L) {
                 warning("`limit`: Use absolute value, not per million")
             } else {
-                limit <- limit / 1e6
+                limit <- limit / 1e6L
             }
             if (limit >= 1L) {
                 p <- p + bcbio_geom_abline(yintercept = limit)
