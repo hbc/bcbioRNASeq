dae8d24b08169775a02a9804df24fa2563584572
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-26 08:22:38 -0400

    Update order of globals

--- R/globals.R
+++ R/globals.R
@@ -1,15 +1,17 @@
 globalVariables(".")
-featureCountsAssays <- requiredAssays
+
+requiredAssays <- "counts"
 featureCountsCallers <- c("star", "hisat2")
 lanePattern <- basejump::lanePattern
 legacyMetricsCols <- c("name", "x53Bias")
 metadataBlacklist <- bcbioBase::metadataBlacklist
 packageVersion <- packageVersion("bcbioRNASeq")
 projectDirPattern <- bcbioBase::projectDirPattern
-requiredAssays <- "counts"
 separatorBar <- basejump::separatorBar
 tximportAssays <- c("counts", "length", "tpm")
 tximportCallers <- c("salmon", "kallisto", "sailfish")
 updateMessage <- basejump::updateMessage
-validCallers <- c(tximportCallers, featureCountsCallers)
 validLevels <- c("genes", "transcripts")
+
+featureCountsAssays <- requiredAssays
+validCallers <- c(tximportCallers, featureCountsCallers)
