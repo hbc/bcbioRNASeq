80b4aa9041164d62845da07f17b7dba0a3e61574
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-08 17:17:47 -0400

    Improve documentation

--- R/general.R
+++ R/general.R
@@ -48,7 +48,6 @@
 #'     - "`tmm`": edgeR trimmed mean of M-values. Calculated on the fly.
 #'     - "`rlog`": DESeq2 **log2** regularized log transformation.
 #'     - "`vst`": DESeq2 **log2** variance stabilizing transformation.
-#'   Transcript-level counts support only "`tpm`".
 #' @param ntop `scalar integer`. Number of top genes to label.
 #' @param passLimit `scalar numeric`. Threshold to plot pass color marker.
 #' @param pointColor `string`. Default point color for the plot.
