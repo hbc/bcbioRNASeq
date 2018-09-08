8e8cf5c452cf4ece49e9b413fdb171b8059e1db9
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-08 07:19:35 -0400

    Checking to see if `tx2gene` object is slotted in metadata

--- R/bcbioRNASeq-validity.R
+++ R/bcbioRNASeq-validity.R
@@ -101,7 +101,7 @@ setValidity(
             sampleDirs = "character",
             sampleMetadataFile = "character",
             template = "character",
-            tx2gene = c("DataFrame", "data.frame"),
+            tx2gene = c("tx2gene", "data.frame"),
             uploadDir = "character",
             utilsSessionInfo = "sessionInfo",
             version = "package_version",
