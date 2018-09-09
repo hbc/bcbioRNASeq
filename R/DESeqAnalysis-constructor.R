5555fb469f25492f3623deeeedda14e5c1cfed1b
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-09 11:36:18 -0400

    Improve working example

--- R/DESeqAnalysis-constructor.R
+++ R/DESeqAnalysis-constructor.R
@@ -3,6 +3,19 @@
 #' Class containing all elements generated during differential expression
 #' analysis with DESeq2.
 #'
+#' @section DESeqDataSet:
+#'
+#' We recommend generating the `DESeqDataSet` by coercion from `bcbioRNASeq`
+#' object using `as(dds, "bcbioRNASeq")`. Don't use the [DESeq2::DESeqDataSet()]
+#' or [DESeq2::DESeqDataSetFromMatrix()] constructors to generate the
+#' `DESeqDataSet` object.
+#'
+#' @section DESeqResults:
+#'
+#' Don't modify any of the `DESeqResults` objects manually. This includes
+#' rearranging the rows or dropping genes without adjusted P values. We'll take
+#' care of this automatically in supported methods.
+#'
 #' @family S4 Object
 #' @author Michael Steinbaugh
 #' @export
@@ -17,14 +30,22 @@
 #' @return `DESeqAnalysis`.
 #'
 #' @examples
+#' library(DESeq2)
+#' dds <- dds_small
+#' class(dds)
+#' vst <- varianceStabilizingTransformation(dds)
+#' class(vst)
+#' resultsNames(dds)
+#' res <- results(dds, name = resultsNames(dds)[[2L]])
+#' class(res)
 #' x <- DESeqAnalysis(
 #'     DESeqDataSet = dds_small,
 #'     DESeqTransform = vst_small,
 #'     DESeqResults = list(
-#'         contrast1 = res_small
+#'         res_small
 #'     ),
 #'     lfcShrink = list(
-#'         contrast1 = DESeq2::lfcShrink(dds = dds_small, coef = 2L)
+#'         lfcShrink(dds = dds_small, coef = 2L)
 #'     )
 #' )
 #' class(x)
