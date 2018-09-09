9706e8425fc459b64bf0fc537f85cea5b5b22522
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-09 10:23:17 -0400

    Define DESeqAnalysis class

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,3 +1,7 @@
+setClassUnion("missingOrNULL", c("missing", "NULL"))
+
+
+
 #' @rdname bcbioRNASeq
 #' @aliases NULL
 #' @exportClass bcbioRNASeq
@@ -7,4 +11,35 @@ bcbioRNASeq <- setClass(
     contains = "RangedSummarizedExperiment"
 )
 
-setClassUnion("missingOrNULL", c("missing", "NULL"))
+
+
+#' DESeq2 Differential Expression Analysis
+#'
+#' Class containing all elements generated during differential expression
+#' analysis with DESeq2.
+#'
+#' @section Slots:
+#'
+#'     - `data`: `DESeqDataSet`.
+#'     - `transform`: `DESeqTransform`.
+#'     - `results`: `list` containing one or more `DESeqResults`.
+#'
+#' @export
+#'
+#' @examples
+#' x <- new(
+#'     "DESeqAnalysis",
+#'     data = dds_small,
+#'     transform = vst_small,
+#'     results = list(res_small)
+#' )
+#' class(x)
+#' slotNames(x)
+setClass(
+    Class = "DESeqAnalysis",
+    slots = list(
+        data = "DESeqDataSet",
+        transform = "DESeqTransform",
+        results = "list"
+    )
+)
