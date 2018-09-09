d68177c6cc7e0c518f7572db122cc5f1ede5a5bc
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-09 10:39:15 -0400

    Remove superfluous rdname

--- R/bcbioRNASeq-constructor.R
+++ R/bcbioRNASeq-constructor.R
@@ -60,7 +60,6 @@
 #' @note `bcbioRNASeq` extended `SummarizedExperiment` prior to v0.2.0, where we
 #'   migrated to `RangedSummarizedExperiment`.
 #'
-#' @rdname bcbioRNASeq
 #' @aliases bcbioRNASeq-class
 #' @docType class
 #' @family S4 Object
