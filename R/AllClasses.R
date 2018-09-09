34227fbc3e136673c69aaa27a70c42f92fe952fc
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-09 15:27:17 -0400

    Improve class definitions

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,7 +1,11 @@
-setClassUnion("missingOrNULL", c("missing", "NULL"))
+setClassUnion(
+    name = "missingOrNULL",
+    members = c("missing", "NULL")
+)
 
 
 
+# bcbioRNASeq ==================================================================
 #' @rdname bcbioRNASeq
 #' @aliases NULL
 #' @exportClass bcbioRNASeq
@@ -13,6 +17,7 @@ setClass(
 
 
 
+# DESeqAnalysis ================================================================
 #' @rdname DESeqAnalysis
 #' @aliases NULL
 #' @exportClass DESeqAnalysis
@@ -25,4 +30,82 @@ setClass(
         DESeqResults = "list",
         lfcShrink = "list"
     )
+)
+
+setValidity(
+    Class = "DESeqAnalysis",
+    method = function(object) {
+        # Require valid dimnames for data set.
+        assertHasValidDimnames(object@DESeqDataSet)
+
+        # Require gene-to-symbol mappings.
+        assert_is_all_of(
+            x = gene2symbol(object@DESeqDataSet),
+            classes = "gene2symbol"
+        )
+
+        # DESeqDataSet and DESeqTransform must match.
+        assert_are_identical(
+            x = dimnames(object@DESeqDataSet),
+            y = dimnames(object@DESeqTransform)
+        )
+
+        # DESeqDataSet and DESeqResults must match.
+        invisible(lapply(
+            X = object@DESeqResults,
+            FUN = function(res) {
+                assert_are_identical(
+                    x = rownames(res),
+                    y = rownames(object@DESeqDataSet)
+                )
+            }
+        ))
+
+        # DESeqResults and lfcShrink rownames must match.
+        invisible(mapply(
+            unshrunken = object@DESeqResults,
+            shrunken = object@lfcShrink,
+            FUN = function(unshrunken, shrunken) {
+                assert_are_identical(
+                    x = rownames(unshrunken),
+                    y = rownames(shrunken)
+                )
+            }
+        ))
+
+        TRUE
+    }
+)
+
+
+
+# resultsTables ================================================================
+#' @rdname resultsTables
+#' @aliases NULL
+#' @exportClass resultsTables
+#' @usage NULL
+setClass(
+    Class = "DESeqResultsTables",
+    slots = list(
+        deg = "tbl_df",
+        degLFC = "tbl_df",
+        degLFCUp = "tbl_df",
+        degLFCDown = "tbl_df",
+        all = "DESeqResults",
+        contrast = "character",
+        alpha = "numeric",
+        lfcThreshold = "numeric"
+    )
+)
+
+# FIXME Improve this.
+setValidity(
+    Class = "DESeqResultsTables",
+    method = function(object) {
+        assert_is_subset(
+            x = c("all", "deg", "degLFCDown", "degLFCUp"),
+            y = names(object)
+        )
+        TRUE
+    }
 )
