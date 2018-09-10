3800b85c900ac297dd974e3546d235bd6fe2ba61
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-09 21:07:36 -0400

    Improve documentation

--- R/data.R
+++ R/data.R
@@ -1,16 +1,24 @@
 #' `bcbioRNASeq` Example
 #'
 #' @family GSE65267
-#' @author Lorena Pantano, Michael Steinbaugh
+#' @author Michael Steinbaugh, Lorena Pantano
+#'
+#' @examples
+#' print(bcb_small)
 "bcb_small"
 
 
 
-#' `DESeqDataSet` Example
+#' `DESeqAnalysis` Example
+#'
+#' Minimal DESeq2 differential expression analysis using [bcb_small].
 #'
 #' @family GSE65267
-#' @author Lorena Pantano, Michael Steinbaugh
-"dds_small"
+#' @author Michael Steinbaugh, Lorena Pantano
+#'
+#' @examples
+#' print(deseq_small)
+"deseq_small"
 
 
 
@@ -18,12 +26,7 @@
 #'
 #' @author Michael Steinbaugh
 #' @keywords internal
-"gender_markers"
-
-
-
-#' `DESeqResults` Example
 #'
-#' @family GSE65267
-#' @author Lorena Pantano, Michael Steinbaugh
-"res_small"
+#' @examples
+#' names(gender_markers)
+"gender_markers"
