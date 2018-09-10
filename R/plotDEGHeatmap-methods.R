ec106a55e5dded56cbd05ac62f1cadee6d040380
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-10 06:36:32 -0400

    Draft updates to `plotDEGHeatmap()`

--- R/plotDEGHeatmap-methods.R
+++ R/plotDEGHeatmap-methods.R
@@ -29,88 +29,84 @@
 #' @examples
 #' # DESeqResults, DESeqTransform ====
 #' plotDEGHeatmap(
-#'     object = deseq_small@results,
+#'     object = deseq_small@results[[1L]],
 #'     counts = deseq_small@transform
+#' )
+#'
+#' # DESeqResults, bcbioRNASeq ====
+#' plotDEGHeatmap(
+#'     object = deseq_small@results[[1L]],
+#'     counts = bcb_small,
+#'     normalized = "vst"
 #' )
 NULL
 
 
 
-.plotDEGHeatmap <-  # nolint
-function(
-    object,
-    counts,
-    alpha = NULL,
-    lfcThreshold = 0L,
-    scale = "row",
-    title = TRUE
-    # Defining additional formals below.
-) {
-    validObject(object)
-    validObject(counts)
-    assert_are_identical(
-        x = rownames(object),
-        y = rownames(counts)
-    )
-    # Coerce to RSE then SE to preserve rowData.
-    if (is(counts, "RangedSummarizedExperiment")) {
-        counts <- as(counts, "RangedSummarizedExperiment")
-    }
-    counts <- as(counts, "SummarizedExperiment")
-    if (is.null(alpha)) {
+.plotDEGHeatmap.DESeqResults <-  # nolint
+    function(
+        object,
+        counts,
+        scale = "row",
+        title = TRUE
+        # Defining additional formals below.
+    ) {
+        validObject(object)
+        validObject(counts)
+        assert_are_identical(rownames(object), rownames(counts))
+        # Coerce to RSE then SE to preserve rowData.
+        if (is(counts, "RangedSummarizedExperiment")) {
+            counts <- as(counts, "RangedSummarizedExperiment")
+        }
+        counts <- as(counts, "SummarizedExperiment")
         alpha <- metadata(object)[["alpha"]]
+        assert_is_a_number(alpha)
+        lfcThreshold <- metadata(object)[["lfcThreshold"]]
+        assert_is_a_number(lfcThreshold)
+        assert_all_are_non_negative(lfcThreshold)
+        # Hiding the choices from the user by default, because in most cases row
+        # scaling should be used.
+        scale <- match.arg(scale, choices = c("row", "column", "none"))
+
+        # Title
+        if (isTRUE(title)) {
+            title <- contrastName(object)
+        } else if (!is_a_string(title)) {
+            title <- NULL
+        }
+
+        deg <- significants(object, padj = alpha, fc = lfcThreshold)
+
+        # Early return if there are no DEGs.
+        if (!length(deg)) {
+            warning("No significant DEGs to plot", call. = FALSE)
+            return(invisible())
+        }
+
+        # Subset the counts to only contain DEGs.
+        counts <- counts[deg, , drop = FALSE]
+
+        # Using `do.call()` return with SummarizedExperiment method here.
+        args <- setArgsToDoCall(
+            args = list(
+                object = counts,
+                interestingGroups = interestingGroups,
+                scale = scale,
+                title = title,
+                ...
+            ),
+            removeArgs = c(
+                "counts",
+                "alpha",
+                "lfcThreshold"
+            ),
+            call = matchS4Call()
+        )
+        do.call(what = plotHeatmap, args = args)
     }
-    assert_is_a_number(alpha)
-    assert_is_a_number(lfcThreshold)
-    assert_all_are_non_negative(lfcThreshold)
-    # Hiding the choices from the user by default, because in most cases row
-    # scaling should be used.
-    scale <- match.arg(
-        arg = scale,
-        choices = c("row", "column", "none")
-    )
-
-    # Title
-    if (isTRUE(title)) {
-        title <- contrastName(object)
-    } else if (!is_a_string(title)) {
-        title <- NULL
-    }
-
-    deg <- significants(object, padj = alpha, fc = lfcThreshold)
-
-    # Early return if there are no DEGs.
-    if (!length(deg)) {
-        warning("No significant DEGs to plot", call. = FALSE)
-        return(invisible())
-    }
-
-    # Subset the counts to only contain DEGs.
-    counts <- counts[deg, , drop = FALSE]
-
-    # Using `do.call()` return with SummarizedExperiment method here.
-    args <- setArgsToDoCall(
-        args = list(
-            object = counts,
-            interestingGroups = interestingGroups,
-            scale = scale,
-            title = title,
-            ...
-        ),
-        removeArgs = c(
-            "object",
-            "counts",
-            "alpha",
-            "lfcThreshold"
-        ),
-        call = matchS4Call(),
-        fun = sys.function()
-    )
-    do.call(what = plotHeatmap, args = args)
-}
 
 # Define the formals.
-f1 <- formals(.plotDEGHeatmap)
+f1 <- formals(.plotDEGHeatmap.DESeqResults)
 f2 <- methodFormals(
     f = "plotHeatmap",
     signature = "SummarizedExperiment"
@@ -120,84 +116,33 @@ f2 <- f2[setdiff(
     y = c(names(f1), "object")
 )]
 f <- c(f1, f2)
-formals(.plotDEGHeatmap) <- f
+formals(.plotDEGHeatmap.DESeqResults) <- f
 
 
 
 #' @rdname plotDEGHeatmap
 #' @export
 setMethod(
-    "plotDEGHeatmap",
-    signature(
-        object = "DESeqResults",
-        counts = "SummarizedExperiment"
-    ),
-    .plotDEGHeatmap
-)
-
-
-
-#' @rdname plotDEGHeatmap
-#' @export
-setMethod(
-    "plotDEGHeatmap",
-    signature(
+    f = "plotDEGHeatmap",
+    signature = signature(
         object = "DESeqResults",
         counts = "DESeqTransform"
     ),
-    getMethod(
-        "plotDEGHeatmap",
-        signature(
-            object = "DESeqResults",
-            counts = "SummarizedExperiment"
-        )
-    )
+    definition = .plotDEGHeatmap.DESeqResults
 )
 
 
 
+# FIXME Improve the formals here.
 #' @rdname plotDEGHeatmap
 #' @export
 setMethod(
-    "plotDEGHeatmap",
-    signature(
-        object = "DESeqResults",
-        counts = "DESeqDataSet"
-    ),
-    function(
-        object,
-        counts,
-        ...
-    ) {
-        validObject(object)
-        validObject(counts)
-        assert_are_identical(
-            x = rownames(object),
-            y = rownames(counts)
-        )
-        message("Using normalized counts")
-        rse <- as(counts, "RangedSummarizedExperiment")
-        assays(rse) <- list(counts = counts(counts, normalized = TRUE))
-        args <- list(
-            object = object,
-            counts = rse,
-            ...
-        )
-        do.call(what = plotDEGHeatmap, args = args)
-    }
-)
-
-
-
-#' @rdname plotDEGHeatmap
-#' @export
-setMethod(
-    "plotDEGHeatmap",
-    signature(
+    f = "plotDEGHeatmap",
+    signature = signature(
         object = "DESeqResults",
         counts = "bcbioRNASeq"
     ),
-    function(
+    definition = function(
         object,
         counts,
         normalized = c("vst", "rlog", "tmm", "tpm", "rle"),
@@ -205,17 +150,15 @@ setMethod(
     ) {
         validObject(object)
         validObject(counts)
-        assert_are_identical(
-            x = rownames(object),
-            y = rownames(counts)
-        )
+        assert_are_identical(rownames(object), rownames(counts))
         normalized <- match.arg(normalized)
         message(paste("Using", normalized, "counts"))
+        # Coerce to `DESeqTransform`.
         rse <- as(counts, "RangedSummarizedExperiment")
         assays(rse) <- list(counts = counts(counts, normalized = normalized))
         args <- list(
             object = object,
-            counts = rse,
+            counts = DESeqTransform(rse),
             ...
         )
         do.call(what = plotDEGHeatmap, args = args)
