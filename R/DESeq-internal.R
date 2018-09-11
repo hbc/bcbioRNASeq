348fd9568b83fa84bab7a7bf16a399aa95e0feb2
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-11 13:09:19 -0400

    Move internal function

--- R/DESeq-internal.R
+++ R/DESeq-internal.R
@@ -1,3 +1,45 @@
+# Calculate a numeric vector to define the colors
+# -1: downregulated
+#  0: not significant
+#  1: upregulated
+.addIsDECol <- function(
+    data,
+    testCol = "padj",
+    alpha,
+    lfcCol = "log2FoldChange",
+    lfcThreshold = 0L
+) {
+    # test: P value or S value
+    test <- data[[testCol]]
+    # lfc: log2 fold change cutoff
+    lfc <- data[[lfcCol]]
+    isDE <- mapply(
+        test = test,
+        lfc = lfc,
+        FUN = function(test, lfc) {
+            if (any(is.na(c(test, lfc)))) {
+                # nonsignificant
+                0L
+            } else if (test < alpha & lfc > lfcThreshold) {
+                # upregulated
+                1L
+            } else if (test < alpha & lfc < -lfcThreshold) {
+                # downregulated
+                -1L
+            } else {
+                0L
+            }
+        },
+        SIMPLIFY = TRUE,
+        USE.NAMES = FALSE
+    )
+    isDE <- as.factor(isDE)
+    data[["isDE"]] <- isDE
+    data
+}
+
+
+
 .matchResults <- function(
     object,
     results,
