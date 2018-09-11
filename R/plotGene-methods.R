20490725df827051d0b5dd2235a870d6e7b97d4f
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-11 13:05:13 -0400

    Improve formals

--- R/plotGene-methods.R
+++ R/plotGene-methods.R
@@ -1,272 +1,164 @@
 # FIXME Need to improve the formals.
+# FIXME Add `DESeqAnalysis` support.
+# FIXME Add DESeqAnalysis method support.
 
 
 
-#' Plot Individual Genes
+#' Plot Gene Expression
 #'
 #' @name plotGene
 #' @family Gene Expression Functions
 #' @author Michael Steinbaugh
-#'
 #' @importFrom basejump plotGene
 #' @export
 #'
-#' @inheritParams general
-#' @param countsAxisLabel `string`. Label to use for the counts axis.
-#' @param medianLine `boolean`. Include median line for each group. Disabled if
-#'   samples are colored by sample name.
-#'
-#' @return
-#' - "`facet`": `ggplot` grouped by `sampleName`, with [ggplot2::facet_wrap()]
-#'   applied to panel the samples.
-#' - "`wide`": Show `ggplot` in wide format, with genes on the x-axis.
-#'
-#' @seealso [DESeq2::plotCounts()].
+#' @inherit basejump::plotGene
 #'
 #' @examples
-#' # Gene identifiers
-#' genes <- head(rownames(bcb_small), n = 4L)
-#' print(genes)
+#'
+#' @examples
+#' g2s <- gene2symbol(bcb_small)
+#' geneIDs <- head(g2s[["geneID"]])
+#' print(geneIDs)
+#' geneNames <- head(g2s[["geneName"]])
+#' print(geneNames)
 #'
 #' # bcbioRNASeq ====
 #' object <- bcb_small
-#' plotGene(object, genes = genes, normalized = "vst", return = "facet")
-#' plotGene(object, genes = genes, normalized = "vst", return = "wide")
+#' plotGene(
+#'     object = object,
+#'     genes = geneIDs,
+#'     normalized = "vst",
+#'     return = "facet"
+#' )
+#' plotGene(
+#'     object = object,
+#'     genes = geneNames,
+#'     normalized = "vst",
+#'     return = "wide"
+#' )
 #'
-#' # DESeqTransform ====
-#' object <- deseq_small@transform
-#' plotGene(object, genes = genes)
+#' # DESeqAnalysis ====
+#' object <- deseq_small
+#' plotGene(object, genes = geneIDs, return = "facet")
+#' plotGene(object, genes = geneNames, return = "wide")
 NULL
 
 
 
-.plotGeneFacet <- function(
-    object,
-    interestingGroups = NULL,
-    countsAxisLabel = "counts",
-    medianLine = TRUE,
-    color = NULL,
-    legend = TRUE
-) {
-    stopifnot(is(object, "SummarizedExperiment"))
-    stopifnot(length(rownames(object)) <= 50L)
-    interestingGroups <- matchInterestingGroups(
-        object = object,
-        interestingGroups = interestingGroups
-    )
-    interestingGroups(object) <- interestingGroups
-
-    object <- convertGenesToSymbols(object)
-    data <- .meltCounts(
-        counts = assay(object),
-        sampleData = sampleData(object)
-    )
-
-    p <- ggplot(
-        data = data,
-        mapping = aes(
-            x = !!sym("interestingGroups"),
-            y = !!sym("counts"),
-            color = !!sym("interestingGroups")
-        )
-    ) +
-        .genePoint(show.legend = legend) +
-        facet_wrap(facets = sym("geneID"), scales = "free_y") +
-        labs(
-            x = NULL,
-            y = countsAxisLabel,
-            color = paste(interestingGroups, collapse = ":\n")
-        )
-
-    if (isTRUE(medianLine) && !identical(interestingGroups, "sampleName")) {
-        p <- p + .geneMedianLine
-    }
-
-    if (is(color, "ScaleDiscrete")) {
-        p <- p + color
-    }
-
-    if (identical(interestingGroups, "sampleName")) {
-        p <- p + guides(color = FALSE)
-    }
-
-    p
-}
-
-
-
-.plotGeneWide <- function(
-    object,
-    interestingGroups,
-    countsAxisLabel = "counts",
-    medianLine = TRUE,
-    color = NULL,
-    legend = TRUE
-) {
-    stopifnot(is(object, "SummarizedExperiment"))
-    stopifnot(length(rownames(object)) <= 50L)
-    interestingGroups <- matchInterestingGroups(
-        object = object,
-        interestingGroups = interestingGroups
-    )
-    interestingGroups(object) <- interestingGroups
-
-    object <- convertGenesToSymbols(object)
-    data <- .meltCounts(
-        counts = assay(object),
-        sampleData = sampleData(object)
-    )
-
-    p <- ggplot(
-        data = data,
-        mapping = aes(
-            x = !!sym("geneID"),
-            y = !!sym("counts"),
-            color = !!sym("interestingGroups")
-        )
-    ) +
-        .genePoint(show.legend = legend) +
-        labs(
-            x = NULL,
-            y = countsAxisLabel,
-            color = paste(interestingGroups, collapse = ":\n")
-        )
-
-    if (isTRUE(medianLine) && !identical(interestingGroups, "sampleName")) {
-        p <- p + .geneMedianLine
-    }
-
-    if (is(color, "ScaleDiscrete")) {
-        p <- p + color
-    }
-
-    p
-}
-
-
-
-#' @rdname plotGene
-#' @export
-setMethod(
-    "plotGene",
-    signature("SummarizedExperiment"),
+.plotGene.bcbioRNASeq <-  # nolint
     function(
         object,
         genes,
-        interestingGroups = NULL,
-        countsAxisLabel = "counts",
-        medianLine = TRUE,
-        color = getOption("bcbio.discrete.color", NULL),
-        legend = getOption("bcbio.legend", TRUE),
-        headerLevel = 2L,
-        return = c("facet", "wide")
-    ) {
-        validObject(object)
-        assert_is_character(genes)
-        interestingGroups <- matchInterestingGroups(
-            object = object,
-            interestingGroups = interestingGroups
-        )
-        interestingGroups(object) <- interestingGroups
-        assert_is_a_bool(medianLine)
-        assertIsColorScaleDiscreteOrNULL(color)
-        assert_is_a_bool(legend)
-        assertIsAHeaderLevel(headerLevel)
-        return <- match.arg(return)
-
-        # Coerce to RangedSummarizedExperiment
-        rse <- object %>%
-            as("RangedSummarizedExperiment") %>%
-            .[genes, , drop = FALSE]
-
-        if (return == "facet") {
-            fxn <- .plotGeneFacet
-        } else if (return == "wide") {
-            fxn <- .plotGeneWide
-        }
-
-        fxn(
-            object = rse,
-            interestingGroups = interestingGroups,
-            countsAxisLabel = countsAxisLabel,
-            medianLine = medianLine,
-            color = color,
-            legend = legend
-        )
-    }
-)
-
-
-
-#' @rdname plotGene
-#' @export
-setMethod(
-    "plotGene",
-    signature("bcbioRNASeq"),
-    function(
-        object,
-        normalized = c("vst", "rlog", "tmm", "tpm", "rle"),
-        ...
+        normalized = c("vst", "rlog", "tmm", "tpm", "rle")
     ) {
         validObject(object)
         normalized <- match.arg(normalized)
         counts <- counts(object, normalized = normalized)
-        # Ensure counts are log2 scale
+        # Ensure counts are always log2 scale.
         if (!normalized %in% c("rlog", "vst")) {
             counts <- log2(counts + 1L)
         }
-        countsAxisLabel <- paste(normalized, "counts (log2)")
         rse <- as(object, "RangedSummarizedExperiment")
-        assay(rse) <- counts
-        plotGene(
-            object = rse,
-            countsAxisLabel = countsAxisLabel,
-            ...
+        assays(rse) <- list(counts)
+        args <- setArgsToDoCall(
+            args = list(
+                object = rse,
+                genes = genes,
+                countsAxisLabel = paste(normalized, "counts (log2)")
+            ),
+            removeArgs = "normalized",
+            call = matchCall()
         )
+        do.call(what = plotGene, args = args)
     }
-)
+
+# Assign the formals.
+f1 <- formals(.plotGene.bcbioRNASeq)
+f2 <- methodFormals(f = "plotGene", signature = "SummarizedExperiment")
+f2 <- f2[setdiff(names(f2), c(names(f1), "countsAxisLabel"))]
+f <- c(f1, f2)
+formals(.plotGene.bcbioRNASeq) <- f
 
 
 
-#' @rdname plotGene
-#' @export
-setMethod(
-    "plotGene",
-    signature("DESeqDataSet"),
-    function(object, ...) {
+.plotGene.DESeqDataSet <-  # nolint
+    function() {
         validObject(object)
-        # Ensure counts are log2 scale
-        counts <- log2(counts(object, normalized = TRUE) + 1L)
+        counts <- counts(object, normalized = TRUE)
+        # Ensure counts are log2 scale.
+        counts <- log2(counts + 1L)
         rse <- as(object, "RangedSummarizedExperiment")
-        assay(rse) <- counts
-        plotGene(
-            object = rse,
-            countsAxisLabel = "normalized counts (log2)",
-            ...
+        assays(rse) <- list(counts)
+        args <- setArgsToDoCall(
+            args = list(
+                object = rse,
+                genes = genes,
+                countsAxisLabel = "normalized counts (log2)"
+            ),
+            removeArgs = "normalized",
+            call = matchCall()
         )
+        do.call(what = plotGene, args = args)
     }
-)
+
+# Assign the formals.
+f <- methodFormals(f = "plotGene", signature = "SummarizedExperiment")
+f <- f[setdiff(names(f), "countsAxisLabel")]
+formals(.plotGene.DESeqDataSet) <- f
 
 
 
-#' @rdname plotGene
-#' @export
-setMethod(
-    "plotGene",
-    signature("DESeqTransform"),
-    function(object, ...) {
+.plotGene.DESeqTransform <-  # nolint
+    function() {
         validObject(object)
         if ("rlogIntercept" %in% colnames(mcols(object))) {
             normalized <- "rlog"
         } else {
             normalized <- "vst"
         }
-        countsAxisLabel <- paste(normalized, "counts (log2)")
-        rse <- as(object, "RangedSummarizedExperiment")
-        plotGene(
-            object = rse,
-            countsAxisLabel = countsAxisLabel,
-            ...
+        args <- setArgsToDoCall(
+            args = list(
+                object = as(object, "RangedSummarizedExperiment"),
+                genes = genes,
+                countsAxisLabel = paste(normalized, "counts (log2)")
+            ),
+            call = matchCall()
         )
+        do.call(what = plotGene, args = args)
     }
+
+# Assign the formals.
+f <- methodFormals(f = "plotGene", signature = "SummarizedExperiment")
+f <- f[setdiff(names(f), "countsAxisLabel")]
+formals(.plotGene.DESeqTransform) <- f
+
+
+
+#' @rdname plotGene
+#' @export
+setMethod(
+    f = "plotGene",
+    signature = signature("bcbioRNASeq"),
+    definition = .plotGene.bcbioRNASeq
+)
+
+
+
+#' @rdname plotGene
+#' @export
+setMethod(
+    f = "plotGene",
+    signature = signature("DESeqTransform"),
+    definition = .plotGene.DESeqTransform
+)
+
+
+
+#' @rdname plotGene
+#' @export
+setMethod(
+    f = "plotGene",
+    signature = signature("DESeqDataSet"),
+    definition = .plotGene.DESeqDataSet
 )
