187dd1f2d7e39db6007981268d9fa35a9cd4601b
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-12 21:47:00 -0400

    Update NAMESPACE

--- R/bcbioRNASeq-package.R
+++ R/bcbioRNASeq-package.R
@@ -64,9 +64,9 @@
 #' @importFrom ggplot2 aes annotation_logticks coord_fixed coord_flip
 #'   element_blank element_text expand_limits facet_wrap geom_bar geom_boxplot
 #'   geom_density geom_hline geom_jitter geom_point geom_polygon geom_ribbon
-#'   geom_smooth geom_vline ggplot ggtitle guides labs position_jitterdodge
-#'   scale_color_manual scale_x_continuous scale_y_continuous stat_summary theme
-#'   xlab ylim
+#'   geom_smooth geom_violin geom_vline ggplot ggtitle guides labs
+#'   position_jitterdodge scale_color_manual scale_x_continuous
+#'   scale_y_continuous stat_summary theme xlab ylim
 #' @importFrom ggrepel geom_label_repel geom_text_repel
 #' @importFrom grid arrow unit
 #' @importFrom knitr kable
