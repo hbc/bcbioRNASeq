acc919ef233414b1c728d7a15560e843a0e7d613
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-12 22:48:34 -0400

    Improve internal `.meltCounts()` utilities

--- R/meltCounts-internal.R
+++ R/meltCounts-internal.R
@@ -1,35 +1,42 @@
-.filterNonzeroGenes <- function(object) {
-    assert_is_all_of(object, "bcbioRNASeq")
-    keep <- rowSums(counts(object)) > 0L
-    object <- object[keep, , drop = FALSE, transform = FALSE]
-    message(paste(nrow(object), "non-zero genes detected"))
-    object
-}
-
-
-
 .meltCounts <- function(object, normalized) {
     assert_is_all_of(object, "bcbioRNASeq")
     assert_is_a_string(normalized)
 
-    # Apply log2 transformation, if  necessary.
-    if (normalized %in% c("rlog", "vst")) {
-        log2Transform <- FALSE
-    } else {
-        log2Transform <- TRUE
-    }
+    # Generate a SummarizedExperiment subset with only non-zero counts.
+    # We're coercing to RSE here to enable fast subsetting on the object.
+    rse <- as(object, "RangedSummarizedExperiment")
+    # Note that we're comparing against the raw counts.
+    keep <- rowSums(counts(rse)) > 0L
+    rse <- rse[keep, , drop = FALSE]
+    message(paste(
+        nrow(rse), "/", nrow(object),
+        "non-zero genes detected"
+    ))
 
-    # Subset the object to only include non-zero genes.
-    nonzero <- .filterNonzeroGenes(object)
-    counts <- counts(nonzero, normalized = normalized)
-
-    # Using SummarizedExperiment method to return melted counts.
-    rse <- as(nonzero, "RangedSummarizedExperiment")
+    # Subset the desired normalized counts, and slot into our SE object.
+    counts <- counts(object, normalized = normalized) %>%
+        .[keep, , drop = FALSE]
     assays(rse) <- list(counts = counts)
-    data <- meltCounts(
-        object = rse,
-        log2Transform = log2Transform
-    )
+
+    # Apply scale transformation to counts axis, if necessary.
+    trans <- .normalizedTrans(normalized)
+
+    # Using our SummarizedExperiment method to return melted counts.
+    data <- meltCounts(object = rse, trans = trans)
     assert_is_all_of(data, "grouped_df")
+
     data
+}
+
+
+
+# Determine if we should apply a log transformation to our normalized counts
+# when plotting. Note that DESeqTransform are already log2 scale.
+.normalizedTrans <- function(normalized) {
+    if (normalized %in% c("rlog", "vst")) {
+        # Already log2 scale.
+        trans <- "identity"
+    } else {
+        trans <- "log2"
+    }
 }
