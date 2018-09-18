c2e854ab4d5ca1f10dbd0ea93dc02153ee7505ff
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-18 14:31:29 -0400

    Offload to basejump

--- R/plotGenesDetected-methods.R
+++ R/plotGenesDetected-methods.R
@@ -1,14 +1,10 @@
-# FIXME Include an option to use the mapped genes from STAR or the genes
-# detected from salmon output.
-# FIXME Explain the minCounts logic in more detail in the documentation.
-
-
-
 #' Plot Genes Detected
 #'
 #' @name plotGenesDetected
 #' @family Quality Control Functions
 #' @author Michael Steinbaugh, Rory Kirchner, Victor Barrera
+#' @importFrom basejump plotGenesDetected
+#' @inherit basejump::plotGenesDetected
 #' @export
 #'
 #' @inheritParams general
@@ -24,71 +20,7 @@ NULL
 #' @rdname plotGenesDetected
 #' @export
 setMethod(
-    "plotGenesDetected",
-    signature("bcbioRNASeq"),
-    function(
-        object,
-        interestingGroups = NULL,
-        limit = 0L,
-        minCounts = 1L,
-        fill = getOption("bcbio.discrete.fill", NULL),
-        flip = getOption("bcbio.flip", TRUE),
-        title = "genes detected"
-    ) {
-        validObject(object)
-        interestingGroups <- matchInterestingGroups(
-            object = object,
-            interestingGroups = interestingGroups
-        )
-        interestingGroups(object) <- interestingGroups
-        assertIsAnImplicitInteger(limit)
-        assert_all_are_non_negative(limit)
-        assertIsAnImplicitInteger(minCounts)
-        assert_all_are_in_range(minCounts, lower = 1L, upper = Inf)
-        assert_all_are_non_negative(minCounts)
-        assertIsFillScaleDiscreteOrNULL(fill)
-        assert_is_a_bool(flip)
-        assertIsAStringOrNULL(title)
-
-        counts <- counts(object, normalized = FALSE)
-        geneCount <- colSums(counts >= minCounts)
-
-        p <- metrics(object) %>%
-            mutate(geneCount = !!geneCount) %>%
-            ggplot(
-                mapping = aes(
-                    x = !!sym("sampleName"),
-                    y = !!sym("geneCount"),
-                    fill = !!sym("interestingGroups")
-                )
-            ) +
-            geom_bar(
-                color = "black",
-                stat = "identity"
-            ) +
-            labs(
-                title = title,
-                x = NULL,
-                y = "gene count",
-                fill = paste(interestingGroups, collapse = ":\n")
-            )
-
-        if (is_positive(limit)) {
-            p <- p + basejump_geom_abline(yintercept = limit)  # nocov
-        }
-
-        if (is(fill, "ScaleDiscrete")) {
-            p <- p + fill
-        }
-
-        if (isTRUE(flip)) {
-            p <- p + coord_flip()
-        }
-
-        if (identical(interestingGroups, "sampleName")) {
-            p <- p + guides(fill = FALSE)
-        }
-
-        p
-    }
+    f = "plotGenesDetected",
+    signature = signature("bcbioRNASeq"),
+    definition = getMethod("plotGenesDetected", "SummarizedExperiment")
 )
