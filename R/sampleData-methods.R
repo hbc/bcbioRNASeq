9372af11290e162dc0b4fa93f4837f6f923acaf9
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-23 19:09:49 -0400

    Simplify the clean return

--- R/sampleData-methods.R
+++ R/sampleData-methods.R
@@ -37,19 +37,15 @@ NULL
 
 .sampleData.bcbioRNASeq <-  # nolint
     function(object, clean = FALSE) {
-        data <- colData(object)
         assert_is_a_bool(clean)
-        # Only return factor columns, if desired.
+        rse <- as(object, "RangedSummarizedExperiment")
+        data <- sampleData(rse)
+        # Only return whitelisted factor columns, if desired.
         if (isTRUE(clean)) {
             data <- data[, vapply(data, is.factor, logical(1L)), drop = FALSE]
             # Drop remaining blacklisted columns.
             setdiff <- setdiff(colnames(data), metadataBlacklist)
             data <- data[, setdiff, drop = FALSE]
-        } else {
-            interestingGroups <- interestingGroups(object)
-            if (length(interestingGroups) > 0L) {
-                data <- uniteInterestingGroups(data, interestingGroups)
-            }
         }
         data
     }
