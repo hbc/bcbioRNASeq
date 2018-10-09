0a812375e350950326bf24a10f7ce5640921ea27
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-10-08 20:48:09 -0400

    Improve `alphaSummary()` to return integer matrix

--- R/alphaSummary-methods.R
+++ R/alphaSummary-methods.R
@@ -17,8 +17,6 @@
 #' @inheritParams general
 #' @inheritParams DESeq2::results
 #' @param alpha `numeric`. Multiple alpha cutoffs.
-#' @param caption `string` or `NULL`. Table caption. If set `NULL`, will be
-#'   generated automatically from the contrast used.
 #'
 #' @return `integer matrix`.
 #'
@@ -44,14 +42,12 @@ NULL
         object,
         alpha = c(0.1, 0.05, 0.01, 1e-3, 1e-6),
         contrast = NULL,
-        name = NULL,
-        caption = NULL
+        name = NULL
     ) {
         validObject(object)
         assert_is_numeric(alpha)
         assert_is_any_of(contrast, c("character", "NULL"))
         assertIsAStringOrNULL(name)
-        assertIsAStringOrNULL(caption)
 
         # Either `contrast` or `name`.
         # If neither are defined, we're checking the intercept.
@@ -60,22 +56,21 @@ NULL
         }
 
         # Generate an automatic caption.
-        if (is.null(caption)) {
-            if (!is.null(contrast)) {
-                caption <- paste(contrast, collapse = " ")
-            } else if (!is.null(name)) {
-                caption <- name
-            } else {
-                caption <- resultsNames(object)[[1L]]
-            }
+        if (!is.null(contrast)) {
+            caption <- paste(contrast, collapse = " ")
+        } else if (!is.null(name)) {
+            caption <- name
+        } else {
+            caption <- resultsNames(object)[[1L]]
         }
+
         message(paste(
             caption,
             paste("Alpha cutoffs:", toString(alpha)),
             sep = "\n"
         ))
 
-        data <- sapply(
+        data <- vapply(
             X = alpha,
             FUN = function(alpha) {
                 args <- list(
@@ -91,24 +86,20 @@ NULL
                 # Keep only the summary lines that contain a colon.
                 output <- output[grepl(" : ", output)]
                 # Extract the values after the colon in summary.
-
-                x <- str_match(
+                match <- str_match(
                     string = output,
-                    pattern = "^(.+)\\s\\:\\s(.+)$"
+                    pattern = "^(.+)\\s\\:\\s([[:digit:]]+).*$"
                 )
-                names <- gsub("\\s+$", "", x[, 2L])
-                values <- x[, 3L]
+                names <- gsub("\\s+$", "", match[, 2L])
+                values <- as.integer(match[, 3L])
                 names(values) <- names
                 values
-            }
+            },
+            FUN.VALUE = integer(4L)
         )
-        # Coerce to integer matrix.
-        data <- data %>%
-            as.data.frame(stringsAsFactors = FALSE) %>%
-            set_colnames(as.character(alpha))
+        colnames(data) <- alpha
 
-        kable(data, caption = caption)
-
+        data
     }
 
 
