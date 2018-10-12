e399ad9abdeb2be635e0259a5e4e7cc9adb3c33d
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-12 14:34:35 -0400

    Rename `coerce` documentation Rd file to `as`

--- R/as-methods.R
+++ R/as-methods.R
@@ -1,17 +1,34 @@
-#' Methods for Coercing an Object to a Class
-#'
-#' @name coerce
-#' @aliases as
-#' @family S4 Functions
-#' @author Michael Steinbaugh
+#' @name as
+#' @aliases coerce
+#' @inherit methods::as
 #' @importFrom methods coerce
 #' @exportMethod coerce
+#' @author Michael Steinbaugh
 #'
-#' @return Object of new class.
+#' @section bcbioRNASeq to DESeqDataSet:
 #'
-#' @seealso
-#' - [methods::as()].
-#' - [methods::canCoerce()].
+#' 1. Coerces to `RangedSummarizedExperiment`.
+#' 2. Rounds raw counts to `integer matrix`.
+#' 3. Subsets [colData()] to include only clean factor columns. See
+#'    [sampleData()] for more details.
+#' 4. Simplifies [metadata()] to include only relevant information and
+#'    updates `sessionInfo`.
+#'
+#' @section bcbioRNASeq to DESeqTransform:
+#'
+#' 1. Coerces to `DESeqDataSet`.
+#' 2. Calls [DESeq2::DESeq2()].
+#' 3. Calls [DESeq2::varianceStabilizingTransformation()].
+#'
+#' @section DESeqAnalysis:
+#'
+#' Supported coercion methods will extract any of these internal objects:
+#'
+#' - `DESeqDataSet`.
+#' - `DESeqTransform`.
+#' - `DESeqResults`. Extracts the first results slotted. Note that this
+#'   corresponds to results containing log2 fold change (LFC) values that
+#'   *have not been shrunken* using [DESeq2::lfcShrink()].
 #'
 #' @examples
 #' ## bcbioRNASeq to DESeqDataSet ====
@@ -47,13 +64,17 @@ NULL
 
 
 # FIXME Add this to documentation as a section.
+# FIXME Minimize the metadata we're passing through.
+# FIXME Ensure sessionInfo is updated.
+
 # Note that gene-level counts are required. Mention `summarizeToGene()`.
 # By default, we're using length-scaled TPM, so a corresponding average
 # transcript length matrix isn't necessary. The average transcript length matrix
 # is only necessary when raw counts matrix isn't scaled during tximport call
 # (see `countsFromAbundance`).
 # @seealso `tximport::tximport`, `DESeq2::DESeqDataSetFromTximport()`.
-#' @rdname coerce
+
+#' @rdname as
 #' @name coerce,bcbioRNASeq,DESeqDataSet-method
 setAs(
     from = "bcbioRNASeq",
@@ -61,10 +82,7 @@ setAs(
     function(from) {
         validObject(from)
         se <- as(from, "RangedSummarizedExperiment")
-        # Don't include the metrics columns inside bcbioRNASeq object.
         colData(se) <- sampleData(from, clean = TRUE)
-        # FIXME Minimize the metadata we're passing through.
-        # FIXME Ensure sessionInfo is updated.
         validObject(se)
         to <- .new.DESeqDataSet(se = se)
         interestingGroups(to) <- interestingGroups(from)
@@ -75,18 +93,17 @@ setAs(
 
 
 
-#' @rdname coerce
+#' @rdname as
 #' @name coerce,bcbioRNASeq,DESeqTransform-method
 setAs(
     from = "bcbioRNASeq",
     to = "DESeqTransform",
     function(from) {
         validObject(from)
-        # First, generate a DESeqDataSet.
         dds <- as(from, "DESeqDataSet")
+        # Expect warning about empty design formula.
+        dds <- suppressWarnings(DESeq(dds))
         validObject(dds)
-        # Coerce using a new on-the-fly calculation.
-        # We're using VST here because it's faster than rlog.
         message("Applying variance stabilizing transformation.")
         dt <- varianceStabilizingTransformation(dds)
         validObject(dt)
@@ -96,7 +113,7 @@ setAs(
 
 
 
-#' @rdname coerce
+#' @rdname as
 #' @name coerce,DESeqAnalysis,DESeqDataSet-method
 setAs(
     from = "DESeqAnalysis",
@@ -111,7 +128,7 @@ setAs(
 
 
 
-#' @rdname coerce
+#' @rdname as
 #' @name coerce,DESeqAnalysis,DESeqTransform-method
 setAs(
     from = "DESeqAnalysis",
@@ -126,7 +143,7 @@ setAs(
 
 
 
-#' @rdname coerce
+#' @rdname as
 #' @name coerce,DESeqAnalysis,DESeqResults-method
 setAs(
     from = "DESeqAnalysis",
