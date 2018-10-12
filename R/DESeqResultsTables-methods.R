9436bee8709f66bb869d5b3cccc7014ef565cdc6
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-12 13:50:42 -0400

    Improve documentation

--- R/DESeqResultsTables-methods.R
+++ R/DESeqResultsTables-methods.R
@@ -1,10 +1,50 @@
 #' `DESeqResultsTables` Generator
 #'
-#' @note Log fold change cutoff threshold ("`lfcThreshold`") does not apply to
-#'   statistical hypothesis testing, only gene filtering in the results tables.
-#'   See [DESeq2::results()] for additional information about using
-#'   `lfcThreshold` and `altHypothesis` to set an alternative hypothesis based
-#'   on expected fold changes.
+#' @section Obtaining results from DESeq2:
+#'
+#' It is recommended to specify the `contrast` argument as `character`:
+#'
+#' 1. Design matrix factor of interest.
+#' 2. Numerator for LFC (expt).
+#' 3. Denominator for LFC (control).
+#'
+#' For example, to get the relative change in gene expression between mutant
+#' and wildtype samples, here's how to set the contrast vector:
+#'
+#' ```
+#' c(
+#'     factor = "genotype",
+#'     numerator = "mutant",
+#'     denominator = "wildtype"
+#' )
+#' ```
+#'
+#' @section Log fold change threshold cutoffs:
+#'
+#' Refer to [DESeq2::results()] for additional information about using
+#' `lfcThreshold` and `altHypothesis` to set an alternative hypothesis based on
+#' expected fold changes. In addition, the "Hypothesis tests with thresholds on
+#' effect size" section in the DESeq2 paper provides additional explanation.
+#'
+#' Don't apply *post hoc* LFC threshold filtering to obtain results tables, as
+#' this will destroy the meaning of the adjusted *P* values computed. If you are
+#' expecting a biological effect size past a particular threshold, set
+#' `lfcThreshold`, but be conservative.
+#'
+#' This [thread][] on the Bioconductor forums explains how [DESeq2::results()]
+#' should be called with regard to LFC cutoffs in nice detail. In particular,
+#' refer to Mike Love:
+#'
+#' "A common procedure is to disregard genes whose estimated LFC *β ir* is below
+#' some threshold, *β ir ≤ θ*. However, this approach loses the benefit of an
+#' easily interpretable FDR, as the reported *P* value and adjusted *P* value
+#' still correspond to the test of *zero* LFC. It is therefore desirable to
+#' include the threshold in the statistical testing procedure directly, i.e.,
+#' not to filter post hoc on a reported fold-change *estimate*, but rather to
+#' evaluate statistically directly whether there is sufficient evidence that the
+#' LFC is above the chosen threshold."
+#'
+#' [thread]: https://support.bioconductor.org/p/101504/
 #'
 #' @name DESeqResultsTables
 #' @family S4 Generators
@@ -19,8 +59,8 @@
 #' @return `DESeqResultsTables`.
 #'
 #' @seealso
-#' - [markdown()].
-#' - [write()].
+#' - [DESeq2::results()].
+#' - [markdown()], [write()].
 #'
 #' @examples
 #' data(deseq_small)
