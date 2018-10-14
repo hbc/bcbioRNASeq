347ff189581acc5722e196a444cfeacdef09e963
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-14 11:30:03 -0400

    Improve consistency of show methods

--- R/show-methods.R
+++ R/show-methods.R
@@ -1,4 +1,5 @@
-# TODO Limit the length of file paths.
+# FIXME Standardize this across all classes.
+# TODO Consider limiting the length of file paths.
 
 
 
@@ -9,118 +10,70 @@
 #' @export
 #'
 #' @examples
-#' data(bcb_small)
+#' data(bcb_small, deseq_small)
 #' show(bcb_small)
+#' show(deseq_small)
 NULL
 
+
+
+.showHeader <- function(object, version = NULL) {
+    cat(c(
+        bold(paste(class(object), version)),
+        italic("http://bioinformatics.sph.harvard.edu/bcbioRNASeq"),
+        "citation(\"bcbioRNASeq\")"
+    ), sep = "\n")
+}
 
 
 .show.bcbioRNASeq <-  # nolint
     function(object) {
         validObject(object)
+        # Metadata.
+        m <- metadata(object)
+        # Row ranges metadata.
+        rrm <- metadata(rowRanges(object))
+        .showHeader(object, version = m[["version"]])
+        showSlotInfo(list(
+            uploadDir = m[["uploadDir"]],
+            dates = as.character(c(
+                bcbio = m[["runDate"]],
+                R = m[["date"]]
+            )),
+            level = m[["level"]],
+            caller = m[["caller"]],
+            sampleMetadataFile = m[["sampleMetadataFile"]],
+            organism = m[["organism"]],
+            gffFile = m[["gffFile"]],
+            annotationHub = rrm[["annotationHub"]],
+            ensemblRelease = rrm[["release"]],
+            genomeBuild = rrm[["build"]],
+            interestingGroups = m[["interestingGroups"]]
+        ))
 
-        # Extend the RangedSummarizedExperiment method
-        rse <- as(object, "RangedSummarizedExperiment")
-
-        return <- c(
-            bold(paste(class(object), metadata(object)[["version"]])),
-            "http://bioinformatics.sph.harvard.edu/bcbioRNASeq",
-            "citation(\"bcbioRNASeq\")",
-            separatorBar,
-            paste(
-                bold("Upload Dir:"),
-                deparse(metadata(object)[["uploadDir"]])
-            ),
-            paste(
-                bold("Upload Date:"),
-                metadata(object)[["runDate"]]
-            ),
-            paste(
-                bold("R Load Date:"),
-                metadata(object)[["date"]]
-            ),
-            paste(
-                bold("Level:"),
-                deparse(metadata(object)[["level"]])
-            ),
-            paste(
-                bold("Caller:"),
-                deparse(metadata(object)[["caller"]])
-            ),
-            paste(
-                bold("Organism:"),
-                deparse(metadata(object)[["organism"]])
-            ),
-            paste(
-                bold("Interesting Groups:"),
-                deparse(metadata(object)[["interestingGroups"]])
-            )
-        )
-
-        # sampleMetadataFile
-        sampleMetadataFile <- metadata(object)[["sampleMetadataFile"]]
-        if (length(sampleMetadataFile)) {
-            return <- c(
-                return,
-                paste(bold("Metadata File:"), deparse(sampleMetadataFile))
-            )
-        }
-
-        # Gene annotations
-        m <- metadata(object)[["rowRangesMetadata"]]
-        if (is.data.frame(m) && length(m)) {
-            annotationHub <-
-                m[m[["name"]] == "id", "value", drop = TRUE]
-            ensemblRelease <-
-                m[m[["name"]] == "ensembl_version", "value", drop = TRUE]
-            genomeBuild <-
-                m[m[["name"]] == "genome_build", "value", drop = TRUE]
-            return <- c(
-                return,
-                paste(bold("AnnotationHub:"), deparse(annotationHub)),
-                paste(bold("Ensembl Release:"), deparse(ensemblRelease)),
-                paste(bold("Genome Build:"), deparse(genomeBuild))
-            )
-        }
-
-        # GFF File
-        gffFile <- metadata(object)[["gffFile"]]
-        if (length(gffFile)) {
-            return <- c(
-                return,
-                paste(bold("GFF File:"), deparse(gffFile))
-            )
-        }
-
-        # Include standard `SummarizedExperiment` information.
-        return <- c(
-            return,
-            separatorBar,
-            capture.output(show(rse))
-        )
-
-        cat(return, sep = "\n")
+        # Extend the standard RangedSummarizedExperiment method.
+        cat(capture.output(show(as(object, "RangedSummarizedExperiment"))))
     }
 
 
 
 .show.DESeqAnalysis <-  # nolint
     function(object) {
-        version <- metadata(object@data)[["version"]]
+        validObject(object)
+        .showHeader(
+            object = object,
+            version = metadata(object@data)[["version"]]
+        )
         contrastNames <- vapply(
             X = object@results,
             FUN = contrastName,
             FUN.VALUE = character(1L)
         )
-        return <- c(
-            bold(paste(class(object), version)),
-            paste(bold("Transform:"), .transformType(object@transform)),
-            bold(paste0("Results (", length(contrastNames), "):")),
-            paste0("  - ", contrastNames),
-            separatorBar,
-            capture.output(show(object@data))
-        )
-        cat(return, sep = "\n")
+        showSlotInfo(list(
+            transform = .transformType(object@transform),
+            contrastNames = contrastNames
+        ))
+        cat(capture.output(show(object@data)))
     }
 
 
@@ -128,6 +81,7 @@ NULL
 .show.DESeqResultsTables <-  # nolint
     function(object) {
         validObject(object)
+        .showHeader(object)
 
         all <- slot(object, "all")
         up <- slot(object, "degUp")
@@ -135,59 +89,39 @@ NULL
 
         contrast <- contrastName(all)
         alpha <- metadata(all)[["alpha"]]
-        lfc <- metadata(all)[["lfcThreshold"]]
+        lfcThreshold <- metadata(all)[["lfcThreshold"]]
 
-        summary <- capture.output(summary(all))
-        # Remove leading and trailing whitespace.
-        summary <- summary[!grepl("^$", summary)]
-        # Remove the lines about results documentation.
-        summary <- summary[!grepl("\\?results$", summary)]
-
-        # Get back the number of genes that have adjusted P values.
-        nPadj <- sum(!is.na(all[["padj"]]))
-
-        # Base mean information.
-        nBaseMeanGT1 <- nrow(all[all[["baseMean"]] > 1L, , drop = FALSE])
-
-        return <- c(
-            bold(class(object)),
-            paste(bold("Contrast:"), contrast),
-            paste(bold("Alpha:"), alpha),
-            paste(bold("LFC threshold:"), lfc)
+        list <- list(
+            contrast = contrast,
+            alpha = alpha,
+            lfcThreshold = lfcThreshold
         )
 
-        # Include the file paths, if they're set.
+        # Include file paths, if they're stashed (from `export()`).
         if (length(object@dropboxFiles) > 0L) {
+            name <- "dropbox export"
             paths <- object@dropboxFiles
-            prefix <- "Dropbox"
         } else if (length(object@localFiles) > 0L) {
+            name <- "export"
             paths <- object@localFiles
-            prefix <- "Local"
         } else {
             paths <- character()
         }
         if (length(paths) > 0L) {
             dirname <- unique(dirname(paths))
             assert_is_a_string(dirname)
-            return <- c(
-                return,
-                paste(bold(paste(prefix, "files:")), dirname)
-            )
+            list[[name]] <- dirname
         }
 
-        return <- c(
-            return,
-            separatorBar,
-            paste(nrow(all), "genes total"),
-            paste(nBaseMeanGT1, "genes with base mean > 1"),
-            paste(nPadj, "genes with adjusted P values"),
-            paste(nrow(up), "upregulated genes"),
-            paste(nrow(down), "downregulated genes"),
-            separatorBar,
-            summary
-        )
+        showSlotInfo(list)
 
-        cat(return, sep = "\n")
+        # Include DESeqResults summary.
+        summary <- capture.output(summary(all)) %>%
+            # Remove leading and trailing whitespace.
+            .[!grepl("^$", .)] %>%
+            # Remove the lines about results documentation.
+            .[!grepl("\\?results$", .)]
+        cat(summary, sep = "\n")
     }
 
 
