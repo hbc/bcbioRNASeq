f5f9afc1b719a1d182ae790ce8873bb01b2af474
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-10-17 12:56:25 -0400

    Kill helper globals

--- tests/testthat/helper_globals.R
+++ tests/testthat/helper_globals.R
@@ -6,14 +6,13 @@
 #     bcbio.discrete.fill = ggplot2::scale_fill_viridis_d()
 # )
 
-data(bcb_small, deseq_small, envir = environment())
-
-dds_small <- as(deseq_small, "DESeqDataSet")
-vst_small <- as(deseq_small, "DESeqTransform")
-res_small <- as(deseq_small, "DESeqResults")
-res_tables <- DESeqResultsTables(res_small)
+# data(bcb_small, deseq_small, envir = environment())
+# dds_small <- as(deseq_small, "DESeqDataSet")
+# vst_small <- as(deseq_small, "DESeqTransform")
+# res_small <- as(deseq_small, "DESeqResults")
+# res_tables <- DESeqResultsTables(res_small)
 
 # FIXME Set this manually, not as a global
-lfc <- 0.25
+# lfc <- 0.25
 
 # nolint end
