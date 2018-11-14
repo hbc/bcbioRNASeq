4361d9a0b0ef05923bce3854cbe6af44e3bf363d
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-13 22:30:24 -0500

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -19,10 +19,10 @@
 #'   estimateSizeFactors fpkm priorInfo results resultsNames rlog
 #'   varianceStabilizingTransformation
 #' @importFrom GenomicFeatures genes makeTxDbFromGFF transcripts
-#' @importFrom S4Vectors as.data.frame complete.cases DataFrame head mcols
+#' @importFrom S4Vectors DataFrame Rle as.data.frame complete.cases head mcols
 #'   mcols<- metadata metadata<- na.omit
-#' @importFrom SummarizedExperiment assay assay<- assayNames assayNames<- assays
-#'   assays<- colData colData<- rowData rowRanges SummarizedExperiment
+#' @importFrom SummarizedExperiment SummarizedExperiment assay assay<-
+#'   assayNames assayNames<- assays assays<- colData colData<- rowData rowRanges
 #' @importFrom assertive.base assert_all_are_true assert_are_identical
 #' @importFrom assertive.files assert_all_are_dirs assert_all_are_existing_files
 #' @importFrom assertive.numbers assert_all_are_greater_than
@@ -31,9 +31,10 @@
 #' @importFrom assertive.properties assert_has_colnames assert_has_dimnames
 #'   assert_has_dims assert_has_names assert_has_no_duplicates assert_has_rows
 #'   assert_is_non_empty assert_is_of_length assert_is_scalar assert_is_vector
-#'   has_dims has_names
-#' @importFrom assertive.sets assert_are_disjoint_sets
+#'   has_dimnames has_dims has_names
+#' @importFrom assertive.sets are_disjoint_sets assert_are_disjoint_sets
 #'   assert_are_intersecting_sets assert_are_set_equal assert_is_subset
+#'   is_subset
 #' @importFrom assertive.strings assert_all_are_matching_regex
 #'   assert_all_are_non_empty_character
 #' @importFrom assertive.types assert_is_a_bool assert_is_a_number
@@ -44,15 +45,15 @@
 #' @importFrom assertthat assert_that validate_that
 #' @importFrom basejump Gene2Symbol Tx2Gene assertFormalGene2Symbol
 #'   basejump_geom_abline basejump_geom_label basejump_geom_label_repel camel
-#'   checkClasses coerceS4ToList convertGenesToSymbols detectLanes
-#'   detectOrganism emptyRanges import initDir interestingGroups
-#'   interestingGroups<- makeGRangesFromEnsembl makeGRangesFromGFF makeNames
-#'   makeSummarizedExperiment mapGenesToRownames markdownHeader markdownList
-#'   markdownPlotlist matchArgsToDoCall matchInterestingGroups meltCounts
-#'   methodFormals metrics organism plotGenesDetected plotHeatmap
-#'   prepareTemplate printString realpath removeNA sampleData sampleData<-
-#'   sanitizeRowData sanitizeSampleData separator showSlotInfo snake
-#'   standardizeCall stripTranscriptVersions uniteInterestingGroups
+#'   coerceS4ToList convertGenesToSymbols detectLanes detectOrganism emptyRanges
+#'   import initDir interestingGroups interestingGroups<- makeGRangesFromEnsembl
+#'   makeGRangesFromGFF makeNames makeSummarizedExperiment mapGenesToRownames
+#'   markdownHeader markdownList markdownPlotlist matchArgsToDoCall
+#'   matchInterestingGroups meltCounts methodFormals metrics organism
+#'   plotGenesDetected plotHeatmap prepareTemplate printString realpath removeNA
+#'   sampleData sampleData<- sanitizeRowData sanitizeSampleData separator
+#'   showSlotInfo snake standardizeCall stripTranscriptVersions
+#'   uniteInterestingGroups validateClasses
 #' @importFrom bcbioBase copyToDropbox getGTFFileFromYAML getMetricsFromYAML
 #'   getSampleDataFromYAML projectDir readDataVersions readProgramVersions
 #'   readSampleData readTx2Gene runDate sampleDirs
@@ -60,7 +61,7 @@
 #' @importFrom dplyr arrange bind_cols desc everything filter group_by left_join
 #'   mutate mutate_all mutate_if pull rename row_number select select_if
 #'   starts_with
-#' @importFrom edgeR calcNormFactors cpm DGEList
+#' @importFrom edgeR DGEList calcNormFactors cpm
 #' @importFrom ggplot2 aes annotation_logticks coord_fixed coord_flip
 #'   element_blank element_text expand_limits facet_wrap geom_bar geom_boxplot
 #'   geom_density geom_hline geom_jitter geom_point geom_polygon geom_ribbon
@@ -81,7 +82,7 @@
 #' @importFrom methods .hasSlot as as<- is new setAs show slot slot<-
 #'   validObject
 #' @importFrom readr read_csv read_tsv write_csv
-#' @importFrom rlang := !! !!! has_length sym syms UQ
+#' @importFrom rlang !! !!! := UQ has_length sym syms
 #' @importFrom scales pretty_breaks
 #' @importFrom sessioninfo session_info
 #' @importFrom stringr str_match str_trunc
