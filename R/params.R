720c0dd5feb13550eb7cc95100fc1b5dbcb6671a
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-20 11:00:16 -0500

    Move these global params to DESeqAnalysis package

--- R/params.R
+++ R/params.R
@@ -1,11 +1,9 @@
 #' @name params
 #' @inherit basejump::params
 #' @keywords internal
-#' @param lfcShrink `boolean`. Use shrunken log2 fold change (LFC) values.
 #' @param normalized `string`. Which normalization method to apply:
 #'   - "`tpm`": Transcripts per million (tximport).
 #'   - "`tmm`": edgeR trimmed mean of M-values. Calculated on the fly.
 #'   - "`rlog`": DESeq2 **log2** regularized log transformation.
 #'   - "`vst`": DESeq2 **log2** variance stabilizing transformation.
-#' @param results `scalar`. Position or name of `DESeqResults`.
 NULL
