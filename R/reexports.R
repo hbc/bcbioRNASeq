6b26a53801b62bf313055ddc7a113c1fbf6634ee
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-25 07:56:39 -0500

    Include additional reexports

--- R/reexports.R
+++ R/reexports.R
@@ -1,3 +1,15 @@
+#' @importFrom basejump Gene2Symbol
+#' @export
+basejump::Gene2Symbol
+
+#' @importFrom basejump sampleData
+#' @export
+basejump::sampleData
+
+#' @importFrom basejump sampleData<-
+#' @export
+basejump::`sampleData<-`
+
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
