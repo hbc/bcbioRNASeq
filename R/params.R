8de1971f77dba32c0c07953686512c9ab9f12f27
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-12 10:01:33 -0500

    Rework documentation and NAMESPACE

--- R/params.R
+++ R/params.R
@@ -1,7 +1,7 @@
 #' @name params
 #' @inherit basejump::params
 #' @keywords internal
-#' @param normalized `string`. Which normalization method to apply:
+#' @param normalized `character(1)`. Which normalization method to apply:
 #'   - "`tpm`": Transcripts per million (tximport).
 #'   - "`tmm`": edgeR trimmed mean of M-values. Calculated on the fly.
 #'   - "`rlog`": DESeq2 **log2** regularized log transformation.
