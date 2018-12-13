f968b9d3d583a8ea9af1dd8e4076c4b0746ef9ac
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-13 06:40:47 -0500

    Update _setup.R [ci skip]

--- inst/rmarkdown/shared/_setup.R
+++ inst/rmarkdown/shared/_setup.R
@@ -1,19 +1,13 @@
-library(knitr)
-library(rmarkdown)
-library(basejump)
-library(bcbioRNASeq)
-library(DESeq2)
-library(tidyverse)
-
 # Set seed for reproducibility.
 set.seed(1454944673L)
 
-opts_chunk[["set"]](
+knitr::opts_chunk[["set"]](
     autodep = TRUE,
     bootstrap.show.code = FALSE,
     # Enable caching with caution.
     cache = FALSE,
     cache.lazy = FALSE,
+    comment = "",
     dev = c("png", "pdf"),
     fig.height = 10L,
     fig.retina = 2L,
@@ -23,8 +17,8 @@ opts_chunk[["set"]](
     tidy = FALSE
 )
 
-theme_set(
-    theme_paperwhite(
+ggplot2::theme_set(
+    basejump::theme_paperwhite(
         base_size = 14L,
         legend_position = "right"
     )
