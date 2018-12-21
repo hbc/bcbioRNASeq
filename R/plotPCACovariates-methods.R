e4c9ea949d6a3142ae3f458538ef7e88a6e66d1f
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-21 11:53:14 -0500

    Update `plotPCACovariates()`

---
+++ R/plotPCACovariates-methods.R
@@ -0,0 +1,93 @@
+#' @name plotPCACovariates
+#' @author Lorena Pantano, Michael Steinbaugh, Rory Kirchner
+#' @inherit bioverbs::plotPCACovariates
+#' @inheritParams params
+#'
+#' @description [DEGreport::degCovariates()] wrapper supporting `bcbioRNASeq`
+#'   class.
+#'
+#' @param metrics `boolean`. Include sample summary metrics as covariates.
+#'   Defaults to include all metrics columns (`TRUE`), but desired columns can
+#'   be specified here as a character vector.
+#' @param ... Additional arguments, passed to [DEGreport::degCovariates()].
+#'
+#' @seealso
+#' - [DEGreport::degCovariates()].
+#' - [DESeq2::rlog()].
+#' - [DESeq2::varianceStabilizingTransformation()].
+NULL
+
+
+
+#' @importFrom bioverbs plotPCACovariates
+#' @aliases NULL
+#' @export
+bioverbs::plotPCACovariates
+
+
+
+plotPCACovariates.bcbioRNASeq <-  # nolint
+    function(
+    object,
+    metrics = TRUE,
+    normalized,
+    ...
+) {
+    validObject(object)
+    assert(isAny(metrics, c("character", "logical")))
+    normalized <- match.arg(normalized)
+
+    # Get the normalized counts.
+    counts <- counts(object, normalized = normalized)
+
+    # Note that we're ungrouping the tibble here, otherwise the downstream
+    # factor/numeric column selection won't work as expected.
+    data <- ungroup(metrics(object))
+    factors <- select_if(data, is.factor)
+    # Drop columns that are all zeroes (not useful to plot).
+    # @roryk Sometimes we are missing values for some samples but not
+    # others; plotPCACovariates was failing in those cases when checking if
+    # a column was a numeric. Here we ignore the NAs for numeric column
+    # checking. Adding the `na.rm` here fixes the issue.
+    numerics <- data %>%
+        select_if(is.numeric) %>%
+        .[, colSums(., na.rm = TRUE) > 0L, drop = FALSE]
+    metadata <- cbind(factors, numerics)
+    rownames(metadata) <- data[["sampleID"]]
+
+    # Select the metrics to use for plot.
+    if (identical(metrics, TRUE)) {
+        # Sort columns alphabetically.
+        col <- sort(colnames(metadata))
+    } else if (identical(metrics, FALSE)) {
+        # Use the defined interesting groups.
+        col <- interestingGroups(object)
+    } else if (is.character(metrics)) {
+        col <- metrics
+    }
+
+    if (length(col) < 2L) {
+        stop("plotPCACovariates() requires >= 2 metrics.")
+    }
+    assert(isSubset(col, colnames(metadata)))
+    metadata <- metadata[, col, drop = FALSE]
+
+    # DEGreport v1.18 has issues joining character vector and factor columns:
+    # - Warning: Column `compare`/`PC`
+    # - Warning: Column `covar`/`term`
+    suppressWarnings(
+        degCovariates(counts = counts, metadata = metadata, ...)
+    )
+}
+
+formals(plotPCACovariates.bcbioRNASeq)[["normalized"]] <- normalizedCounts
+
+
+
+#' @rdname plotPCACovariates
+#' @export
+setMethod(
+    f = "plotPCACovariates",
+    signature = signature("bcbioRNASeq"),
+    definition = plotPCACovariates.bcbioRNASeq
+)
