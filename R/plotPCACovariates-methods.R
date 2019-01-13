9f6ae82c266bff833f37278edb6869a48a53e046
Michael Steinbaugh <mike@steinbaugh.com>
2019-01-13 18:57:57 -0500

    Bug fix using method code from bcbioRNASeq v0.3.5 development branch

--- R/plotPCACovariates-methods.R
+++ R/plotPCACovariates-methods.R
@@ -43,37 +43,46 @@ setMethod(
         assert_is_any_of(metrics, c("character", "logical"))
         normalized <- match.arg(normalized)
 
+        # Get the normalized counts.
         counts <- counts(object, normalized = normalized)
 
-        metadata <- metrics(object)
-        factors <- select_if(metadata, is.factor)
-        numerics <- select_if(metadata, is.numeric) %>%
-            # Drop columns that are all zeroes (not useful to plot)
-            .[, colSums(., na.rm=TRUE) > 0L, drop = FALSE]
+        # Note that we're ungrouping the tibble here, otherwise the downstream
+        # factor/numeric column selection won't work as expected.
+        data <- ungroup(metrics(object))
+        factors <- select_if(data, is.factor)
+        # Drop columns that are all zeroes (not useful to plot).
+        # @roryk Sometimes we are missing values for some samples but not
+        # others; plotPCACovariates was failing in those cases when checking if
+        # a column was a numeric. Here we ignore the NAs for numeric column
+        # checking. Adding the `na.rm` here fixes the issue.
+        numerics <- data %>%
+            select_if(is.numeric) %>%
+            .[, colSums(., na.rm = TRUE) > 0L, drop = FALSE]
         metadata <- cbind(factors, numerics)
+        rownames(metadata) <- data[["sampleID"]]
 
-        # Select the metrics to use for plot
+        # Select the metrics to use for plot.
         if (identical(metrics, TRUE)) {
-            # Sort columns alphabetically
+            # Sort columns alphabetically.
             col <- sort(colnames(metadata))
         } else if (identical(metrics, FALSE)) {
-            # Use the defined interesting groups
+            # Use the defined interesting groups.
             col <- interestingGroups(object)
         } else if (is.character(metrics)) {
             col <- metrics
         }
 
-        assert_is_subset(col, colnames(metadata))
-        # Stop on 1 metrics column
         if (length(col) < 2L) {
-            stop("`plotPCACovariates()` requires >= 2 metrics")
+            stop("plotPCACovariates() requires >= 2 metrics.")
         }
+        assert_is_subset(col, colnames(metadata))
         metadata <- metadata[, col, drop = FALSE]
 
-        degCovariates(
-            counts = counts,
-            metadata = metadata,
-            ...
+        # DEGreport v1.18 has issues joining character and factor columns.
+        # - Warning: Column `compare`/`PC`
+        # - Warning: Column `covar`/`term`
+        suppressWarnings(
+            degCovariates(counts = counts, metadata = metadata, ...)
         )
     }
 )
