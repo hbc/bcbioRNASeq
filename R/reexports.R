939c31d3f275eb4977e9cad372f2151aa23c4f3d
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-28 17:12:54 -0400

    Update NAMESPACE required for vignette to knit

---
+++ R/reexports.R
@@ -0,0 +1,63 @@
+#' @importFrom basejump export
+#' @export
+basejump::export
+
+#' @importFrom basejump import
+#' @export
+basejump::import
+
+#' @importFrom basejump interestingGroups
+#' @export
+basejump::interestingGroups
+
+#' @importFrom basejump interestingGroups<-
+#' @export
+basejump::`interestingGroups<-`
+
+#' @importFrom basejump loadData
+#' @export
+basejump::loadData
+
+#' @importFrom basejump loadRemoteData
+#' @export
+basejump::loadRemoteData
+
+#' @importFrom basejump pasteURL
+#' @export
+basejump::pasteURL
+
+#' @importFrom basejump sampleData
+#' @export
+basejump::sampleData
+
+#' @importFrom basejump sampleData<-
+#' @export
+basejump::`sampleData<-`
+
+#' @importFrom basejump saveData
+#' @export
+basejump::saveData
+
+
+
+#' @importFrom firestarter plotHeatmap
+#' @export
+firestarter::plotHeatmap
+
+#' @importFrom firestarter plotCorrelationHeatmap
+#' @export
+firestarter::plotCorrelationHeatmap
+
+#' @importFrom firestarter plotQuantileHeatmap
+#' @export
+firestarter::plotQuantileHeatmap
+
+
+
+#' @importFrom minimalism plotGenesDetected
+#' @export
+minimalism::plotGenesDetected
+
+#' @importFrom minimalism theme_paperwhite
+#' @export
+minimalism::theme_paperwhite
