dac57eca245897da3003be71351b45ebe9b4d9cc
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-17 10:59:45 -0400

    Add docker checks

---
+++ travis-docker.R
@@ -0,0 +1,7 @@
+utils::sessionInfo()
+sessioninfo::session_info()
+
+rcmdcheck::rcmdcheck(args = "--no-manual", error_on = "error")
+BiocCheck::BiocCheck(`quit-with-status` = FALSE)
+
+lintr::lint_package()
