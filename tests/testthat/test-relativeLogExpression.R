6d2ff4c86ce112824f6d09380dc486eb0697a9a3
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-17 13:20:46 -0400

    Add code coverage

---
+++ tests/testthat/test-relativeLogExpression.R
@@ -0,0 +1,10 @@
+context("relativeLogExpression")
+
+test_that("SummarizedExperiment", {
+    x <- relativeLogExpression(object)
+    expect_is(x, "matrix")
+    expect_identical(
+        object = round(unname(x[1, , drop = TRUE]), digits = 3L),
+        expected = c(1.388, 1.140, 1.266, 0.822, 0.916, 1.313)
+    )
+})
