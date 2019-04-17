70a22241cb76f3b9d81a3b6025e54247f6db7f72
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-17 12:05:15 -0400

    Switch back to using globals in unit tests

--- tests/testthat/helper-globals.R
+++ tests/testthat/helper-globals.R
@@ -1,13 +1,11 @@
+data(bcb, envir = environment())
+
 # nolint start
-
-
 
 # bcbio default color palette support
 # options(
 #     bcbio.discrete.color = ggplot2::scale_color_viridis_d(),
 #     bcbio.discrete.fill = ggplot2::scale_fill_viridis_d()
 # )
-
-
 
 # nolint end
