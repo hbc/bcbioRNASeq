0671a327ababc1fb03a09122f51ff10453cbc238
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-17 12:30:04 -0400

    Break out and reorganize unit tests

---
+++ tests/testthat/test-extract.R
@@ -0,0 +1,64 @@
+context("extract")
+
+object <- bcb
+nrow <- 50L
+ncol <- 2L
+
+test_that("Normal gene and sample selection", {
+    subset <- object[seq_len(nrow), seq_len(ncol)]
+    expect_s4_class(object, "bcbioRNASeq")
+    expect_identical(
+        object = dim(subset),
+        expected = c(nrow, ncol)
+    )
+    expect_identical(
+        object = rownames(subset),
+        expected = head(rownames(object), n = nrow)
+    )
+    expect_identical(
+        object = colnames(subset),
+        expected = head(colnames(object), n = ncol)
+    )
+})
+
+test_that("Require at least 50 genes, 2 samples", {
+    expect_error(object[seq_len(49L), ])
+    expect_error(object[, seq_len(1L)])
+})
+
+test_that("Check for unmodified return when using empty brackets", {
+    expect_identical(
+        object = object[, ],
+        expected = object
+    )
+})
+
+test_that("Calculate DESeq2 transforms by default", {
+    # Transform enabled by default (if calculated).
+    expect_identical(
+        object = object %>%
+            .[seq_len(nrow), ] %>%
+            assayNames(),
+        expected = c(
+            "counts",
+            "tpm",
+            "avgTxLength",
+            "normalized",
+            "vst",
+            "fpkm"
+        )
+    )
+})
+
+test_that("Allow the user to skip transforms, using `recalculate` argument", {
+    expect_identical(
+        object = object %>%
+            .[seq_len(nrow), , recalculate = FALSE] %>%
+            assayNames(),
+        expected = c(
+            "counts",
+            "tpm",
+            "avgTxLength"
+        )
+    )
+})
---
+++ tests/testthat/test-updateObject.R
@@ -0,0 +1,26 @@
+test_that("updateObject", {
+    # Load a legacy object that doesn't contain rowRanges.
+    load("bcb_invalid.rda")
+    object <- bcb_invalid
+
+    expect_error(
+        object = validObject(object),
+        regexp = "rowRanges"
+    )
+    expect_identical(
+        object = slot(object, "metadata")[["version"]],
+        expected = package_version("0.1.4")
+    )
+
+    x <- suppressWarnings(updateObject(object))
+    expect_s4_class(x, "bcbioRNASeq")
+    expect_true(validObject(x))
+    expect_identical(
+        object = metadata(x)[["version"]],
+        expected = packageVersion
+    )
+    expect_identical(
+        object = metadata(x)[["previousVersion"]],
+        expected = package_version("0.1.4")
+    )
+})
