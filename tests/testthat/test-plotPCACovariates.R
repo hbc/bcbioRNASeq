fc2e8ab797e3ea6ffba394f6bc5cf582c3be1122
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-23 08:02:38 -0400

    Update unit tests for R 3.4 backward compatibility

--- tests/testthat/test-plotPCACovariates.R
+++ tests/testthat/test-plotPCACovariates.R
@@ -3,14 +3,24 @@ context("plotPCACovariates")
 test_that("plotPCACovariates", {
     x <- plotPCACovariates(object)
     expect_is(x, "list")
-    expect_identical(
-        names(x),
-        c(
+
+    if (packageVersion("DEGreport") < "1.18") {
+        expected <- c(
+            "significantCovars",
+            "plot",
+            "corMatrix",
+            "pcsMatrix",
+            "scatterPlot",
+            "effectsSignificantCovars"
+        )
+    } else {
+        expected <- c(
             "plot",
             "corMatrix",
             "pcsMatrix",
             "scatterPlot",
             "significants"
         )
-    )
+    }
+    expect_identical(names(x), expected)
 })
