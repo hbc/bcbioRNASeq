683bfb45b11b29f31f44e5806c2d5aa149933442
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-06 15:30:16 -0400

    Update RLE unit test

--- tests/testthat/test-relativeLogExpression.R
+++ tests/testthat/test-relativeLogExpression.R
@@ -5,6 +5,6 @@ test_that("SummarizedExperiment", {
     expect_is(x, "matrix")
     expect_identical(
         object = round(unname(x[1L, , drop = TRUE]), digits = 3L),
-        expected = c(1.388, 1.140, 1.266, 0.822, 0.916, 1.313)
+        expected = c(103.579, 89.313, 100.178, 62.151, 71.880, 108.420)
     )
 })
