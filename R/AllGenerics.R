4af27f49a2df90bd2c40858a668c9bd2ef43712c
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-18 15:34:25 -0400

    Define new generics for working with aligned counts

---
+++ R/AllGenerics.R
@@ -0,0 +1,19 @@
+#' @rdname pseudoVsAlignedCounts
+#' @export
+setGeneric(
+    name = "pseudoVsAlignedCounts",
+    def = function(object, ...) {
+        standardGeneric("pseudoVsAlignedCounts")
+    }
+)
+
+
+
+#' @rdname slotAlignedCounts
+#' @export
+setGeneric(
+    name = "slotAlignedCounts",
+    def = function(object, ...) {
+        standardGeneric("slotAlignedCounts")
+    }
+)
