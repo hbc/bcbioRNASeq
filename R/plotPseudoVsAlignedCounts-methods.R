a55247254fd74fcfa05e7146ccc30928e6a2b20a
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-23 12:40:06 -0400

    Simplify `plotPseudoVsAlignedCounts()` internal code

--- R/plotPseudoVsAlignedCounts-methods.R
+++ R/plotPseudoVsAlignedCounts-methods.R
@@ -1,7 +1,3 @@
-## FIXME Rework this function, importing code from acidplots.
-
-
-
 #' Compare pseudoaligned counts to aligned counts.
 #'
 #' @note Currently supported for salmon or kallisto. The function will
@@ -30,25 +26,27 @@ NULL
 
 
 
+#' @importFrom acidplots plotCountsCorrelation plotCountsCorrelationHeatmap
+NULL
+
+
+
 ## Updated 2019-07-23.
 `plotPseudoVsAlignedCounts,bcbioRNASeq` <-  # nolint
     function(
         object,
         genes = NULL,
         title = "pseudo vs. aligned counts",
-        return = c("plotCorrelationHeatmap", "cor"),
         ...
     ) {
         validObject(object)
         assert(
+            .isGeneLevel(object),
             isSubset(c("counts", "aligned"), assayNames(object)),
             isString(title, nullOK = TRUE)
         )
-        return <- match.arg(return)
-
         pseudo <- assays(object)[["counts"]]
         aligned <- assays(object)[["aligned"]]
-
         assert(
             is.matrix(pseudo),
             !is.integer(pseudo),
@@ -56,15 +54,6 @@ NULL
             is.matrix(aligned),
             is.integer(aligned)
         )
-
-        ## Censor genes that aren't present in both.
-        censor <- apply(X = aligned, MARGIN = 1L, FUN = anyNA)
-        message(sprintf("Censoring %d genes.", sum(censor)))
-        keep <- !censor
-        pseudo <- pseudo[keep, , drop = FALSE]
-        aligned <- aligned[keep, , drop = FALSE]
-        assert(!anyNA(aligned))
-
         if (is.character(genes)) {
             assert(
                 isCharacter(genes),
@@ -72,44 +61,19 @@ NULL
                 isSubset(genes, rownames(pseudo)),
                 isSubset(genes, rownames(aligned))
             )
-            pseudo %<>%
-                .[genes, , drop = FALSE] %>%
-                meltCounts(minCounts = NULL) %>%
-                ungroup() %>%
-                mutate_if(is.factor, as.character) %>%
-                mutate(!!sym("type") := "pseudo")
-            aligned %<>%
-                .[genes, , drop = FALSE] %>%
-                meltCounts(minCounts = NULL) %>%
-                ungroup() %>%
-                mutate_if(is.factor, as.character) %>%
-                mutate(!!sym("type") := "aligned")
-            data <- bind_rows(pseudo, aligned)
-            ggplot(
-                data = data,
-                mapping = aes(
-                    x = !!sym("colname"),
-                    y = !!sym("counts"),
-                    colour = !!sym("type")
-                )
-            ) +
-                geom_point() +
-                facet_wrap(facets = sym("rowname"), scales = "free_y") +
-                labs(x = "sample", title = title)
+            ## FIXME Switch to acidplots approach.
+            pseudo <- pseudo[genes, , drop = FALSE]
+            aligned <- aligned[genes, , drop = FALSE]
+            plotCountsCorrelation(x = pseudo, y = aligned)
         } else {
-            ## Correlation heatmap.
-            ## Consider limiting to the top n genes expressed instead.
-            cor <- cor(x = pseudo, y = aligned, method = "pearson")
-            assert(!anyNA(cor))
-            se <- SummarizedExperiment(
-                assays = list(cor = cor),
-                colData = colData(object)
-            )
-            plotCorrelationHeatmap(
-                object = se,
-                title = title,
-                ...
-            )
+            ## Censor genes that aren't present in both.
+            censor <- apply(X = aligned, MARGIN = 1L, FUN = anyNA)
+            message(sprintf("Censoring %d genes.", sum(censor)))
+            keep <- !censor
+            pseudo <- pseudo[keep, , drop = FALSE]
+            aligned <- aligned[keep, , drop = FALSE]
+            assert(!anyNA(aligned))
+            plotCountsCorrelationHeatmap(x = pseudo, y = aligned)
         }
     }
 
