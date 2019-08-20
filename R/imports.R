404f1f92f1b5247347cf26445d6ac4a4622344a5
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-20 15:25:55 -0400

    Simplify ggplot NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -27,13 +27,8 @@
 #'   runDate sampleDirs
 #' @importFrom cowplot plot_grid
 #' @importFrom edgeR DGEList calcNormFactors cpm scaleOffset
-#' @importFrom ggplot2 aes annotation_logticks coord_fixed coord_flip
-#'   element_blank element_text expand_limits facet_wrap geom_bar geom_boxplot
-#'   geom_density geom_hline geom_jitter geom_point geom_polygon geom_ribbon
-#'   geom_smooth geom_violin geom_vline ggplot ggtitle guides labs
-#'   position_jitterdodge scale_color_manual scale_x_continuous scale_x_discrete
-#'   scale_y_continuous stat_summary theme xlab ylim
-#' @importFrom ggrepel geom_label_repel geom_text_repel
+#' @importFrom ggplot2 aes expand_limits geom_point ggplot ggtitle guides labs
+#'   scale_y_continuous theme
 #' @importFrom goalie areDisjointSets areIntersectingSets assert bapply
 #'   containsAURL hasDimnames hasLength hasRownames hasUniqueCols
 #'   hasValidDimnames isADirectory isAFile isAny isCharacter isDirectory isFile
