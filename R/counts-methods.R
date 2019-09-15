1763c57688b00f49a534f75433e92891d8156319
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-15 12:13:53 -0400

    Update timestamp

--- R/counts-methods.R
+++ R/counts-methods.R
@@ -7,7 +7,7 @@
 #'
 #' @name counts
 #' @author Michael Steinbaugh, Lorena Pantano
-#' @note Updated 2019-08-07.
+#' @note Updated 2019-09-15.
 #'
 #' @inheritParams acidroxygen::params
 #' @param normalized `character(1)` or `logical(1)`.
