13a66c87659ccea39630cc60cd88f63a0732a7e0
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-15 10:50:44 -0400

    Add support for handling dots passthrough

--- R/ggplot2-internal.R
+++ R/ggplot2-internal.R
@@ -1,9 +1,14 @@
-## Dynamic handling of axis transformation.
-## Applies to counts that need to be log scaled or are already log2.
-## Updated 2019-08-20.
-.dynamicTrans <- function(object, normalized) {
+#' Dynamic handling of axis transformation
+#'
+#' Applies to counts that need to be log scaled or are already log2.
+#'
+#' @note Updated 2019-09-15.
+#' @noRd
+.dynamicTrans <- function(object, normalized, ...) {
     validObject(object)
-    normalized <- match.arg(normalized, choices = normalizedCounts)
+    dots <- list(...)
+    assert(areDisjointSets(c("countsAxisLabel", "trans"), names(dots)))
+    normalized <- match.arg(arg = normalized, choices = normalizedCounts)
     ## Check for DESeqTransform that are already log2 scale and update `trans`.
     if (normalized %in% c("rlog", "vst")) {
         trans <- "identity"
@@ -12,15 +17,18 @@
     }
     ## Coerce to RangedSummarizedExperiment.
     counts <- counts(object, normalized = normalized)
+    assays <- SimpleList(counts)
+    names(assays) <- normalized
     rse <- as(object, "RangedSummarizedExperiment")
-    assays(rse) <- list(counts)
-    assayNames(rse) <- normalized
+    assays(rse) <- assays
     ## Set the counts axis label.
     countsAxisLabel <- paste(normalized, "counts")
     ## Return.
-    list(
+    out <- list(
         object = rse,
         trans = trans,
         countsAxisLabel = countsAxisLabel
     )
+    out <- c(out, dots)
+    out
 }
