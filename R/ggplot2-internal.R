80ea6c7c6ad4bd3b0fcb3edd9ffc94677f37a90c
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-16 14:15:04 -0400

    Attempt to rework `.dynamicTrans()` as `.normalizedPlotArgs()`

--- R/ggplot2-internal.R
+++ R/ggplot2-internal.R
@@ -16,37 +16,27 @@
 
 
 
-
-## FIXME This approach is now broken due to "countsAxisLabel"
-
-#' Dynamic handling of axis transformation
+#' Coerce to SummarizedExperiment and dynamically handle axis transformation
 #'
 #' Applies to counts that need to be log scaled or are already log2.
 #'
 #' @note Updated 2019-09-16.
 #' @noRd
-.dynamicTrans <- function(object, normalized, ...) {
+.normalizedPlotArgs <- function(object, normalized, ...) {
     se <- .normalizedSE(object = object, normalized = normalized)
     dots <- list(...)
-    ## FIXME
-    assert(areDisjointSets(c("countsAxisLabel", "trans"), names(dots)))
-    ## FIXME
-    ## > countsAxisLabel <- paste(normalized, "counts")
+    assert(areDisjointSets("trans", names(dots)))
+    args <- c(
+        object = se,
+        trans = "log2",
+        dots
+    )
+    countsAxisLabel <- paste(normalized, "counts")
     ## Check for DESeqTransform that are already log2 scale and update `trans`.
     if (normalized %in% c("rlog", "vst")) {
-        trans <- "identity"
-        ## FIXME
-        ## > countsAxisLabel <- paste(countsAxisLabel, "(log2)")
-    } else {
-        trans <- "log2"
+        args[["trans"]] <- "identity"
+        countsAxisLabel <- paste(countsAxisLabel, "(log2)")
     }
-    ## Return arguments list.
-    out <- list(
-        object = se,
-        trans = trans
-        ## FIXME
-        ## > countsAxisLabel = countsAxisLabel
-    )
-    out <- c(out, dots)
-    out
+    args[["labels"]][["countsAxis"]] <- countsAxis
+    args
 }
