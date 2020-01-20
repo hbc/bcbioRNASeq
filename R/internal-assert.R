3c292c24d934e1df28b1edede92f0fd92303f5b2
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-20 18:41:14 -0500

    Improve featureCounts support

--- R/internal-assert.R
+++ R/internal-assert.R
@@ -1,9 +1,11 @@
 ## Check that the user input contains valid countsFromAbundance parameter used
 ## with tximport, for coercion to DESeqDataSet, DGEList objects.
-## Updated 2020-01-17.
+## Updated 2020-01-20.
 .assertHasValidCFA <- function(object) {
     assert(is(object, "SummarizedExperiment"))
     cfa <- metadata(object)[["countsFromAbundance"]]
+    ## Note that featureCounts callers will return NULL here.
+    if (is.null(cfa)) return(TRUE)
     assert(isCharacter(cfa))
     if (!isSubset(cfa, c("lengthScaledTPM", "no"))) {
         stop(
