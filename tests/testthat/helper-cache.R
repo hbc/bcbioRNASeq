3c4c64b3ec3773ffc70bf9ba93522083d7919b96
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-20 18:54:52 -0500

    Cache minimal fastrnaseq run from bcbio-nextgen unit tests

---
+++ tests/testthat/helper-cache.R
@@ -0,0 +1,30 @@
+if (!isTRUE(goalie::hasInternet())) {
+    warning("No Internet connection detected.")
+    return()
+}
+dir.create("cache", showWarnings = FALSE)
+files <- "fastrnaseq.tar.gz"
+mapply(
+    FUN = function(remoteDir, file, envir) {
+        destfile <- file.path("cache", file)
+        if (!file.exists(destfile)) {
+            utils::download.file(
+                url = paste(remoteDir, file, sep = "/"),
+                destfile = destfile
+            )
+        }
+    },
+    file = files,
+    MoreArgs = list(
+        remoteDir = bcbioRNASeqTestsURL,
+        envir = environment()
+    )
+)
+rm(files)
+if (!isTRUE(dir.exists(file.path("cache", "fastrnaseq")))) {
+    utils::untar(
+        tarfile = file.path("cache", "fastrnaseq.tar.gz"),
+        exdir = "cache"
+    )
+}
+stopifnot(dir.exists(file.path("cache", "fastrnaseq")))
