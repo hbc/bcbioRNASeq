6af344c4cd6f3282e49498dfc76edb3f1da0387a
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-04 15:45:59 -0500

    Include prepareTemplate as a reexport

--- R/reexports.R
+++ R/reexports.R
@@ -1,14 +1,18 @@
+#' @export
 #' @importFrom AcidGenerics plotFeaturesDetected
 #' @importMethodsFrom AcidPlots plotFeaturesDetected
-#' @export
 AcidGenerics::plotFeaturesDetected
 
+#' @export
 #' @importFrom AcidGenerics plotHeatmap
 #' @importMethodsFrom AcidPlots plotHeatmap
-#' @export
 AcidGenerics::plotHeatmap
 
+#' @export
 #' @importFrom AcidGenerics plotQuantileHeatmap
 #' @importMethodsFrom AcidPlots plotQuantileHeatmap
-#' @export
 AcidGenerics::plotQuantileHeatmap
+
+#' @export
+#' @importFrom basejump prepareTemplate
+basejump::prepareTemplate
