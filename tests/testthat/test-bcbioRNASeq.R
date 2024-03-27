03cd96c1b9da6ef5bece060b80945097c8d54799
Michael Steinbaugh <mike@steinbaugh.com>
2024-03-27 16:24:05 -0400

    Move GFF file test to longtests

--- tests/testthat/test-bcbioRNASeq.R
+++ tests/testthat/test-bcbioRNASeq.R
@@ -55,7 +55,7 @@ test_that("Import salmon counts (default)", {
     )
 })
 
-test_that("Fast mode in R", {
+test_that("Fast mode (in R)", {
     object <- bcbioRNASeq(uploadDir = uploadDir, fast = TRUE)
     expect_s4_class(object, "bcbioRNASeq")
     expect_true(validObject(object))
@@ -248,33 +248,4 @@ test_that("Sample selection", {
         object = colnames(object),
         expected = keep
     )
-})
-
-## Testing both gene and transcript level. GFF3 files are also supported, but
-## we're only testing GTF here for speed. This functionality is covered in
-## basejump tests also.
-test_that("GTF/GFF file", {
-    for (level in eval(formals(bcbioRNASeq)[["level"]])) {
-        gffURL <- pasteUrl(
-            "ftp.ensembl.org",
-            "pub",
-            "release-90",
-            "gtf",
-            "mus_musculus",
-            "Mus_musculus.GRCm38.90.gtf.gz",
-            protocol = "ftp"
-        )
-        gffFile <- file.path(cacheDir, basename(gffURL))
-        if (!file.exists(gffFile)) {
-            initDir(cacheDir)
-            download.file(url = gffURL, destfile = gffFile)
-        }
-        object <- bcbioRNASeq(
-            uploadDir = uploadDir,
-            level = level,
-            organism = organism,
-            gffFile = gffFile
-        )
-        expect_s4_class(object, "bcbioRNASeq")
-    }
 })
